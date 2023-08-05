36f28cc84cf94523f671cf92fd018f95c4f64f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:37 +0000

    Add package azkv-secrets-loader/0.0.4/azkv_secrets_loader-0.0.4-py3-none-any.whl

---
+++ packages/azkv-secrets-loader/azkv_secrets_loader-0.0.4-py3-none-any.whl/secrets_loader/secrets_loader.py
@@ -0,0 +1,26 @@
+import os
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from azure.core.exceptions import ResourceNotFoundError
+
+def load_secret(secret_name):
+    underscore_name = secret_name.replace("-", "_")
+    ev = os.getenv(underscore_name)
+    if ev:
+        return ev
+    
+    credential = DefaultAzureCredential()
+    key_vault_uri = os.getenv("KEY_VAULT_URI")
+    if not key_vault_uri:
+        raise Exception("Enviromental Variable KEY_VAULT_URI missing.")
+    
+    client = SecretClient(vault_url=key_vault_uri, credential=credential)
+
+    dashed_name = secret_name.replace("_", "-")
+    try:
+        secret_value = client.get_secret(dashed_name).value
+        return secret_value
+    except ResourceNotFoundError:
+        raise ValueError(f"Secret '{dashed_name}' not found in Azure Key Vault.")
+    except Exception as e:
+        raise RuntimeError(f"Failed to retrieve secret '{dashed_name}' from Azure Key Vault: {str(e)}")
