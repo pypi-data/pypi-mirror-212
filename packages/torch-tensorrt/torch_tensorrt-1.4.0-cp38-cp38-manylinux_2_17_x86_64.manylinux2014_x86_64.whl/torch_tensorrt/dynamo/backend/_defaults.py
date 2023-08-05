d9e71de7839d057af7a50f8e8341191c5d69d7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:18 +0000

    Add package torch-tensorrt/1.4.0/torch_tensorrt-1.4.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torch-tensorrt/torch_tensorrt-1.4.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torch_tensorrt/dynamo/backend/_defaults.py
@@ -0,0 +1,8 @@
+from torch_tensorrt.fx.utils import LowerPrecision
+
+
+PRECISION = LowerPrecision.FP32
+DEBUG = False
+MAX_WORKSPACE_SIZE = 20 << 30
+MIN_BLOCK_SIZE = 5
+PASS_THROUGH_BUILD_FAILURES = False
