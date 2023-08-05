66e3723cfbf0095f2f552d4b34c10fab8784b07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:52 +0000

    Add package scoringrules/0.2.0/scoringrules-0.2.0-py3-none-any.whl

---
+++ packages/scoringrules/scoringrules-0.2.0-py3-none-any.whl/scoringrules/backend/base.py
@@ -0,0 +1,54 @@
+from abc import abstractmethod
+from typing import TypeVar
+
+from .arrayapi import Array
+
+ArrayLike = TypeVar("ArrayLike", Array, float)
+
+
+class AbstractBackend:
+    """The abstract backend class."""
+
+    backend_name: str
+
+    @abstractmethod
+    def crps_ensemble(
+        self,
+        fcts: Array,
+        obs: ArrayLike,
+        axis: int = -1,
+        sorted_ensemble: bool = False,
+        estimator: str = "pwm",
+    ) -> Array:
+        """Compute the CRPS for a finite ensemble."""
+
+    @abstractmethod
+    def crps_normal(self, mu: ArrayLike, sigma: ArrayLike, obs: ArrayLike) -> Array:
+        """Compute the CRPS for the normal distribution."""
+
+    @abstractmethod
+    def crps_lognormal(
+        self, mulog: ArrayLike, sigmalog: ArrayLike, obs: ArrayLike
+    ) -> Array:
+        """Compute the CRPS for the log normal distribution."""
+
+    @abstractmethod
+    def energy_score(
+        self, fcts: Array, obs: Array, m_axis: int = -2, v_axis: int = -1
+    ) -> Array:
+        """Compute the Energy Score for a multivariate finite ensemble."""
+
+    @abstractmethod
+    def variogram_score(
+        self,
+        fcts: Array,
+        obs: Array,
+        p: float = 1.0,
+        m_axis: int = -2,
+        v_axisc: int = -1,
+    ) -> Array:
+        """Compute the Variogram Score for a multivariate finite ensemble."""
+
+    @abstractmethod
+    def brier_score(self, fcts: ArrayLike, obs: ArrayLike) -> Array:
+        """Compute the Brier Score for predicted probabilities."""
---
+++ packages/scoringrules/scoringrules-0.2.0-py3-none-any.whl/scoringrules/backend/numba.py
@@ -0,0 +1,115 @@
+from typing import TypeVar
+
+import numpy as np
+
+from .arrayapi import Array
+from .base import AbstractBackend
+from .gufuncs import (
+    _brier_score_ufunc,
+    _crps_ensemble_akr_circperm_gufunc,
+    _crps_ensemble_akr_gufunc,
+    _crps_ensemble_fair_gufunc,
+    _crps_ensemble_int_gufunc,
+    _crps_ensemble_nrg_gufunc,
+    _crps_ensemble_pwm_gufunc,
+    _crps_ensemble_qd_gufunc,
+    _crps_lognormal_ufunc,
+    _crps_normal_ufunc,
+    _energy_score_gufunc,
+    _variogram_score_gufunc,
+)
+
+ArrayLike = TypeVar("ArrayLike", np.ndarray, float)
+# ArrayLike = Array | float
+
+
+class NumbaBackend(AbstractBackend):
+    """The numba backend."""
+
+    def __init__(self):
+        self.backend_name = "numba"
+
+    @staticmethod
+    def crps_ensemble(
+        forecasts: Array,
+        observations: ArrayLike,
+        axis=-1,
+        sorted_ensemble=False,
+        estimator="int",
+    ) -> Array:
+        """Compute the CRPS for a finite ensemble."""
+        if axis != -1:
+            forecasts = np.moveaxis(forecasts, axis, -1)
+
+        if not sorted_ensemble and estimator not in [
+            "nrg",
+            "akr",
+            "akr_circperm",
+            "fair",
+        ]:
+            forecasts = np.sort(forecasts, axis=-1)
+
+        if estimator == "int":
+            out = _crps_ensemble_int_gufunc(forecasts, observations)
+        elif estimator == "qd":
+            out = _crps_ensemble_qd_gufunc(forecasts, observations)
+        elif estimator == "nrg":
+            out = _crps_ensemble_nrg_gufunc(forecasts, observations)
+        elif estimator == "pwm":
+            out = _crps_ensemble_pwm_gufunc(forecasts, observations)
+        elif estimator == "fair":
+            out = _crps_ensemble_fair_gufunc(forecasts, observations)
+        elif estimator == "akr":
+            out = _crps_ensemble_akr_gufunc(forecasts, observations)
+        elif estimator == "akr_circperm":
+            out = _crps_ensemble_akr_circperm_gufunc(forecasts, observations)
+        else:
+            raise ValueError(f"{estimator} is not a valid estimator for numba backend.")
+
+        return out
+
+    @staticmethod
+    def crps_normal(mu: ArrayLike, sigma: ArrayLike, observation: ArrayLike) -> Array:
+        """Compute the CRPS for a normal distribution."""
+        return _crps_normal_ufunc(mu, sigma, observation)
+
+    @staticmethod
+    def crps_lognormal(
+        mulog: ArrayLike, sigmalog: ArrayLike, observation: ArrayLike
+    ) -> Array:
+        """Compute the CRPS for a log normal distribution."""
+        return _crps_lognormal_ufunc(mulog, sigmalog, observation)
+
+    @staticmethod
+    def energy_score(
+        forecasts: Array,
+        observations: Array,
+        m_axis: int = -2,
+        v_axis: int = -1,
+    ) -> Array:
+        """Compute the Energy Score for a finite multivariate ensemble."""
+        if m_axis != -2:
+            forecasts = np.moveaxis(forecasts, m_axis, -2)
+        if v_axis != -1:
+            forecasts = np.moveaxis(forecasts, v_axis, -1)
+            observations = np.moveaxis(observations, v_axis, -1)
+        return _energy_score_gufunc(forecasts, observations)
+
+    @staticmethod
+    def brier_score(forecasts: ArrayLike, observations: ArrayLike) -> Array:
+        """Compute the Brier Score for predicted probabilities."""
+        return _brier_score_ufunc(forecasts, observations)
+
+    @staticmethod
+    def variogram_score(
+        forecasts: Array,
+        observations: Array,
+        p: float = 1.0,
+        m_axis: int = -2,
+        v_axis: int = -1,
+    ) -> Array:
+        """Compute the Variogram Score for a finite multivariate ensemble."""
+        return _variogram_score_gufunc(forecasts, observations, p)
+
+    def __repr__(self) -> str:
+        return "NumbaBackend"
