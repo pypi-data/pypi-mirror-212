0dd04f750ffeccbc494eca7bfe602bf9789118e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:15 +0000

    Add package bilireq/0.2.6/bilireq-0.2.6-py3-none-any.whl

---
+++ packages/bilireq/bilireq-0.2.6-py3-none-any.whl/bilireq/grpc/dynamic/__init__.py
@@ -0,0 +1,34 @@
+from ..protos.bilibili.ad.v1 import ad_pb2
+from ..protos.bilibili.app.dynamic.v2.dynamic_pb2 import (
+    DynAllReply,
+    DynAllReq,
+    DynMixUpListViewMoreReply,
+    DynMixUpListViewMoreReq,
+    DynSpaceReq,
+    DynSpaceRsp,
+)
+from ..protos.bilibili.app.dynamic.v2.dynamic_pb2_grpc import DynamicStub
+from ..utils import grpc_request
+
+
+@grpc_request
+async def grpc_get_user_dynamics(
+    uid: int, offset: str = "", page: int = 1, **kwargs
+) -> DynSpaceRsp:
+    stub = DynamicStub(kwargs.pop("_channel"))
+    req = DynSpaceReq(host_uid=uid, history_offset=offset, page=page)
+    return await stub.DynSpace(req, **kwargs)
+
+
+@grpc_request
+async def grpc_get_followed_dynamics(**kwargs) -> DynAllReply:
+    stub = DynamicStub(kwargs.pop("_channel"))
+    req = DynAllReq()
+    return await stub.DynAll(req, **kwargs)
+
+
+@grpc_request
+async def grpc_get_followed_dynamic_users(**kwargs) -> DynMixUpListViewMoreReply:
+    stub = DynamicStub(kwargs.pop("_channel"))
+    req = DynMixUpListViewMoreReq(sort_type=1)
+    return await stub.DynMixUpListViewMore(req, **kwargs)
