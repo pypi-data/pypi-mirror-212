8afcbcd21ce87e4566e7c5080e1c028f584d4109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:00 +0000

    Add package customtkinterx/0.2.2/customtkinterx-0.2.2-py3-none-any.whl

---
+++ packages/customtkinterx/customtkinterx-0.2.2-py3-none-any.whl/customtkinterx/__init__.py
@@ -0,0 +1,20 @@
+from customtkinterx.CTkCustom import CTkCustom
+from customtkinterx.CTkFluentTheme import fluent_theme, fluent_json as CTkFluentThemeJson, fluent_path as CTkFluentThemePath, use_fluent_theme as CTkFluentTheme
+from customtkinterx.CTkMinimalTheme import minimal_theme, minimal_json as CTkMinimalThemeJson, minimal_path as CTkMinimalThemePath, use_fluent_theme as CTkMinimalTheme
+
+from customtkinterx.CTkInfoBar import CTkInfoBar, NORMAL, CAUTION, CRITICAL
+from customtkinterx.CTkMegaMenuBar import CTkMegaMenuBar
+
+# From https://github.com/Akascape/CTkMenuBar
+from customtkinterx.CTkMenuBar import CTkMenuBar, CTkTitleMenu, CustomDropdownMenu as CTkCustomDropdownMenu
+
+# From https://github.com/Akascape/CTkPDFViewer
+from customtkinterx.CTkPDFViewer import CTkPDFViewer
+
+# From https://github.com/Akascape/CTkToolTip
+from customtkinterx.CTkToolTip import CTkToolTip
+
+# From https://github.com/Akascape/CTkTable
+from customtkinterx.CTkTable import CTkTable
+
+CTkFluentThemeTempPath = fluent_theme()
