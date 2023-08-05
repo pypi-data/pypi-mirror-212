df525d6590597d9defe0d9d210cffcb1c75b6969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:56 +0000

    Add package test-class/0.0.2/test-class-0.0.2.tar.gz

---
+++ packages/test-class/test-class-0.0.2.tar.gz/test-class-0.0.2/src/test_package/test_module.py
@@ -0,0 +1,7 @@
+class TestClass:
+    def __init__(self, var1=None, var2=None):
+        self.var1 = var1
+        self.var2 = var2
+
+    def concatenate(self):
+        print (self.var1 + self.var2)
