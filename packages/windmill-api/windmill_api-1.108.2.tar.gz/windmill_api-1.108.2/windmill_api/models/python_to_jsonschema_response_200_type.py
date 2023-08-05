d1527b228ac4e17b140d1f635bfec3f223e5d5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:24 +0000

    Add package windmill-api/1.108.2/windmill_api-1.108.2.tar.gz

---
+++ packages/windmill-api/windmill_api-1.108.2.tar.gz/windmill_api-1.108.2/windmill_api/models/python_to_jsonschema_response_200_type.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class PythonToJsonschemaResponse200Type(str, Enum):
+    VALID = "Valid"
+    INVALID = "Invalid"
+
+    def __str__(self) -> str:
+        return str(self.value)
