dd85fc800220a777291b525a48a1043494bdd7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:00 +0000

    Add package nats-py/2.3.1/nats-py-2.3.1.tar.gz

---
+++ packages/nats-py/nats-py-2.3.1.tar.gz/nats-py-2.3.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+# Metadata goes in pyproject.toml.
+# These are here for GitHub's dependency graph.
+setup(
+    name="nats-py",
+    extras_require={
+        'nkeys': ['nkeys'],
+        'aiohttp': ['aiohttp'],
+        'fast_parse': ['fast-mail-parser'],
+    }
+)
