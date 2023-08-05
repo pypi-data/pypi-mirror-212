340dc8f864d38dd44cde81d899f99fab57a2d106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:23 +0000

    Add package CheckSSLCert/0.1.0/CheckSSLCert-0.1.0.tar.gz

---
+++ packages/CheckSSLCert/CheckSSLCert-0.1.0.tar.gz/CheckSSLCert-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name='CheckSSLCert',
+    version='0.1.0',
+    url='https://github.com/Huntroid-India/CheckSSLCert',
+    project_urls={
+        "Bug Tracker": "https://github.com/Huntroid-India/CheckSSLCert/issues",
+    },
+    license='MIT',
+    author='Srimath',
+    author_email='srimath8@gmail.com',
+    description='CheckSSLCert is a Python library that allows you to check the SSL certificate status.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    package_dir={'': "src"},
+    packages=setuptools.find_packages("src"),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+    python_requires='>=3.6'
+)
