b953dc7732c5e755b83fd0b9092b0ff96df53e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:22 +0000

    Add package dist-buble/1.0/dist_buble-1.0.tar.gz

---
+++ packages/dist-buble/dist_buble-1.0.tar.gz/dist_buble-1.0/dist_buble/__init__.py
@@ -0,0 +1,17 @@
+def per(a, b = "pass"):
+    if b != "pass":
+        try:
+            return (a+b)*2
+        except:
+            return "FATAL ERROR"
+    else:
+        try:
+            return a*4
+        except:
+            return "FATAL ERROR"
+
+def ser(a, b):
+    try:
+        return a*b
+    except:
+        return "FATAL ERROR"
