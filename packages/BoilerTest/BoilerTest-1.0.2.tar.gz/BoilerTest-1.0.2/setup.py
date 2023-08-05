e1db0adcc43dfde9fa1caf91f2a310a1aee6775b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:18 +0000

    Add package BoilerTest/1.0.2/BoilerTest-1.0.2.tar.gz

---
+++ packages/BoilerTest/BoilerTest-1.0.2.tar.gz/BoilerTest-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="BoilerTest",
+    version="1.0.2",
+    description="Python test file generator",
+    long_description="A utility for automatically creating test files for Python classes",
+    author="Julius Olsson",
+    author_email="julius.olsson05@gmail.com",
+    url="https://github.com/Juliusolsson05/BoilerTest.git",
+    packages=["BoilerTest"],
+    entry_points={
+        "console_scripts": [
+            "boilertest=BoilerTest.main:main",
+        ],
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.8",
+)
