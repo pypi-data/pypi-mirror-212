575d620ccd7b34e1afedbd3e554519d5854210da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:47 +0000

    Add package genetikaplusIO/0.0.5/genetikaplusIO-0.0.5.tar.gz

---
+++ packages/genetikaplusIO/genetikaplusIO-0.0.5.tar.gz/genetikaplusIO-0.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r') as r:
+    requirements = r.read().splitlines()
+
+with open("LICENSE.txt", 'r') as l:
+    license = l.read()
+
+setup(
+    name="genetikaplusIO",
+    version="0.0.5",
+    author="Alon Ben Zion",
+    author_email="alon@genetikaplus.com",
+    license=license,
+    description="IO for database",
+    py_modules=["gp_db_io", "gp_storage_io", "gp_sqlconnection"],
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requirements
+)
