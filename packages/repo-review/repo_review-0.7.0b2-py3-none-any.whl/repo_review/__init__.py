a78a1badab6f289bd01571d14e681267614d1ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:56 +0000

    Add package repo-review/0.7.0b2/repo_review-0.7.0b2-py3-none-any.whl

---
+++ packages/repo-review/repo_review-0.7.0b2-py3-none-any.whl/repo_review/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2022 Henry Schreiner. All rights reserved.
+
+repo-review: Review repos with a set of checks defined by plugins.
+"""
+
+
+from __future__ import annotations
+
+__version__ = "0.7.0b2"
+
+__all__ = ["__version__"]
