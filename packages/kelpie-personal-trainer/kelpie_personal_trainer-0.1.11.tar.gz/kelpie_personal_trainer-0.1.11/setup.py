9bfcc41c9787fdcef5f04626df9442eec473a485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:35 +0000

    Add package kelpie-personal-trainer/0.1.11/kelpie_personal_trainer-0.1.11.tar.gz

---
+++ packages/kelpie-personal-trainer/kelpie_personal_trainer-0.1.11.tar.gz/kelpie_personal_trainer-0.1.11/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.11'
+DESCRIPTION = 'Personal Trainer Library'
+
+# Setting up
+setup(
+    name="kelpie_personal_trainer",
+    version=VERSION,
+    author="Ariel",
+    author_email="<arillesmana2001@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_data={
+        'kelpie_personal_trainer': ['models/*']
+    },
+    include_package_data=True,
+    install_requires=['pandas', 'opencv-python', 'mediapipe', 'numpy'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
