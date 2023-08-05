6db1682a7747673170fba186e5b3adedd9393ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:32 +0000

    Add package raya/2.2.2.dev0/raya-2.2.2.dev0.tar.gz

---
+++ packages/raya/raya-2.2.2.dev0.tar.gz/raya-2.2.2.dev0/src/raya/handlers/cv/classifier_handler_base.py
@@ -0,0 +1,29 @@
+import typing
+from rclpy.node import Node
+from raya.handlers.cv.model_base import CVModelBase
+
+
+class ClassifierHandlerBase(CVModelBase):
+
+    def __init__(self, node: Node, topic: str, model_id: int, model_info: dict,
+                 continues_msg: bool, cli_cmd, cmd_call):
+        self.cli_cmd = cli_cmd
+
+    async def get_classifications_once(self,
+                                       as_dict=False,
+                                       get_timestamp=False):
+        pass
+
+    def get_current_classifications(self, as_dict=False, get_timestamp=False):
+        pass
+
+    def set_classifications_callback(
+            self,
+            callback: typing.Callable = None,
+            callback_async: typing.Callable = None,
+            as_dict: bool = False,
+            call_without_classifications: bool = False):
+        pass
+
+    def cancel_finds(self):
+        pass
---
+++ packages/raya/raya-2.2.2.dev0.tar.gz/raya-2.2.2.dev0/src/raya/tools/image.py
@@ -0,0 +1,10 @@
+def show_image_once(img, title='', scale=1.0):
+    pass
+
+
+def show_image(img, title='', scale=1.0):
+    pass
+
+
+def save_image(img, path: str):
+    pass
