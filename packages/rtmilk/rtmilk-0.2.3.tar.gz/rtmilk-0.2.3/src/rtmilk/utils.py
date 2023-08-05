6bf33b24ce341c883dde3d23ddd572ac75561775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:20 +0000

    Add package rtmilk/0.2.3/rtmilk-0.2.3.tar.gz

---
+++ packages/rtmilk/rtmilk-0.2.3.tar.gz/rtmilk-0.2.3/src/rtmilk/utils.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timezone
+from typing import Generic, Optional, TypeVar
+
+from pydantic.fields import ModelField # pylint: disable=no-name-in-module
+
+PydanticField = TypeVar('PydanticField')
+
+# copied from https://github.com/samuelcolvin/pydantic/issues/181
+class EmptyStrToNone(Generic[PydanticField]):
+	@classmethod
+	def __get_validators__(cls):
+		yield cls.validate
+
+	@classmethod
+	def validate(cls, v: PydanticField, field: ModelField) -> Optional[PydanticField]:
+		if v == '':
+			return None
+
+		if field.sub_fields[0].type_ == datetime:
+			return datetime.strptime(v, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+
+		return v
