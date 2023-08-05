5bdf844c9e36386eb10ddf952e742ac8213908a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:45 +0000

    Add package pylwr/1.0.0/pylwr-1.0.0.tar.gz

---
+++ packages/pylwr/pylwr-1.0.0.tar.gz/pylwr-1.0.0/README.md
@@ -0,0 +1,9 @@
+# Python第三方包二次封装
+
+应该会用到一些其他依赖
+
+## 安装
+
+```shell
+pip install pylwr
+```
---
+++ packages/pylwr/pylwr-1.0.0.tar.gz/pylwr-1.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+ 
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="pylwr",  # 项目名称，保证它的唯一性，不要跟pypi上已存在的包名冲突即可
+    version="1.0.0",  # 程序版本
+    py_modules=['mysql'],  # 需要上传的模块名称，这样可以让这些模块直接import导入
+    author="linwr",  # 项目作者
+    author_email="953297255@qq.com",  # 作者邮件
+    description="各种包使用的二次封装",  # 项目的一句话描述
+    long_description=long_description,  # 加长版描述
+    long_description_content_type="text/markdown",  # 描述使用Markdown
+    url="https://gitee.com/linwanrui/pylwr",  # 项目地址
+    packages=setuptools.find_packages("src"),  # 无需修改
+    # install_requires=['docutils>=0.3'], # 依赖包增加
+)
