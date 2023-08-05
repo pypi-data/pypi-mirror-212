d2fb4d38c69d33208007099e3f586c924d056ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:22 +0000

    Add package cadmium/0.0.0/cadmium-0.0.0.tar.gz

---
+++ packages/cadmium/cadmium-0.0.0.tar.gz/cadmium-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# Third party modules
+from setuptools import setup
+
+setup(name='cadmium',
+      version='0.0.0',
+      description='placeholder',
+      url='http://github.com/SK/cadmium',
+      author='Stephen RA King',
+      author_email='sking.github@gmail.com',
+      license='MIT',
+      packages=['cadmium'],
+      zip_safe=False)
