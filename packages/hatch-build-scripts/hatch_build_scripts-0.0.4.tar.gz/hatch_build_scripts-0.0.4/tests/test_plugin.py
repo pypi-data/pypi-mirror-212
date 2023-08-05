39a807aeb4f5f09e670d29e12196daeed3666697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:20 +0000

    Add package hatch-build-scripts/0.0.4/hatch_build_scripts-0.0.4.tar.gz

---
+++ packages/hatch-build-scripts/hatch_build_scripts-0.0.4.tar.gz/hatch_build_scripts-0.0.4/tests/test_plugin.py
@@ -0,0 +1,68 @@
+from hatch_build_scripts.plugin import OneScriptConfig
+
+from .utils import create_project
+
+
+def test_plugin(tmpdir):
+    tmp_lib_dir = tmpdir / "lib"
+    tmp_lib_dir.mkdir()
+
+    (tmp_lib_dir / "some-dir").mkdir()
+    (tmp_lib_dir / "another-dir").mkdir()
+
+    some_dir_out = tmp_lib_dir / "some-dir-out"
+    some_dir_out.mkdir()
+    # we expect that this file will not be cleaned
+    (some_dir_out / "module.py").write_text('print("hello")', "utf-8")
+    # we expect that this file will be cleaned
+    (some_dir_out / "f3.txt").write_text("this should be cleaned", "utf-8")
+
+    another_dir_out = tmp_lib_dir / "another-dir-out"
+    another_dir_out.mkdir()
+    # we expect that this file will be cleaned
+    (another_dir_out / "module.py").write_text('print("hello")', "utf-8")
+
+    proj = create_project(
+        tmp_lib_dir,
+        [
+            OneScriptConfig(
+                out_dir="fake",
+                commands=["echo 'hello world' > fake.txt"],
+                artifacts=["fake.txt"],
+            ),
+            OneScriptConfig(
+                out_dir="some-dir-out",
+                work_dir="some-dir",
+                commands=[
+                    "echo 'hello world' > f1.txt",
+                    "echo 'hello world' > f2.txt",
+                ],
+                # this will not clean the data.json file
+                artifacts=["*.txt"],
+            ),
+            OneScriptConfig(
+                out_dir="another-dir-out",
+                work_dir="another-dir",
+                commands=[
+                    "echo 'hello world' > f1.txt",
+                    "echo 'hello world' > f2.txt",
+                ],
+                artifacts=["*.txt"],
+                clean_out_dir=True,
+            ),
+        ],
+    )
+
+    proj.build()
+
+    extract_dir = tmpdir / "extract"
+    extract_dir.mkdir()
+
+    with proj.dist() as dist:
+        dist.extractall(extract_dir)
+
+        assert (extract_dir / "fake" / "fake.txt").exists()
+        assert (extract_dir / "some-dir-out" / "module.py").exists()
+
+        assert not (extract_dir / "some-dir-out" / "f3.txt").exists()
+        assert not (extract_dir / "another-dir-out" / "module.py").exists()
---
+++ packages/hatch-build-scripts/hatch_build_scripts-0.0.4.tar.gz/hatch_build_scripts-0.0.4/tests/utils.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+import subprocess
+import sys
+import zipfile
+from collections.abc import Iterator, Sequence
+from contextlib import contextmanager
+from dataclasses import asdict
+from pathlib import Path
+
+import toml
+
+import hatch_build_scripts
+from hatch_build_scripts.plugin import OneScriptConfig
+
+ROOT_DIR = Path(__file__).parent.parent
+
+
+def create_project(path: Path | str, scripts: Sequence[OneScriptConfig]) -> FakeProject:
+    path = Path(path)
+
+    full_config = {
+        "project": {
+            "name": "test-project",
+            "version": "0.0.0",
+            "description": "A test project",
+        },
+        "build-system": {
+            "requires": ["hatchling", f"hatch-build-scripts @ {ROOT_DIR.as_uri()}"],
+            "build-backend": "hatchling.build",
+        },
+        "tool": {
+            "hatch": {
+                "build": {
+                    "hooks": {
+                        "build-scripts": {
+                            "scripts": list(map(asdict, scripts)),
+                        }
+                    }
+                }
+            }
+        },
+    }
+
+    (path / "pyproject.toml").write_text(toml.dumps(full_config), encoding="utf-8")
+
+    return FakeProject(path)
+
+
+class FakeProject:
+    def __init__(self, path: Path) -> None:
+        self.path = path
+
+    def build(self) -> None:
+        subprocess.run(
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "cache",
+                "remove",
+                hatch_build_scripts.__name__,
+            ],
+            cwd=self.path,
+            check=True,
+        )
+        subprocess.run(
+            [sys.executable, "-m", "hatch", "build"],
+            cwd=self.path,
+            check=True,
+        )
+
+    @contextmanager
+    def dist(self) -> Iterator[zipfile.ZipFile]:
+        files = list((self.path / "dist").glob("*.whl"))
+        assert len(files) == 1
+        with zipfile.ZipFile(str(files[0])) as whl:
+            yield whl
