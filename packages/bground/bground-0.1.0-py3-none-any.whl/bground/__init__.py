da63a44d40e2a0528175a28317672ffcf4036df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:10 +0000

    Add package bground/0.1.0/bground-0.1.0-py3-none-any.whl

---
+++ packages/bground/bground-0.1.0-py3-none-any.whl/bground/__init__.py
@@ -0,0 +1,19 @@
+# Package initialization file.
+# (required for packing/distributing the directory as PY-package
+
+'''
+BGROUND package
+---------------
+Semi-automatic background subtraction.
+
+The package can subtract background in 1D data = X,Y data.
+The data are typically saved in a file containing two (or more) columns.
+
+* bground.ui = simple user interface
+* bground.iplot = creating interactive plot for background definition
+* bground.bdata = classes/data structures for background calculations
+* bground.bfunc = functions and utilities for background calculations
+'''
+
+# VERSION can be read when building PY-package
+__version__ = '0.1.0'
