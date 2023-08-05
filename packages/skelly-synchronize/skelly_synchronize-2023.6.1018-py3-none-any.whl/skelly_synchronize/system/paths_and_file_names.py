84d9cfa60bc26ba6f1676baaa8d6aa66b769b62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:14 +0000

    Add package skelly-synchronize/2023.6.1018/skelly_synchronize-2023.6.1018-py3-none-any.whl

---
+++ packages/skelly-synchronize/skelly_synchronize-2023.6.1018-py3-none-any.whl/skelly_synchronize/system/paths_and_file_names.py
@@ -0,0 +1,18 @@
+# directory names
+SYNCHRONIZED_VIDEOS_FOLDER_NAME = "synchronized_videos"
+AUDIO_FILES_FOLDER_NAME = "audio_files"
+TRIMMED_AUDIO_FOLDER_NAME = "trimmed_audio"
+
+# file names
+DEBUG_TOML_NAME = "synchronization_debug.toml"
+DEBUG_PLOT_NAME = "debug_plot.png"
+
+# debug dictionary keys
+RAW_VIDEO_NAME = "Raw_video_information"
+SYNCHRONIZED_VIDEO_NAME = "Synchronized_video_information"
+AUDIO_NAME = "Audio_information"
+LAG_DICTIONARY_NAME = "Lag_dictionary"
+
+# figshare info
+FIGSHARE_ZIP_FILE_URL = "https://figshare.com/ndownloader/files/40293973"
+FIGSHARE_SAMPLE_DATA_FILE_NAME = "freemocap_sample_data"
