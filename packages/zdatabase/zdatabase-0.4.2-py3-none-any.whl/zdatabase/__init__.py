aae89cf0fe7c760403fa885d2cd55f48ff53a407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:57 +0000

    Add package zdatabase/0.4.2/zdatabase-0.4.2-py3-none-any.whl

---
+++ packages/zdatabase/zdatabase-0.4.2-py3-none-any.whl/zdatabase/__init__.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class Database:
+    def make_url(self, config, db_type):
+        if db_type == 'postgre':
+            url = 'postgresql://{user}:{password}@{host}:{port}/{db}'.format(**config)
+        else:
+            url = 'mysql+pymysql://{user}:{password}@{host}:{port}/{db}?charset=utf8mb4'.format(**config)
+        return url
+
+    def init(self, url):
+        engine = create_engine(url)
+        metadata = MetaData(bind=engine)
+        Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        self.session = Session()
+        return engine, metadata
+ 
+ 
+Base = declarative_base()
+db = Database()
