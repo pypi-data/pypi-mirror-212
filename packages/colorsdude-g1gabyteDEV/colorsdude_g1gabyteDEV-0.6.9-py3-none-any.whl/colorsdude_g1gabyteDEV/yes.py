3558deaa6b1ca60bb4a8a5433141ff30f162b7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:49 +0000

    Add package colorsdude-g1gabyteDEV/0.6.9/colorsdude_g1gabyteDEV-0.6.9-py3-none-any.whl

---
+++ packages/colorsdude-g1gabyteDEV/colorsdude_g1gabyteDEV-0.6.9-py3-none-any.whl/colorsdude_g1gabyteDEV/yes.py
@@ -0,0 +1,27 @@
+bold = '\033[1m'
+normal = '\033[0m'
+italic = '\x1B[3m'
+
+gold = '\033[38;2;230;190;0m' + bold
+silver = '\033[38;2;221;221;221m' + bold
+copper = '\033[38;2;170;44;0m' + bold
+
+paleyellow = '\033[38;2;255;255;215m' + bold
+lime = '\033[38;2;00;255;00m' + bold
+turquoise = '\033[38;2;0;255;255m' + bold
+teal = '\033[38;2;0;170;170m' + bold
+
+yellow = '\033[38;2;255;255;0m' + bold
+green = '\033[38;2;00;160;00m' + bold
+blue = '\033[38;2;0;40;255m' + bold
+purple = '\033[38;2;130;0;250m' + bold
+brown = '\033[38;2;135;62;35m' + bold
+red = '\033[0;31m' + bold
+orange = '\033[38;2;255;90;0m' + bold
+
+darkgrey = '\033[38;2;100;100;100m' + bold
+grey = '\033[38;2;130;130;130m' + bold
+
+white = '\033[38;2;255;255;255m' + bold
+platinum = '\033[38;2;205;192;255m' + bold
+pink = '\033[38;2;255;205;192m' + bold
