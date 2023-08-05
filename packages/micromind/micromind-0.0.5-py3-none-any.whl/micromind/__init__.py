1d35b048f6de4e8a97e91a5801bb612071daef56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:04 +0000

    Add package micromind/0.0.5/micromind-0.0.5-py3-none-any.whl

---
+++ packages/micromind/micromind-0.0.5-py3-none-any.whl/micromind/__init__.py
@@ -0,0 +1,18 @@
+from .networks.phinet import PhiNet
+from .utils import configlib
+
+
+# Package version
+__version__ = "0.0.5"
+
+"""datasets_info is a dictionary that contains information about the attributes
+of the datasets.
+This dictionary is used in networks.py inside the from_pretrained class method
+in order to examine the inputs and initialize the PhiNet or, in case of
+mismatching between dataset and Nclasses, raise an AssertionError."""
+datasets_info = {
+    "CIFAR-100": {"Nclasses": 100, "NChannels": 3, "ext": ".pth.tar"},
+    "CIFAR-10": {"Nclasses": 10, "NChannels": 3, "ext": ".pth.tar"},
+    "ImageNet-1k": {"Nclasses": 1000, "NChannels": 3, "ext": ".pth.tar"},
+    "MNIST": {"Nclasses": 10, "NChannels": 1, "ext": ".pth.tar"},
+}
