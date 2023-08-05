59e196a429274316b96ba08e50d3ead55bf915fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:24 +0000

    Add package mct-nightly/1.8.0.4062023.post428/mct_nightly-1.8.0.4062023.post428-py3-none-any.whl

---
+++ packages/mct-nightly/mct_nightly-1.8.0.4062023.post428-py3-none-any.whl/model_compression_toolkit/exporter/model_exporter/keras/fakely_quant_keras_exporter.py
@@ -0,0 +1,131 @@
+# Copyright 2022 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from typing import Dict, Callable
+
+import keras.models
+import keras.models
+import tensorflow as tf
+from keras.engine.base_layer import Layer
+
+from model_compression_toolkit.logger import Logger
+from model_compression_toolkit.exporter.model_exporter.keras.base_keras_exporter import \
+    BaseKerasExporter
+from mct_quantizers import KerasQuantizationWrapper
+
+
+
+class FakelyQuantKerasExporter(BaseKerasExporter):
+    """
+    Exporter for fakely-quant Keras models.
+    The exporter expects to receive an exportable model (where each layer's full quantization parameters
+    can be retrieved), and convert it into a fakely-quant model (namely, weights that are in fake-quant
+    format) and fake-quant layers for the activations.
+    """
+
+    def __init__(self,
+                 model: keras.models.Model,
+                 is_layer_exportable_fn: Callable,
+                 save_model_path: str):
+        """
+
+        Args:
+            model: Model to export.
+            is_layer_exportable_fn: Callable to check whether a layer can be exported or not.
+            save_model_path: Path to save the exported model.
+        """
+
+        super().__init__(model,
+                         is_layer_exportable_fn,
+                         save_model_path)
+
+    def export(self) -> Dict[str, type]:
+        """
+        Convert an exportable (fully-quantized) Keras model to a fakely-quant model
+        (namely, weights that are in fake-quant format) and fake-quant layers for the activations.
+        """
+
+        def _unwrap_quantize_wrapper(layer: Layer):
+            """
+            Convert layer wrapped with ExtendedQuantizeWrapper to the layer it wraps,
+             so it's ready to export. Notice that layers with activation quantization remain
+             wrapped using ExtendedQuantizeWrapper to contain its activation quantizer.
+
+            Args:
+                layer: Layer to unwrap.
+
+            Returns:
+                Layer after unwrapping.
+
+            """
+            assert self.is_layer_exportable_fn(layer), f'Layer {layer.name} is not exportable.'
+
+            # If weights are quantized, use the quantized weight for the new built layer.
+            if layer.is_weights_quantization:
+                new_layer = layer.layer.__class__.from_config(layer.layer.get_config())
+                with tf.name_scope(new_layer.name):
+                    new_layer.build(layer.input_shape)
+
+                # Build a list of the layer's new weights.
+                weights_list = []
+                # Go over weights, check if they should be quantized, and quantize if this is the case:
+                for w in new_layer.weights:
+                    val = None
+                    for qw in layer.weights:
+                        if w.name in qw.name:
+                            # Use quantized weight if layer attribute should be quantized.
+                            # For example: check if 'kernel_0' is an attribute
+                            # that should be quantized. First, extract 'kernel' from variable name, check if the
+                            # quantize config contains this as an attribute for quantization. If so -
+                            # Take the quantized weight from the quantize_config and set it to the new layer.
+                            attribute_name = w.name.split('/')[-1].split(':')[0]
+                            if attribute_name in layer.weights_quantizers.keys():
+                                quantizer = layer.weights_quantizers.get(attribute_name)
+                                val = quantizer(qw)
+                            else:
+                                val = qw
+                    if val is None:
+                        Logger.error(f'Could not match weight name: {w.name}')
+                    weights_list.append(val)
+
+                new_layer.set_weights(weights_list)
+                new_layer.trainable = False
+
+                # If activations are also quantized, wrap the layer back using ActivationQuantizeConfig
+                # from original wrapper (weights wrapping is no longer needed).
+                if layer.is_activation_quantization:
+                    new_layer = KerasQuantizationWrapper(layer=new_layer,
+                                                         activation_quantizers=layer.activation_quantizers)
+
+                return new_layer
+
+            # If this is a layer with activation quantization only, just return it
+            # as activation quantization in the fake-quant case uses the wrapper for quantization.
+            return layer
+
+
+        # clone each layer in the model and apply _unwrap_quantize_wrapper to layers wrapped with a QuantizeWrapper.
+        self.exported_model = tf.keras.models.clone_model(self.model,
+                                                          input_tensors=None,
+                                                          clone_function=_unwrap_quantize_wrapper)
+
+        if self.exported_model is None:
+            Logger.critical(f'Exporter can not save model as it is not exported')  # pragma: no cover
+
+        Logger.info(f'Exporting FQ Keras model to: {self.save_model_path}')
+
+        keras.models.save_model(self.exported_model, self.save_model_path)
+
+        return FakelyQuantKerasExporter.get_custom_objects()
+
---
+++ packages/mct-nightly/mct_nightly-1.8.0.4062023.post428-py3-none-any.whl/model_compression_toolkit/exporter/model_wrapper/keras/validate_layer.py
@@ -0,0 +1,82 @@
+# Copyright 2022 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from typing import Any
+
+
+from model_compression_toolkit.logger import Logger
+from model_compression_toolkit.constants import FOUND_TF
+
+from mct_quantizers import BaseInferableQuantizer
+
+
+if FOUND_TF:
+    from keras.engine.input_layer import InputLayer
+    from mct_quantizers import KerasQuantizationWrapper
+
+    def is_keras_layer_exportable(layer: Any) -> bool:
+        """
+        Check whether a Keras layer is a valid exportable layer or not.
+
+        Args:
+            layer: Keras layer to check if considered to be valid for exporting.
+
+        Returns:
+            Check whether a Keras layer is a valid exportable layer or not.
+        """
+        # Keras Input layers are not wrapped
+        if isinstance(layer, InputLayer):
+            return True
+
+        valid_layer = isinstance(layer, KerasQuantizationWrapper)
+        if not valid_layer:
+            Logger.error(
+                f'Exportable layer must be wrapped using KerasQuantizationWrapper, but layer {layer.name} is of type '
+                f'{type(layer)}') # pragma: no cover
+
+        valid_weights_quantizers = isinstance(layer.weights_quantizers, dict)
+        if not valid_weights_quantizers:
+            Logger.error(
+                f'KerasQuantizationWrapper must have a weights_quantizers but has a '
+                f'{type(layer.weights_quantizers)} object') # pragma: no cover
+
+        for _, weights_quantizer in layer.weights_quantizers.items():
+            if not isinstance(weights_quantizer, BaseInferableQuantizer):
+                Logger.error(
+                    f'weights_quantizer must be a BaseInferableQuantizer object but has a '
+                    f'{type(weights_quantizer)} object')  # pragma: no cover
+
+        valid_activation_quantizers = isinstance(layer.activation_quantizers, list)
+        if not valid_activation_quantizers:
+            Logger.error(
+                f'KerasQuantizationWrapper must have a activation_quantizers list but has a '
+                f'{type(layer.activation_quantizers)} object') # pragma: no cover
+
+        for activation_quantizers in layer.activation_quantizers:
+            if not isinstance(activation_quantizers, BaseInferableQuantizer):
+                Logger.error(
+                    f'activation_quantizers must be a BaseInferableQuantizer object but has a '
+                    f'{type(activation_quantizers)} object')  # pragma: no cover
+
+        quantizers = layer.activation_quantizers + list(layer.weights_quantizers.values())
+        is_valid_quantizers = all([isinstance(x, BaseInferableQuantizer) for x in quantizers])
+        if not is_valid_quantizers:
+            Logger.error(f'Found a quantizer that is not of type BaseInferableQuantizer') # pragma: no cover
+
+        return True
+else:
+    def is_keras_layer_exportable(*args, **kwargs):  # pragma: no cover
+        Logger.error('Installing tensorflow and tensorflow_model_optimization is mandatory '
+                     'when using is_keras_layer_exportable. '
+                     'Could not find Tensorflow package.')
