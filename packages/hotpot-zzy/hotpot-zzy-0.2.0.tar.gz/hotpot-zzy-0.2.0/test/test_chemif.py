20d93af575f27a90bc864337d7192d360e5dd0a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:00 +0000

    Add package hotpot-zzy/0.2.0/hotpot-zzy-0.2.0.tar.gz

---
+++ packages/hotpot-zzy/hotpot-zzy-0.2.0.tar.gz/hotpot-zzy-0.2.0/test/test_chemif.py
@@ -0,0 +1,64 @@
+"""
+python v3.7.9
+@Project: hotpot
+@File   : test_chemif.py
+@Author : Zhiyuan Zhang
+@Date   : 2023/3/15
+@Time   : 3:47
+"""
+import hotpot.cheminfo as ci
+from hotpot._io import Parser
+
+def mol2_read():
+    path_mol2 = 'examples/struct/mol.mol2'
+    mol = ci.Molecule.read_from(path_mol2)
+
+    return mol
+
+
+def dump_gjf():
+    path_mol2 = 'examples/struct/mol.mol2'
+    mol = ci.Molecule.read_from(path_mol2)
+
+    script = mol.dump('gjf',
+                      link0='CPU=0-48',
+                      route='M062X/Def2SVP opt(MaxCyc=10)/freq SCRF pop(Always)',
+                      )
+
+    print(script)
+
+
+def parse_g16log():
+    path_mol1 = 'examples/struct/0.log'
+    path_mol2 = 'examples/struct/222.log'
+    path_mol3 = 'examples/struct/Cs-VOHSAM-5.mol2'
+
+    mol1 = ci.Molecule.read_from(path_mol1, 'g16log')
+    mol2 = ci.Molecule.read_from(path_mol2, 'g16log')
+    mol3 = ci.Molecule.read_from(path_mol3)
+
+    return mol1, mol2, mol3
+
+
+def perturb_cif():
+    path_cif = 'examples/struct/aCarbon.cif'
+    mol = ci.Molecule.read_from(path_cif, 'cif')
+    gen = mol.perturb_mol_lattice(mol_distance=0.05, max_generate_num=2)
+
+    for i, gen_mol in enumerate(gen):
+        gen_mol.writefile('cif', f'output/gen_cif/aCarbon_{i}.cif')
+
+
+if __name__ == '__main__':
+    import time
+    m1, m2, m3 = parse_g16log()
+    # m1.dump('smi')
+    # m3.dump('smi')
+    # data = mol.dump('dpmd_sys', path_save='output/dpmd_sys')
+    # m = m1 + m3
+    # for a in mol.atoms:
+    #     print(a.partial_charge, a.spin_density, a.force_vector.shape)
+    # perturb_cif()
+    # m1 = ci.Molecule.read_from('examples/struct/aCarbon.cif')
+    m2 = ci.Molecule.read_from('examples/struct/aCarbon.xyz')
+
