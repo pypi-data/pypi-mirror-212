ed975cf6035ccbfb939e4eedf1fc17df5dfdbfa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:11 +0000

    Add package callapi/0.1.0/callapi-0.1.0-py3-none-any.whl

---
+++ packages/callapi/callapi-0.1.0-py3-none-any.whl/callapi/__init.py
@@ -0,0 +1,3 @@
+from .callapi import api_key
+
+__all__ = ['api_key']
---
+++ packages/callapi/callapi-0.1.0-py3-none-any.whl/callapi/callapi.py
@@ -0,0 +1,8 @@
+import requests
+
+api_key = None
+
+def make_api_call():
+    global api_key
+    # Your API call implementation here
+    pass
