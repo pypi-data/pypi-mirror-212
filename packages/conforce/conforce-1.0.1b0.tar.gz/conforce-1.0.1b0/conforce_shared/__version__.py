437148fe0aa00215d7e5b9c54164dbaccda4d0a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:53 +0000

    Add package conforce/1.0.1b0/conforce-1.0.1b0.tar.gz

---
+++ packages/conforce/conforce-1.0.1b0.tar.gz/conforce-1.0.1b0/conforce_shared/__version__.py
@@ -0,0 +1,2 @@
+version = '1.0.1-beta'
+build_date = '2023.06.04 19:50:50'
---
+++ packages/conforce/conforce-1.0.1b0.tar.gz/conforce-1.0.1b0/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+import conforce_shared
+
+
+def read_file(path: str):
+    with open(path, "r") as fh:
+        return fh.read()
+
+
+def read_requirements(path: str):
+    return [
+        requirement.strip()
+        for requirement
+        in read_file(path).splitlines()
+        if not requirement.startswith("#") or requirement.strip() == ""
+    ]
+
+
+setup(
+    name=conforce_shared.project,
+    version=conforce_shared.version,
+    author=conforce_shared.author,
+    author_email='matthias.rettl@unileoben.ac.at',
+    description=conforce_shared.description,
+    long_description=read_file("README.md"),
+    long_description_content_type="text/markdown",
+    url='https://conforce.readthedocs.io/',
+    project_urls={
+        'Documentation': 'https://conforce.readthedocs.io/',
+        'Source': 'https://github.com/mrettl/conforce',
+        'Tracker': 'https://github.com/mrettl/conforce/issues',
+    },
+    license_files=["LICENSE.txt"],
+    packages=['conforce', 'conforce_shared'],
+    package_dir={'.': ''},
+    package_data={'conforce_shared': ['*.dll', '*.so']},
+    install_requires=read_requirements("requirements.txt"),
+    extras_require={
+        "doc": read_requirements("doc/requirements.txt"),
+        "examples": read_requirements("examples/requirements.txt")
+    },
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Programming Language :: Python :: 3 :: Only",
+        (
+            "Development Status :: 3 - Alpha" if "-alpha" in conforce_shared.version.lower()
+            else "Development Status :: 4 - Beta" if "-beta" in conforce_shared.version.lower()
+            else "Development Status :: 5 - Production/Stable"
+        )
+    ]
+)
