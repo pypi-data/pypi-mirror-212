29db4f30ea0672f1a3b604c5fe679d81dda35dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:59 +0000

    Add package tolibaev/1.0/tolibaev-1.0.tar.gz

---
+++ packages/tolibaev/tolibaev-1.0.tar.gz/tolibaev-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tolibaev',
+      version='1.0',
+      description='Математический модуль',
+      packages=['tolibaev'],
+      author_email='tolibaevrinat4@gmail.com',
+      zip_safe=False)
---
+++ packages/tolibaev/tolibaev-1.0.tar.gz/tolibaev-1.0/tolibaev/tolibaev.py
@@ -0,0 +1,17 @@
+def kv(a):
+    return a * a
+
+def kub(a):
+    return a * a * a
+
+def kv_kor(a):
+    return a**(1/2)
+
+def kub_kor(a):
+    return a ** (1 / 3)
+
+def step(a, b):
+    return a ** b
+
+
+
