70911af191bd383f72b1a3c15eaec15d50294cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:20 +0000

    Add package as3toplevel/0.0.3/as3toplevel-0.0.3.tar.gz

---
+++ packages/as3toplevel/as3toplevel-0.0.3.tar.gz/as3toplevel-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as ld:
+      long_desc = ld.read()
+
+setup(name="as3toplevel",
+      version="0.0.3",
+      author="ajdelguidice",
+      author_email="ajdelguidice@gmail.com",
+      url="https://github.com/ajdelguidice/python-as3toplevel",
+      py_modules=["as3toplevel"],
+      description="Python implementation of the ActionScript3 toplevel",
+      long_description=long_desc,
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      classifiers=[
+            "Development Status :: 4 - Beta",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.8",
+            "Topic :: Utilities",
+            ],
+      python_requires=">=3.8",
+      install_requires=["numpy",],
+      )
