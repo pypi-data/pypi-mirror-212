de7fbcb84113cc62420804b0e2f6a93256a73208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:13 +0000

    Add package relevanceai/4.0.0/relevanceai-4.0.0-py3-none-any.whl

---
+++ packages/relevanceai/relevanceai-4.0.0-py3-none-any.whl/relevanceai/__init__.py
@@ -0,0 +1,6 @@
+from relevanceai.auth import login, config
+from relevanceai.chain import create
+from relevanceai.env import set_key, list_keys, delete_key
+from relevanceai.datasets import Dataset, list_datasets
+
+__version__ = "4.0.0"
