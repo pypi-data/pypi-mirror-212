580411f775f5a3a673b1b7686de0fe7b3da6ad2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:25 +0000

    Add package zibanu-django/1.2.0a10/zibanu-django-1.2.0a10.tar.gz

---
+++ packages/zibanu-django/zibanu-django-1.2.0a10.tar.gz/zibanu-django-1.2.0a10/README.md
@@ -0,0 +1,7 @@
+# Zibanu Django 
+
+This package or library contains some functionalities commonly used in django projects and django rest_framework projects. 
+
+## Django db
+
+
---
+++ packages/zibanu-django/zibanu-django-1.2.0a10.tar.gz/zibanu-django-1.2.0a10/zibanu/django/auth/api/services/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# ****************************************************************
+# IDE:          PyCharm
+# Developed by: macercha
+# Date:         16/04/23 18:16
+# Project:      Zibanu - Django
+# Module Name:  __init__.py
+# Description:
+# ****************************************************************
+from .user import UserService
+
+__all__ = [
+    "UserService"
+]
