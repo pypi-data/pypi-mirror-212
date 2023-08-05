85d3f1575a2245f1d98584f697cfd17e2c338a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:20 +0000

    Add package balladeer/0.37.0/balladeer-0.37.0.tar.gz

---
+++ packages/balladeer/balladeer-0.37.0.tar.gz/balladeer-0.37.0/README.rst
@@ -0,0 +1,46 @@
+Balladeer
+:::::::::
+
+This is a Python package for creating web-enabled interactive screenplay.
+For documentation, please visit the `development blog site
+<https://tundish.github.io/balladeer/>`_.
+
+Installation
+============
+
+Here are the install instructions for Linux. You need Python version 3.11 or higher.
+
+Virtual Environment
+-------------------
+
+#. First make a fresh Python virtual environment::
+
+    python3 -m venv ~/balladeer-app
+
+Packages
+--------
+
+#. Update the package manager within it::
+
+    ~/balladeer-app/bin/pip install -U pip wheel
+
+#. Install (or update) Balladeer::
+
+    ~/balladeer-app/bin/pip install -U balladeer
+
+Examples
+========
+
+#. Download the `repository as a zip file <https://github.com/tundish/balladeer/archive/master.zip>`_.
+   Unzip it to a local directory.
+
+#. `cd` to one of the directories under `examples`::
+
+    cd balladeer/examples/ex_00_hello_world
+
+#. Run the example like this::
+
+    ~/balladeer-app/bin/python -m main
+
+    Hello, World!
+
---
+++ packages/balladeer/balladeer-0.37.0.tar.gz/balladeer-0.37.0/balladeer/lite/world.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#   encoding: utf-8
+
+# This is part of the Balladeer library.
+# Copyright (C) 2023 D E Haynes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from collections import defaultdict
+from collections.abc import Generator
+import enum
+
+from balladeer.lite.entity import Entity
+from balladeer.lite.types import Grouping
+from balladeer.lite.types import State
+
+
+class WorldBuilder:
+    def __init__(self, map, config=None):
+        self.map = map
+        self.config = config
+
+        self.entities = list(self.build())
+        self.typewise = Grouping.typewise(self.entities)
+
+    @property
+    def statewise(self):
+        rv = Grouping(list)
+        for entity in self.entities:
+            for state in entity.states.values():
+                rv[str(state)].append(entity)
+        return rv
+
+    def build(self):
+        return ()
