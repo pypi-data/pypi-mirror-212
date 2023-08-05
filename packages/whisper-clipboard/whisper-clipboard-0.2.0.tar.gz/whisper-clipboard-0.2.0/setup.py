a43e5b4e5d0fa47c7cf8f82392c74ecf143200d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:13 +0000

    Add package whisper-clipboard/0.2.0/whisper-clipboard-0.2.0.tar.gz

---
+++ packages/whisper-clipboard/whisper-clipboard-0.2.0.tar.gz/whisper-clipboard-0.2.0/setup.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).resolve().parent
+
+with open(this_directory / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open(this_directory / "requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+# Rest of your setup.py code...
+
+
+setup(
+    name="whisper-clipboard",
+    version="0.2.0",
+    description="A basic TUI for transcribing audio to your clipboard using OpenAI's whisper models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Bent Mueller",
+    author_email="bentmuller.ai@gmail.com",
+    url="http://github.com/data-stepper/whisper-clipboard",
+    py_modules=["transcribe"],
+    entry_points={
+        "console_scripts": [
+            "transcribe = transcribe:main",
+        ],
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    install_requires=requirements,
+)
