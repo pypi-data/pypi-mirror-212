1f849196d3424a37a93dd65fce4d69750b81d3db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:47 +0000

    Add package babi/1.5.5/babi-1.5.5.tar.gz

---
+++ packages/babi/babi-1.5.5.tar.gz/babi-1.5.5/babi/_types.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Protocol  # python3.8+
+else:
+    Protocol = object
---
+++ packages/babi/babi-1.5.5.tar.gz/babi-1.5.5/babi/hl/interface.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import NamedTuple
+from typing import Tuple
+
+from babi._types import Protocol
+from babi.buf import Buf
+
+
+class HL(NamedTuple):
+    x: int
+    end: int
+    attr: int
+
+
+HLs = Tuple[HL, ...]
+
+
+class RegionsMapping(Protocol):
+    def __getitem__(self, idx: int) -> HLs: ...
+
+
+class FileHL(Protocol):
+    @property
+    def include_edge(self) -> bool: ...
+    @property
+    def regions(self) -> RegionsMapping: ...
+    def highlight_until(self, lines: Buf, idx: int) -> None: ...
+    def register_callbacks(self, buf: Buf) -> None: ...
