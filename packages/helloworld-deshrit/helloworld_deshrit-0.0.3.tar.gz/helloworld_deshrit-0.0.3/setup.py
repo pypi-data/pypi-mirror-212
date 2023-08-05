dca2d5b18a0bea3f03e3a88282a5320418308506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:23 +0000

    Add package helloworld-deshrit/0.0.3/helloworld_deshrit-0.0.3.tar.gz

---
+++ packages/helloworld-deshrit/helloworld_deshrit-0.0.3.tar.gz/helloworld_deshrit-0.0.3/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="helloworld_deshrit",
+    version="0.0.3",
+    description="This is just a simple hello world project",
+    long_description="Adding the long description for the project just some shitt i dunno ",
+    author="Deshrit Baral",
+    author_email="deshritbaral@gmail.com",
+    package_dir={
+        '': 'src'
+    }
+)
