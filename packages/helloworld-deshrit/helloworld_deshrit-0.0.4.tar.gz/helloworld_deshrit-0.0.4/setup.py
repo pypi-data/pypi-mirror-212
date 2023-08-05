ef8964e9dc441ea21a11261c17cf066e90de0521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:24 +0000

    Add package helloworld-deshrit/0.0.4/helloworld_deshrit-0.0.4.tar.gz

---
+++ packages/helloworld-deshrit/helloworld_deshrit-0.0.4.tar.gz/helloworld_deshrit-0.0.4/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="helloworld_deshrit",
+    version="0.0.4",
+    description="This is just a simple hello world project",
+    long_description="Adding the long description for the project just some shitt i dunno ",
+    url="https://github.com/deshrit/helloworld_deshrit",
+    author="Deshrit Baral",
+    author_email="deshritbaral@gmail.com",
+    package_dir={
+        '': 'src'
+    }
+)
