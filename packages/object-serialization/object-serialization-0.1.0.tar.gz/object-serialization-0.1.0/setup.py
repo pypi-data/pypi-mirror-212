9552913df314f6c1a52e9df49aac7e9f060ec914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:21 +0000

    Add package object-serialization/0.1.0/object-serialization-0.1.0.tar.gz

---
+++ packages/object-serialization/object-serialization-0.1.0.tar.gz/object-serialization-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='object-serialization',
+    version='0.1.0',
+    description='Python serialization package for python objects',
+    packages=find_packages(),
+    install_requires=[],
+)
