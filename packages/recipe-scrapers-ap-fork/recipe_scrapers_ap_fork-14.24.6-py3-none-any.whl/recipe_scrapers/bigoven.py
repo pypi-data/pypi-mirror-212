caa283609e80aa1d8efa69426b88c3884c6e7993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:22 +0000

    Add package recipe-scrapers-ap-fork/14.24.6/recipe_scrapers_ap_fork-14.24.6-py3-none-any.whl

---
+++ packages/recipe-scrapers-ap-fork/recipe_scrapers_ap_fork-14.24.6-py3-none-any.whl/recipe_scrapers/bigoven.py
@@ -0,0 +1,53 @@
+# mypy: disallow_untyped_defs=False
+from ._abstract import AbstractScraper
+from ._utils import get_yields, normalize_string
+
+
+class BigOven(AbstractScraper):
+    @classmethod
+    def host(cls):
+        return "bigoven.com"
+
+    def title(self):
+        return self.schema.title()
+
+    def total_time(self):
+        return self.schema.total_time()
+
+    def yields(self):
+        return get_yields(self.soup.find("div", {"class": "yield"}).text)
+
+    def image(self):
+        return self.schema.image()
+
+    def ingredients(self):
+        ig_list = self.soup.find("ul", {"class": "ingredients-list"})
+        if ig_list is not None:
+            rows = ig_list.findAll("li")
+            return [
+                normalize_string(row.span.text)
+                for row in rows
+                if "ingHeading" not in row.span["class"]
+            ]
+        else:
+            return None
+
+    def instructions(self):
+        ps = self.soup.find("div", {"class": "instructions"}).findAll("p")
+        return "\n".join([normalize_string(p.text) for p in ps])
+
+    def ratings(self):
+        try:
+            cnt = (
+                self.soup.find("div", {"class": "recipe-rating"})
+                .find("span", {"class": "count"})
+                .text
+            )
+            rating = (
+                self.soup.find("div", {"class": "recipe-rating"})
+                .find("span", {"class": "rating"})
+                .text
+            )
+            return {"count": int(cnt), "rating": round(float(rating), 2)}
+        except Exception:
+            return None
