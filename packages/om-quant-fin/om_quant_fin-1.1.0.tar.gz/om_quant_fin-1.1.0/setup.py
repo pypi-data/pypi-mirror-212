052e048c04a7c4021a285618338923fae05c1e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:12 +0000

    Add package om-quant-fin/1.1.0/om_quant_fin-1.1.0.tar.gz

---
+++ packages/om-quant-fin/om_quant_fin-1.1.0.tar.gz/om_quant_fin-1.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from setuptools import setup
+from os import path
+
+# Read the contents of the README.md file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name = "om_quant_fin",
+    version = "1.1.0",
+    packages = find_packages(),
+    install_requires = [
+        "yfinance",
+        "pandas",
+        "scikit-learn",
+        "numpy",
+        "time",
+        "sys",
+        "plotly",
+        "statsmodels.tsa.stattools",
+        "numpy_ext",
+        "warnings",
+        "datetime"
+    ],
+    author = "Outspoken Market",
+    author_email = "info@outspokenmarket.com",
+    description = "The Outspoken Market is training the next quantitative generation. OM Quant Fin is a modern Python package for quantitative trading analysis. Our mission is to make your quant life easier and more accurate.",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
+
