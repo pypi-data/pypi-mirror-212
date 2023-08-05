07007b23df2d45b25fd42a8aabfe3132d1befb36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:46 +0000

    Add package can-message-data-generator/0.7.3/can_message_data_generator-0.7.3-cp37-abi3-manylinux_2_17_ppc64.manylinux2014_ppc64.whl

---
+++ packages/can-message-data-generator/can_message_data_generator-0.7.3-cp37-abi3-manylinux_2_17_ppc64.manylinux2014_ppc64.whl/can_message_data_generator/__init__.py
@@ -0,0 +1,5 @@
+from .can_message_data_generator import *
+
+__doc__ = can_message_data_generator.__doc__
+if hasattr(can_message_data_generator, "__all__"):
+    __all__ = can_message_data_generator.__all__
