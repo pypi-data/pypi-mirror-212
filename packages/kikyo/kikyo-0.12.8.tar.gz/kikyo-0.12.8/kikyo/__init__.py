9e84cd5c54e201a69b2057f67b127f927a8c038e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:32 +0000

    Add package kikyo/0.12.8/kikyo-0.12.8.tar.gz

---
+++ packages/kikyo/kikyo-0.12.8.tar.gz/kikyo-0.12.8/kikyo/__init__.py
@@ -0,0 +1,8 @@
+from .client import Kikyo
+from .datahub import DataHub
+from .oss import OSS
+from .search import Search
+from .settings import Settings
+from .config import configure_by_consul
+
+__version__ = '0.12.8'
