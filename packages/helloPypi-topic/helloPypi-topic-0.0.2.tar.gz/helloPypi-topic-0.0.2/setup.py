b50c46293eb10a9387311a9f976f2de951d1d6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:22 +0000

    Add package helloPypi-topic/0.0.2/helloPypi-topic-0.0.2.tar.gz

---
+++ packages/helloPypi-topic/helloPypi-topic-0.0.2.tar.gz/helloPypi-topic-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="helloPypi-topic",
+    version="0.0.2",
+    author="Caleb",
+    description="A basic hello package",
+    packages=find_packages(),
+    install_requires=[],
+)
