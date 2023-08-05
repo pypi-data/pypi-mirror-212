e29d2113bfd290b6ff30fdfd620412657468601b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:27 +0000

    Add package andebox/0.38/andebox-0.38.tar.gz

---
+++ packages/andebox/andebox-0.38.tar.gz/andebox-0.38/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+requirements = [line.strip() for line in open('requirements.txt').readlines()]
+
+setup(name='andebox',
+      version='0.38',
+      scripts=['andebox'],
+      packages=find_packages(),
+      install_requires=requirements,
+      )
