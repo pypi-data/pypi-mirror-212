27ff0ae3a30213b3c72daca38a07053e2a5806f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:12 +0000

    Add package callapi/0.1.0/callapi-0.1.0.tar.gz

---
+++ packages/callapi/callapi-0.1.0.tar.gz/callapi-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='callapi',
+    version='0.1.0',
+    description='A module for making API calls',
+    packages=['callapi'],
+    install_requires=[
+        'requests',
+    ],
+)
