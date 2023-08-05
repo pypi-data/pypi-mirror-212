c334111c06bfedbffe53f1bea2c1ecc8dac94680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:40 +0000

    Add package pyscriptnative/0.3.2/pyscriptnative-0.3.2.tar.gz

---
+++ packages/pyscriptnative/pyscriptnative-0.3.2.tar.gz/pyscriptnative-0.3.2/pyscriptnative.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template_string
+
+__version__ = "0.3.2"
+
+def template_html(body):
+    template = '''
+    <!DOCTYPE html>
+    <html>
+
+        <head>
+
+            <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
+
+            <script defer src="https://pyscript.net/latest/pyscript.js"></script>
+
+            {% block script %}
+
+                <py-script>
+
+                    {{ script }}
+
+                </py-script>
+
+            {% endblock %}
+
+            {% block requirements %}
+
+                <py-config>
+
+                    packages = {{ requirements }}
+                
+                </py-config>
+
+            {% endblock %}
+
+            {% block css %}
+
+                <style>
+
+                    {{ css }}
+                
+                </style>
+            
+            {% endblock %}
+            
+        </head>
+        
+        <body>
+
+            {% block body %}
+
+                ''' + body + '''
+
+            {% endblock %}
+                
+        </body>
+
+    </html>
+    '''
+
+    return template
+
+class Router:
+    def __init__(self):
+        self.script_content = ""
+        self.style_content = ""
+        self.body_content = ""
+        self.requirements = []
+
+    def script(self, func):
+        self.script_content = func
+
+    def html(self, func):
+        self.body_content = func
+
+    def css(self, func):
+        self.style_content = func
+
+    def require(self, pkg: str):
+        self.requirements.append(pkg)
+    
+    def requires(self, pkgs: list):
+        self.requirements = pkgs
+
+    def run(self, host='localhost', port=8080, debug=False):
+        app = Flask(__name__)
+
+        @app.route('/')
+        def index():
+            return render_template_string(template_html(body=self.body_content), host=host, port=port, debug=debug, script=self.script_content, style=self.style_content, requirements=self.requirements)
+
+        app.run(host=host, port=port, debug=debug)
