fe706167411fc4f88c6cfb4c5a0b682bca082396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:01 +0000

    Add package wzry/1.0.0/wzry-1.0.0.tar.gz

---
+++ packages/wzry/wzry-1.0.0.tar.gz/wzry-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+    name='wzry',
+    version='1.0.0',
+    author='bob',
+    author_email='56141537@qq.com',
+    description='A wzry game.',
+    long_description_content_type="""text/markdown""",
+    url='https://www.github.com/',
+    packages=['wzry'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
