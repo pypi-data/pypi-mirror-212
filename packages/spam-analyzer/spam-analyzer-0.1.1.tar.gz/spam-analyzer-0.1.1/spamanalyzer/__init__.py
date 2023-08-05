c2f1d88d88b38a8e603d24b194715b271d946960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:15 +0000

    Add package spam-analyzer/0.1.1/spam-analyzer-0.1.1.tar.gz

---
+++ packages/spam-analyzer/spam-analyzer-0.1.1.tar.gz/spam-analyzer-0.1.1/spamanalyzer/__init__.py
@@ -0,0 +1,20 @@
+"""
+.. include:: ../README.md
+.. include:: ../docs/classification.md
+.. include:: ../docs/requirements.md
+.. include:: ../docs/testing.md
+"""
+
+from os import path
+from spamanalyzer.analyzer import *
+
+__all__ = ['MailAnalyzer', 'MailAnalysis', 'Domain', 'Date']
+
+__version__ = "0.1.1"
+__config_path__ = path.join(path.expanduser('~'), '.config', 'spamanalyzer')
+
+__defaults__ = {
+    "SPAMANALYZER_CONF_PATH": __config_path__,
+    "SPAMANALYZER_CONF_FILE": path.join(__config_path__, "config.yaml"),
+    "SPAMANALYZER_VERSION": __version__,
+}
