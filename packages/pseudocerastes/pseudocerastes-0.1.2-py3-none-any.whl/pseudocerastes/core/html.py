deea0a213274b13a0f140ae6ee01d3a541338680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:53 +0000

    Add package pseudocerastes/0.1.2/pseudocerastes-0.1.2-py3-none-any.whl

---
+++ packages/pseudocerastes/pseudocerastes-0.1.2-py3-none-any.whl/pseudocerastes/core/html.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+class HtmlNode:
+    def __init__(self, tag:str, attrs:list[tuple[str, str | None]]) -> None:
+        self.index:int = 0
+        self.tag = tag
+        self.attrs: dict[str, str | None] = dict()
+        self.children: dict[int, Any] = dict()
+        self.text: dict[int, str] = dict()
+        for i in attrs:
+            self.attrs[i[0]] = i[1]
+
+    def add_child(self, child:Any):
+        self.children[self.index] = child
+        self.index += 1
+
+    def add_text(self, text:str):
+        self.text[self.index] = text
+        self.index += 1
+
+    def get_dom(self):
+        children = []
+        for i in range(self.index):
+            if i in self.children:
+                children.append(self.children[i].get_dom())
+            else:
+                children.append(self.text[i])
+        return {
+            "tag": self.tag,
+            "attrs": self.attrs,
+            "children": children
+        }
+    
+    def walk(self) -> list["HtmlNode"]:
+        yield self
+        for i in self.children:
+            for child in self.children[i].walk():
+                yield child
