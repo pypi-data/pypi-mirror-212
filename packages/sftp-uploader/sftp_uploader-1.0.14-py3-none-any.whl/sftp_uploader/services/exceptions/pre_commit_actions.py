45844c561b7060134cbf01dd72ec6b71356dd6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:05 +0000

    Add package sftp-uploader/1.0.14/sftp_uploader-1.0.14-py3-none-any.whl

---
+++ packages/sftp-uploader/sftp_uploader-1.0.14-py3-none-any.whl/sftp_uploader/services/exceptions/pre_commit_actions.py
@@ -0,0 +1,2 @@
+class CannotActivatePythonVenv(Exception):
+    message = 'Cannot activate python virtual env, check please it by `/project_root_directory/venv/` path'
---
+++ packages/sftp-uploader/sftp_uploader-1.0.14-py3-none-any.whl/sftp_uploader/services/exceptions/sftp.py
@@ -0,0 +1,6 @@
+class ErrorConnectSFTPException(Exception):
+    message = 'Error connect to SFTP'
+
+
+class ErrorUploadSFTPException(Exception):
+    message = 'Error upload to SFTP'
