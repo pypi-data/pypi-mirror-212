0101f1313c86c14879fd6355fd5e3dce2fb6f387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:27 +0000

    Add package innovationmerge/1.0.1/innovationmerge-1.0.1-py3-none-any.whl

---
+++ packages/innovationmerge/innovationmerge-1.0.1-py3-none-any.whl/innovationmerge/__init__.py
@@ -0,0 +1,3 @@
+from innovationmerge.src.ai.edge.object_detection import edge_object_detect
+
+__all__ = ["edge_object_detect"]
---
+++ packages/innovationmerge/innovationmerge-1.0.1-py3-none-any.whl/innovationmerge/tests/test_object_detection.py
@@ -0,0 +1,4 @@
+from innovationmerge import edge_object_detect
+
+def test_edge_object_detect():
+    assert edge_object_detect() == "object detection file"
