b8587dfd5b391e60471164cc2e3ddbf8c65b49eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:59 +0000

    Add package scikit-query/0.1/scikit_query-0.1-py3-none-any.whl

---
+++ packages/scikit-query/scikit_query-0.1-py3-none-any.whl/skquery/pairwise/Random.py
@@ -0,0 +1,22 @@
+import numpy as np
+from ..strategy import QueryStrategy
+
+
+class Random(QueryStrategy):
+    def __init__(self, n_clusters):
+        super().__init__()
+        self.n_clusters = n_clusters
+
+    def fit(self, X, partition=None, oracle=None):
+        ml, cl = [], []
+        constraints = {"ml": ml, "cl": cl}
+        candidates = [np.random.choice(range(X.shape[0]), size=2, replace=False).tolist() for _ in range(oracle.budget)]
+
+        for i, j in candidates:
+            must_linked = oracle.query(i, j)
+            if must_linked:
+                ml.append((i, j))
+            else:
+                cl.append((i, j))
+
+        return constraints
