8f5a7b6b120fa77b2bc9bc97408b8b33f23637d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:34 +0000

    Add package mlplatformutils/0.4/mlplatformutils-0.4.tar.gz

---
+++ packages/mlplatformutils/mlplatformutils-0.4.tar.gz/mlplatformutils-0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='mlplatformutils',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.4',
+    license='MIT',
+    author='Keshav Singh',
+    author_email='keshav_singh@hotmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    keywords='mlplatformutils',
+    install_requires=[
+          'applicationinsights','gremlinpython','azureml-core'
+      ],
+
+)
