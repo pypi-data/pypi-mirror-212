d690b9584dbd5c375b2829a574ebadb589da9928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:54 +0000

    Add package ai-services/0.5.0rc16/ai_services-0.5.0rc16-py3-none-any.whl

---
+++ packages/ai-services/ai_services-0.5.0rc16-py3-none-any.whl/services/cli_project.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+
+import click
+from rich.console import Console
+from secrets import token_urlsafe
+
+from services import init_script
+
+console = Console()
+
+
+@click.command()
+@click.option(
+    "--vite/--no-vite",
+    default=False,
+    is_flag=True,
+    show_default=True,
+    help="Get support for Vite, HTML feature should be enabled",
+)
+@click.option(
+    "--html/--no-html",
+    default=False,
+    is_flag=True,
+    show_default=True,
+    help="Add a index view example and templates",
+)
+@click.option(
+    "--users/--no-users",
+    default=False,
+    is_flag=True,
+    show_default=True,
+    help="Provide the user system",
+)
+@click.option(
+    "--tasks/--no-tasks",
+    default=False,
+    is_flag=True,
+    show_default=True,
+    help="Add a dummy task and the worker as example",
+)
+@click.option(
+    "--sql/--no-sql",
+    default=False,
+    is_flag=True,
+    show_default=True,
+    help="Add DB related files",
+)
+@click.option(
+    "--storage/--no-storage",
+    default=False,
+    is_flag=True,
+    show_default=True,
+    help="Add a default storage",
+)
+@click.option(
+    "--name",
+    default=None,
+    help="Name of the project, if empty it will ask you for a name",
+)
+@click.argument("base_path")
+def create_service_project(base_path, name, vite, html, users, tasks, sql, storage):
+    """Start a new project"""
+    root = Path(base_path).resolve()
+    opts = init_script.ScriptOpts(
+        base_path=root,
+        secret_key=token_urlsafe(32),
+        app_name=name,
+        users=users,
+        vite_enabled=vite,
+        tasks=tasks,
+        sql=sql,
+        storage=storage,
+        html=html,
+    )
+    print(opts)
+
+    init_script.create_project(opts)
---
+++ packages/ai-services/ai_services-0.5.0rc16-py3-none-any.whl/services/files/app/models.py
@@ -0,0 +1,30 @@
+from typing import Any, Dict
+from pydantic import BaseModel
+from sqlalchemy import BigInteger, Column, String
+# from services.db.psql_search import WithSearchField, tsvector_column
+{% if data.users -%}
+from {{ data.app_name}}.users_models import UserModel, GroupModel, user_group_table
+{% endif -%}
+
+from .db import Base
+
+
+# class SearchExampleModel(Base, WithSearchField):
+#     __tablename__ = "{{ data.app_name }}_example"
+#     __mapper_args__ = {"eager_defaults": True}
+# 
+#     id = Column(BigInteger, primary_key=True)
+#     name = Column(String())
+#     title = Column(String())
+#     description = Column(String())
+#     tsvector = tsvector_column()
+
+class ExampleModel(Base):
+    __tablename__ = "{{ data.app_name }}_example"
+    __mapper_args__ = {"eager_defaults": True}
+
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String())
+    title = Column(String())
+    description = Column(String())
+
