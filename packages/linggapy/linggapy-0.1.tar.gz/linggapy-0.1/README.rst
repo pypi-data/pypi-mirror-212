b949dda20c4232cdff5ed8f1a465f02084c5694b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:51 +0000

    Add package linggapy/0.1/linggapy-0.1.tar.gz

---
+++ packages/linggapy/linggapy-0.1.tar.gz/linggapy-0.1/README.rst
@@ -0,0 +1,22 @@
+Pengater
+- ma (madaar, majalan)
+- ka (kadaar, kajemak)
+- sa (sadurung, sausan)
+- pa (pasaut, )
+- pi (pitutur)
+- a (abungkul)
+
+Selselan
+- in (sinurat)
+- um (gumanti)
+- el (telunjuk)
+- er (gerabah)
+
+Pengiring 
+- ang
+- in
+- an
+- a
+- n
+- e
+- ne
---
+++ packages/linggapy/linggapy-0.1.tar.gz/linggapy-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='linggapy',
+    url='https://github.com/putuwaw/linggapy',
+    author='Putu Widyantara Artanta Wibawa',
+    author_email='putuwaw973@gmail.com',
+    packages=['linggapy'],
+    version='0.1',
+    license='MIT',
+    keywords=['stemming', 'stem', 'balinese', 'language'],
+    description='Library for Stemming Balinese Text Language'
+)
