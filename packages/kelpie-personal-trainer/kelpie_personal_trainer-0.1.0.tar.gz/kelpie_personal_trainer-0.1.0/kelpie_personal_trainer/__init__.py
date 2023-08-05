b6674d307f40efa9530b04dec4c8e7c0ff3e3165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:57 +0000

    Add package kelpie-personal-trainer/0.1.0/kelpie_personal_trainer-0.1.0.tar.gz

---
+++ packages/kelpie-personal-trainer/kelpie_personal_trainer-0.1.0.tar.gz/kelpie_personal_trainer-0.1.0/kelpie_personal_trainer/__init__.py
@@ -0,0 +1,3 @@
+from body_type import check_body
+
+print(check_body('./test-image/v-shape-guy.jpg'))
---
+++ packages/kelpie-personal-trainer/kelpie_personal_trainer-0.1.0.tar.gz/kelpie_personal_trainer-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Personal Trainer Library'
+
+# Setting up
+setup(
+    name="kelpie_personal_trainer",
+    version=VERSION,
+    author="Ariel",
+    author_email="<arillesmana2001@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['pandas', 'opencv-python', 'pickle', 'mediapipe', 'numpy'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
