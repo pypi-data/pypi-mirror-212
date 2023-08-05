e1427a0da1f7f61842601219fa910db3be26529f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:58 +0000

    Add package firegs/0.2/firegs-0.2.tar.gz

---
+++ packages/firegs/firegs-0.2.tar.gz/firegs-0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="firegs",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'google-auth',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+        'google-api-python-client',
+        'google-auth-transport-requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'firegs = firegs.script:main',
+        ],
+    },
+    author="Android Dev Notes",
+    author_email="awesomedevnotes@gmail.com",
+    description="A tool to add app to Firebase project from the command line",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/androiddevnotes/firegs",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
