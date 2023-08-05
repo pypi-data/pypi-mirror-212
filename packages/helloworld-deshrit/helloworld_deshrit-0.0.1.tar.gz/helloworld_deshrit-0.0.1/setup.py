cc370711fab5fd523ea1ff4e4d523e620c93b090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:03 +0000

    Add package helloworld-deshrit/0.0.1/helloworld_deshrit-0.0.1.tar.gz

---
+++ packages/helloworld-deshrit/helloworld_deshrit-0.0.1.tar.gz/helloworld_deshrit-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="helloworld_deshrit",
+    version="0.0.1",
+    author="Deshrit Baral",
+    author_email="deshritbaral@gmail.com",
+    package_dir={
+        '': 'src'
+    }
+)
