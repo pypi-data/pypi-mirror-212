e8e14e811815d55182fe65c674b2f23be13c8ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:35 +0000

    Add package strictdoc/0.0.42a8/strictdoc-0.0.42a8.tar.gz

---
+++ packages/strictdoc/strictdoc-0.0.42a8.tar.gz/strictdoc-0.0.42a8/strictdoc/backend/sdoc/models/free_text.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from strictdoc.backend.sdoc.models.inline_link import InlineLink
+from strictdoc.helpers.auto_described import auto_described
+
+
+@auto_described
+class FreeText:
+    def __init__(self, parent, parts: List):
+        assert isinstance(parts, list)
+        self.parent = parent
+        self.parts = parts
+        self.ng_level = None
+
+    @property
+    def is_requirement(self):
+        return False
+
+    @property
+    def is_section(self):
+        return False
+
+    def get_parts_as_text(self) -> str:
+        # [LINK: SECTION-CUSTOM-GRAMMARS]
+        text = ""
+        for part in self.parts:
+            if isinstance(part, str):
+                text += part
+            elif isinstance(part, InlineLink):
+                text += "[LINK: "
+                text += part.link
+                text += "]"
+        return text
+
+
+class FreeTextContainer(FreeText):
+    def __init__(self, parts):
+        super().__init__(None, parts)
