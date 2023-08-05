fd30f2b0280a0c5d7145f83aff51a622b4b325fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:19 +0000

    Add package mypackagelicenseprototype/1.2.0/myPackageLicensePrototype-1.2.0.tar.gz

---
+++ packages/mypackagelicenseprototype/myPackageLicensePrototype-1.2.0.tar.gz/mypackagelicenseprototype-1.2.0/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+import codecs
+import chardet
+
+def get_encoding(file):
+    with open(file, 'rb') as f:
+        return chardet.detect(f.read())['encoding']
+
+encoding = get_encoding('requirements.txt')
+install_requires = codecs.open('requirements.txt', 'r', encoding=encoding).read().splitlines()
+ 
+setup(
+    name='mypackagelicenseprototype',
+    version='1.2.0',
+    description='Description of your package',
+    install_requires=install_requires,
+    ppackage_data={'': ['src/*.py']}
+)
+
+
+# from setuptools import setup, find_packages
+
+#from myPackageLicensePrototype import __version__
+
+# Read requirements from requirements.txt
+# import codecs
+# import chardet
+# def get_encoding(file):
+#     with open(file, 'rb') as f:
+#         return chardet.detect(f.read())['encoding']
+
+# encoding = get_encoding('requirements.txt')
+# install_requires = codecs.open('requirements.txt', 'r', encoding=encoding).read().splitlines()
+
+# Package information
+# setup(
+#     name='myPackageLicensePrototype ',
+#     version='1.0.0',
+
+#     url='https://github.com/da-roth/PackageLicensePrototype',
+#     author='Daniel Roth',
+#     author_email='daniel-roth@posteo.org',
+
+#     install_requires=install_requires,
+#     packages=find_packages(),
+#     include_package_data=True,
+#     package_data={'': ['src/*.py']}
+# )
