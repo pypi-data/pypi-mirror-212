804a6b0fe571909e9e5353f26154558913ca4f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:16 +0000

    Add package chromadb-semantic/1.0.3/chromadb-semantic-1.0.3.tar.gz

---
+++ packages/chromadb-semantic/chromadb-semantic-1.0.3.tar.gz/chromadb-semantic-1.0.3/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+    name="chromadb-semantic",
+    version="1.0.3",
+    packages=find_packages(),
+    py_modules=['chromadb_semantic'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'chromadb_semantic = chromadb_semantic:main',
+        ],
+    },
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',)
