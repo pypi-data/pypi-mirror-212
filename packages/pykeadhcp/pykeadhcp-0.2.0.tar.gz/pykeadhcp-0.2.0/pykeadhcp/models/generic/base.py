7f7082b259bb540b56274df583cb3e5f46798f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:05 +0000

    Add package pykeadhcp/0.2.0/pykeadhcp-0.2.0.tar.gz

---
+++ packages/pykeadhcp/pykeadhcp-0.2.0.tar.gz/pykeadhcp-0.2.0/pykeadhcp/models/generic/base.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+def normalize_keys(string: str) -> str:
+    return string.replace("_", "-")
+
+
+class KeaBaseModel(BaseModel):
+    class Config:
+        alias_generator = normalize_keys
+        allow_population_by_field_name = True
+        use_enum_values = True
