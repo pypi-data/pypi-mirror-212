e7f1e91b2b868452bc59f037c9054407169aa252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:05 +0000

    Add package custom-numbers/1.0.0/custom_numbers-1.0.0-py3-none-any.whl

---
+++ packages/custom-numbers/custom_numbers-1.0.0-py3-none-any.whl/custom_numbers/__init__.py
@@ -0,0 +1,6 @@
+#from .custom_numbers import CustomNumeralSystem, CustomNumber # Saving some typing to the users
+from typing import List
+
+__version__: str = "1.0.0"
+__author__: str = r"Evgueni Antonov (Evgueni.Antonov@gmail.com)"
+__all__: List[str] = ["custom_numbers"]
