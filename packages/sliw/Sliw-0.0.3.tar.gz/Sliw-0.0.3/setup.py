02367f683a5460eb0cb97d924cbae0223b4ee911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:19 +0000

    Add package sliw/0.0.3/Sliw-0.0.3.tar.gz

---
+++ packages/sliw/Sliw-0.0.3.tar.gz/Sliw-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Command analyzer for pandas new version and python'
+
+# Setting up
+setup(
+    name="Sliw",
+    version=VERSION,
+    author="sliw team",
+    author_email="giokhvichia69@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/plain",
+    packages=find_packages(),
+    install_requires=[
+        "nltk",
+        "pandas"
+    ],
+    keywords=['python', 'data', 'analyze', 'information', "simplicity"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
