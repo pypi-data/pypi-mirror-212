921d79b35698eefbc9ec290387151b2305372c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:12 +0000

    Add package django-admin-buttons/0.2.5/django-admin-buttons-0.2.5.tar.gz

---
+++ packages/django-admin-buttons/django-admin-buttons-0.2.5.tar.gz/django-admin-buttons-0.2.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from glob import glob
+
+
+setup(
+    name='django_admin_buttons',
+    version='0.2.5',
+    license='MIT',
+    author="Mina Atef",
+    author_email='mina.atef0@gmail.com',
+    packages=['django_admin_buttons','django_admin_buttons.templates','django_admin_buttons.static'],
+        data_files = [
+        ('templates', glob('templates/*')), # files in source_dir only - not recursive
+        ('static', glob('static/*')), # includes sub-folders - recursive
+        # etc...
+    ],
+
+    url='https://github.com/minaaaatef/Django-Admin-Buttons',
+    keywords=('django','buttons','admin','actions'),
+    install_requires=[
+      ],
+
+
+)
