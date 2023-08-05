a2566280f88ece66e5d61db4045390b9e0ffb11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:53 +0000

    Add package fetch-lyrics-from-genius/0.1.0/fetch-lyrics-from-genius-0.1.0.tar.gz

---
+++ packages/fetch-lyrics-from-genius/fetch-lyrics-from-genius-0.1.0.tar.gz/fetch-lyrics-from-genius-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='fetch-lyrics-from-genius',
+    version='0.1.0',
+    description='A package to fetch lyrics from Genius.com',
+    author='Andrew Beveridge',
+    author_email='andrew@beveridge.uk',
+    packages=['fetch_lyrics_from_genius'],
+    install_requires=[
+        'lyricsgenius',
+        'musicbrainzngs',
+        'python-slugify'
+    ],
+    entry_points={
+        'console_scripts': [
+            'fetch-lyrics = fetch_lyrics_from_genius.fetch_lyrics:main',
+        ],
+    },
+)
