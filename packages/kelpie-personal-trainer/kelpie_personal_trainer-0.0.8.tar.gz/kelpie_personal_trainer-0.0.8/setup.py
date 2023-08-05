33580e92e194807b8604512d8a445c7ed9a6b404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:39 +0000

    Add package kelpie-personal-trainer/0.0.8/kelpie_personal_trainer-0.0.8.tar.gz

---
+++ packages/kelpie-personal-trainer/kelpie_personal_trainer-0.0.8.tar.gz/kelpie_personal_trainer-0.0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.8'
+DESCRIPTION = 'Basic hello world package'
+
+# Setting up
+setup(
+    name="kelpie_personal_trainer",
+    version=VERSION,
+    author="Ariel",
+    author_email="<arillesmana2001@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['opencv-python'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
