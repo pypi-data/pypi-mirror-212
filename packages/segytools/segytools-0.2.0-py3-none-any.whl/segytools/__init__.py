1e4ee14a698aae86a7f79783c1324f31b9b3c03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:02 +0000

    Add package segytools/0.2.0/segytools-0.2.0-py3-none-any.whl

---
+++ packages/segytools/segytools-0.2.0-py3-none-any.whl/segytools/__init__.py
@@ -0,0 +1,6 @@
+"""segytools"""
+
+from .segy_abstract_header import SegyAbstractHeader
+from .segy_file_header import SegyFileHeaderRev2
+from .segy_header_item import SegyHeaderItem
+from .segy_trace_header import SegyTraceHeaderRev2
