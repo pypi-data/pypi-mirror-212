ebb614d8f6d9baa785136bfbbfdadf2ed6e48885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:26 +0000

    Add package pyBigKinds/1.0.0/pyBigKinds-1.0.0-py3-none-any.whl

---
+++ packages/pyBigKinds/pyBigKinds-1.0.0-py3-none-any.whl/pyBigKinds/preprocessing.py
@@ -0,0 +1,127 @@
+import pandas as pd
+from sklearn.decomposition import NMF, PCA, TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.manifold import TSNE
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import Normalizer
+
+from .base import (
+    counter_to_dataframe,
+    duplication_remover,
+    keyword_list,
+    keyword_parser,
+    word_counter,
+)
+
+
+def day_range(df):
+    """날짜 범위 파악"""
+    print("first day: ", df["일자"].min(), "\n", "last day: ", df["일자"].max())
+
+
+def press_counter(df):
+    """언론사 별 보도 빈도"""
+    freq = df["언론사"].value_counts()
+    brod_df = pd.DataFrame(freq).reset_index()
+    brod_df.rename(columns={"index": "언론사", "언론사": "기사"}, inplace=True)
+    return brod_df
+
+
+def keyword_dataframe(df):
+    """키워드 단어 빈도"""
+    lis = keyword_list(df)
+    keywords = keyword_parser(lis)
+    counter = word_counter(keywords)
+    df = counter_to_dataframe(counter)
+    return df
+
+
+def keyword_dataframe_no_duplicated(df):
+    """키워드 중복 제거 단어 빈도"""
+    lis = keyword_list(df)
+    keywords = keyword_parser(lis)
+    keywords_set = duplication_remover(keywords)
+    counter = word_counter(keywords_set)
+    df = counter_to_dataframe(counter)
+    return df
+
+
+def tfidf(df, *press: str):
+    """키워드 상대 빈도"""
+    if press:
+        df = df[press]
+    lis = keyword_list(df)
+
+    tfidfv = TfidfVectorizer()
+    tdm = tfidfv.fit_transform(lis)
+
+    word_count = (
+        pd.DataFrame(
+            {
+                "단어": tfidfv.get_feature_names_out(),
+                "빈도": tdm.sum(axis=0).flat,
+            },
+        )
+        .sort_values("빈도", ascending=False)
+        .reset_index(drop=True)
+    )
+    return word_count
+
+
+def tfidf_vector(df):
+    """tfidf vector"""
+    lis = keyword_list(df)
+    pipeline = Pipeline(
+        [
+            ("vect", CountVectorizer()),
+            ("tfidf", TfidfTransformer()),
+        ],
+    )
+    vec = pipeline.fit_transform(lis).toarray()
+    return vec
+
+
+def normalize_vector(vec):
+    """normalize vector"""
+    vec_nor = Normalizer().fit_transform(vec)
+    return vec_nor
+
+
+def pca(vec, Random_State=123):
+    """PCA"""
+
+    pca_df = PCA(n_components=2, random_state=Random_State, copy=False).fit_transform(
+        vec,
+    )
+    pca_df = pd.DataFrame(pca_df, columns=["component 0", "component 1"])
+
+    return pca_df
+
+
+def nmf(vec, Random_State=123):
+    """NMF"""
+
+    nmf_df = NMF(
+        n_components=2, random_state=Random_State, init="random",
+    ).fit_transform(vec)
+    nmf_df = pd.DataFrame(nmf_df, columns=["component 0", "component 1"])
+
+    return nmf_df
+
+
+def t_sne(vec, learn_Rate=100):
+    """t-sne"""
+
+    tsne = TSNE(n_components=2, learning_rate=learn_Rate).fit_transform(vec)
+    tsne_df = pd.DataFrame(tsne, columns=["component 0", "component 1"])
+
+    return tsne_df
+
+
+def lsa(vec):
+    """LSA"""
+
+    svd = TruncatedSVD(n_components=2).fit_transform(vec)
+    svd_df = pd.DataFrame(data=svd, columns=["component 0", "component 1"])
+
+    return svd_df
---
+++ packages/pyBigKinds/pyBigKinds-1.0.0-py3-none-any.whl/pyBigKinds/visualization.py
@@ -0,0 +1,76 @@
+# pylint: disable=W0612
+
+import platform
+
+import matplotlib.pyplot as plt
+import wordcloud
+
+from .base import (
+    counter_to_dataframe,
+    duplication_remover,
+    keyword_list,
+    keyword_parser,
+    word_counter,
+)
+
+if platform.system() in ["Windows", "linux"]:
+    plt.rcParams["font.family"] = "Malgun Gothic"
+    font_path = "malgun"
+
+elif platform.system() == "Darwin":
+    plt.rcParams["font.family"] = "AppleGothic"
+    font_path = "AppleGothic"
+
+else:
+    print("미지원 os입니다.")
+
+plt.rcParams["axes.unicode_minus"] = False
+
+
+def keywords_wordcloud(df, press: str):
+    """언론사 별 키워드 워드클라우드 생성"""
+    df_keywords = df[df["언론사"] == press]
+    keywords = keyword_list(df_keywords)
+    news_key = keyword_parser(keywords)
+    news_key = duplication_remover(news_key)
+    key = word_counter(news_key)
+    news_key = counter_to_dataframe(key)
+    wc = wordcloud.WordCloud(
+        font_path=font_path,
+        width=500,
+        height=500,
+        background_color="white",
+    ).generate_from_frequencies(news_key.set_index("단어").to_dict()["빈도"])
+
+    plt.imshow(wc)
+    plt.axis("off")
+    plt.show()
+
+
+def top_words(df, press: str):
+    """언론사 별 사용 단어 빈도 상위 25개"""
+    df_keywords = df[df["언론사"].str.contains(press)]
+    keywords = keyword_list(df_keywords)
+    news_key = keyword_parser(keywords)
+    news_key = duplication_remover(news_key)
+    key = word_counter(news_key)
+    news_key = counter_to_dataframe(key)
+
+    data = news_key.head(25)
+    plt.barh(data["단어"], data["빈도"].sort_values(ascending=True))
+    plt.show()
+
+
+def scatterplot(df, label: str):
+    """scatter plot for dimension reduction"""
+    fig, ax = plt.subplots()
+    try:
+        groups = df.groupby(label)
+    except:
+        raise ValueError("There's no label")
+
+    for name, points in groups:
+        ax.scatter(points["component 0"], points["component 1"], label=name)
+
+    ax.legend()
+    plt.show()
