46e320f13915d71b46477d79060d59ec132d9806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:02 +0000

    Add package nanobind/1.4.0/nanobind-1.4.0-py3-none-any.whl

---
+++ packages/nanobind/nanobind-1.4.0-py3-none-any.whl/nanobind/src/error.cpp
@@ -0,0 +1,196 @@
+/*
+    src/error.cpp: libnanobind functionality for exceptions
+
+    Copyright (c) 2022 Wenzel Jakob
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#include <nanobind/nanobind.h>
+#include "buffer.h"
+#include "nb_internals.h"
+
+NAMESPACE_BEGIN(NB_NAMESPACE)
+NAMESPACE_BEGIN(detail)
+
+Buffer buf(128);
+
+NAMESPACE_END(detail)
+
+python_error::python_error() {
+    PyErr_Fetch(&m_type, &m_value, &m_trace);
+    check(m_type,
+          "nanobind::python_error::python_error(): error indicator unset!");
+}
+
+python_error::~python_error() {
+    if (m_type || m_value || m_trace) {
+        gil_scoped_acquire acq;
+        /* With GIL held */ {
+            // Clear error status in case the following executes Python code
+            error_scope scope;
+            Py_XDECREF(m_type);
+            Py_XDECREF(m_value);
+            Py_XDECREF(m_trace);
+        }
+    }
+    free(m_what);
+}
+
+python_error::python_error(const python_error &e)
+    : std::exception(e), m_type(e.m_type), m_value(e.m_value),
+      m_trace(e.m_trace) {
+    if (m_type || m_value || m_trace) {
+        gil_scoped_acquire acq;
+        Py_XINCREF(m_type);
+        Py_XINCREF(m_value);
+        Py_XINCREF(m_trace);
+    }
+    if (e.m_what)
+        m_what = NB_STRDUP(e.m_what);
+}
+
+python_error::python_error(python_error &&e) noexcept
+    : std::exception(e), m_type(e.m_type), m_value(e.m_value),
+      m_trace(e.m_trace), m_what(e.m_what) {
+    e.m_type = e.m_value = e.m_trace = nullptr;
+    e.m_what = nullptr;
+}
+
+const char *python_error::what() const noexcept {
+    using detail::buf;
+
+    // Return the existing error message if already computed once
+    if (m_what)
+        return m_what;
+
+    gil_scoped_acquire acq;
+
+    // Try again with GIL held
+    if (m_what)
+        return m_what;
+
+    PyErr_NormalizeException(&m_type, &m_value, &m_trace);
+    check(m_type,
+          "nanobind::python_error::what(): PyNormalize_Exception() failed!");
+
+    if (m_trace) {
+        if (PyException_SetTraceback(m_value, m_trace) < 0)
+            PyErr_Clear();
+    }
+
+#if defined(Py_LIMITED_API) || defined(PYPY_VERSION)
+    object mod = module_::import_("traceback"),
+           result = mod.attr("format_exception")(handle(m_type), handle(m_value), handle(m_trace));
+    m_what = NB_STRDUP(borrow<str>(str("\n").attr("join")(result)).c_str());
+#else
+    buf.clear();
+    if (m_trace) {
+        PyTracebackObject *to = (PyTracebackObject *) m_trace;
+
+        // Get the deepest trace possible
+        while (to->tb_next)
+            to = to->tb_next;
+
+        PyFrameObject *frame = to->tb_frame;
+        Py_XINCREF(frame);
+
+        std::vector<PyFrameObject *, detail::py_allocator<PyFrameObject *>> frames;
+
+        while (frame) {
+            frames.push_back(frame);
+#if PY_VERSION_HEX >= 0x03090000
+            frame = PyFrame_GetBack(frame);
+#else
+            frame = frame->f_back;
+            Py_XINCREF(frame);
+#endif
+        }
+
+        buf.put("Traceback (most recent call last):\n");
+        for (auto it = frames.rbegin(); it != frames.rend(); ++it) {
+            frame = *it;
+#if PY_VERSION_HEX >= 0x03090000
+            PyCodeObject *f_code = PyFrame_GetCode(frame);
+#else
+            PyCodeObject *f_code = frame->f_code;
+#endif
+            buf.put("  File \"");
+            buf.put_dstr(borrow<str>(f_code->co_filename).c_str());
+            buf.put("\", line ");
+            buf.put_uint32(PyFrame_GetLineNumber(frame));
+            buf.put(", in ");
+            buf.put_dstr(borrow<str>(f_code->co_name).c_str());
+            buf.put('\n');
+#if PY_VERSION_HEX >= 0x03090000
+            Py_DECREF(f_code);
+#endif
+            Py_DECREF(frame);
+        }
+    }
+
+    if (m_type) {
+        object name = handle(m_type).attr("__name__");
+        buf.put_dstr(borrow<str>(name).c_str());
+        buf.put(": ");
+    }
+
+    if (m_value)
+        buf.put_dstr(str(m_value).c_str());
+    m_what = buf.copy();
+#endif
+
+    return m_what;
+}
+
+void python_error::restore() noexcept {
+    check(m_type,
+          "nanobind::python_error::restore(): error was already restored!");
+
+    PyErr_Restore(m_type, m_value, m_trace);
+    m_type = m_value = m_trace = nullptr;
+}
+
+builtin_exception::builtin_exception(exception_type type, const char *what)
+    : std::runtime_error(what ? what : ""), m_type(type) { }
+builtin_exception::~builtin_exception() { }
+
+NAMESPACE_BEGIN(detail)
+
+void register_exception_translator(exception_translator t, void *payload) {
+    nb_translator_seq *cur  = &internals->translators,
+                      *next = new nb_translator_seq(*cur);
+    cur->next = next;
+    cur->payload = payload;
+    cur->translator = t;
+}
+
+NB_CORE PyObject *exception_new(PyObject *scope, const char *name,
+                                PyObject *base) {
+    object modname;
+    if (PyModule_Check(scope))
+        modname = getattr(scope, "__name__", handle());
+    else
+        modname = getattr(scope, "__module__", handle());
+
+    if (!modname.is_valid())
+        raise("nanobind::detail::exception_new(): could not determine module "
+              "name!");
+
+    str combined =
+        steal<str>(PyUnicode_FromFormat("%U.%s", modname.ptr(), name));
+
+    object result = steal(PyErr_NewException(combined.c_str(), base, nullptr));
+    check(result, "nanobind::detail::exception_new(): creation failed!");
+
+    if (hasattr(scope, name))
+        raise("nanobind::detail::exception_new(): an object of the same name "
+              "already exists!");
+
+    setattr(scope, name, result);
+    return result.release().ptr();
+}
+
+NAMESPACE_END(detail)
+NAMESPACE_END(NB_NAMESPACE)
---
+++ packages/nanobind/nanobind-1.4.0-py3-none-any.whl/nanobind/src/nb_static_property.cpp
@@ -0,0 +1,72 @@
+#include "nb_internals.h"
+
+NAMESPACE_BEGIN(NB_NAMESPACE)
+NAMESPACE_BEGIN(detail)
+
+/// `nb_static_property.__get__()`: Always pass the class instead of the instance.
+static PyObject *nb_static_property_descr_get(PyObject *self, PyObject *, PyObject *cls) {
+    if (internals->nb_static_property_enabled) {
+        return NB_SLOT(PyProperty_Type, tp_descr_get)(self, cls, cls);
+    } else {
+        Py_INCREF(self);
+        return self;
+    }
+}
+
+/// `nb_static_property.__set__()`: Just like the above `__get__()`.
+static int nb_static_property_descr_set(PyObject *self, PyObject *obj, PyObject *value) {
+    PyObject *cls = PyType_Check(obj) ? obj : (PyObject *) Py_TYPE(obj);
+    return NB_SLOT(PyProperty_Type, tp_descr_set)(self, cls, value);
+}
+
+PyTypeObject *nb_static_property_tp() noexcept {
+    PyTypeObject *tp = internals->nb_static_property;
+
+    if (NB_UNLIKELY(!tp)) {
+        PyType_Slot members;
+
+        #if PY_VERSION_HEX >= 0x030C0000
+            int basicsize = - (int) sizeof(PyObject *),
+                itemsize = 0;
+
+            // See https://github.com/python/cpython/issues/98963
+            PyMemberDef members_def[] = {
+                { "__doc__", T_OBJECT, 0, Py_RELATIVE_OFFSET, nullptr },
+                { nullptr, 0, 0, 0, nullptr }
+            };
+
+            members = { Py_tp_members, members_def };
+        #else
+            int basicsize = (int) PyProperty_Type.tp_basicsize,
+                itemsize = (int) PyProperty_Type.tp_itemsize;
+
+            members = { Py_tp_members, PyProperty_Type.tp_members };
+        #endif
+
+        PyType_Slot slots[] = {
+            { Py_tp_base, &PyProperty_Type },
+            { Py_tp_descr_get, (void *) nb_static_property_descr_get },
+            members,
+            { 0, nullptr }
+        };
+
+        PyType_Spec spec = {
+            /* .name = */ "nanobind.nb_static_property",
+            /* .basicsize = */ basicsize,
+            /* .itemsize = */ itemsize,
+            /* .flags = */ Py_TPFLAGS_DEFAULT,
+            /* .slots = */ slots
+        };
+
+        tp = (PyTypeObject *) PyType_FromSpec(&spec);
+        check(tp, "nb_static_property type creation failed!");
+
+        internals->nb_static_property = tp;
+        internals->nb_static_property_descr_set = nb_static_property_descr_set;
+    }
+
+    return tp;
+}
+
+NAMESPACE_END(NB_NAMESPACE)
+NAMESPACE_END(detail)
