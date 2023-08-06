06ff129b108c1c837e08a96f5d4e0cf2486652cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:07 +0000

    Add package PlanaPY/0.1.7/PlanaPY-0.1.7.tar.gz

---
+++ packages/PlanaPY/PlanaPY-0.1.7.tar.gz/PlanaPY-0.1.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='PlanaPY',
+    version='0.1.7',
+    license='MIT',
+    author='Erik Schau',
+    author_email='hopfenspiel@gmail.com',
+    package_dir = {'': 'PlanaPY'},
+    packages=find_packages(where='PlanaPY'),
+    description='Anaplan RESTful API Convenience Library',
+    url='https://github.com/hopfenspiel/PlanaPY',
+    keywords='anaplan',
+    install_requires=[
+          'requests',
+          'pandas',
+      ],
+
+)
