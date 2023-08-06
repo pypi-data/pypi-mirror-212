9eaae358507ceaf7e091e8f3896fa7106310dc3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:23 +0000

    Add package wandbmon/0.1.0/wandbmon-0.1.0-py3-none-any.whl

---
+++ packages/wandbmon/wandbmon-0.1.0-py3-none-any.whl/wandbmon/sdk/evaluate.py
@@ -0,0 +1,11 @@
+def evaluate(df: "pandas.DataFrame", config: dict) -> dict:
+    """Evaluate a dataframe of metrics against a config.
+
+    Args:
+        df (pandas.DataFrame): A dataframe of metrics.
+        config (dict): The config to evaluate against.
+
+    Returns:
+        dict: A dictionary of evaluation results.
+    """
+    pass
