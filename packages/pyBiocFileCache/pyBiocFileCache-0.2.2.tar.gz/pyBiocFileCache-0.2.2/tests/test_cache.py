57489e2ddb3ed7c3d4ebc5dcaed4265a0d72a7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:38 +0000

    Add package pyBiocFileCache/0.2.2/pyBiocFileCache-0.2.2.tar.gz

---
+++ packages/pyBiocFileCache/pyBiocFileCache-0.2.2.tar.gz/pyBiocFileCache-0.2.2/tests/test_cache.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+
+from pybiocfilecache.BiocFileCache import BiocFileCache
+
+__author__ = "jkanche"
+__copyright__ = "jkanche"
+__license__ = "MIT"
+
+CACHE_DIR = os.getcwd() + "/cache"
+
+
+def test_create_cache():
+    bfc = BiocFileCache(CACHE_DIR)
+    assert os.path.exists(CACHE_DIR == True)
+
+    bfc.purge()
+
+
+def test_add_get_operations():
+    bfc = BiocFileCache(CACHE_DIR)
+
+    bfc.add("test1", os.getcwd() + "/tests/data/test1.txt")
+    rec1 = bfc.get("test1")
+    assert rec1 != None
+
+    bfc.add("test2", os.getcwd() + "/tests/data/test2.txt")
+    rec2 = bfc.get("test2")
+    assert rec2 != None
+
+    frec1 = open(rec1.rpath, "r").read()
+    assert frec1 == "test1"
+
+    frec2 = open(rec2.rpath, "r").read()
+    assert frec2 == "test2"
+
+    bfc.purge()
+
+
+def test_remove_operations():
+    bfc = BiocFileCache(CACHE_DIR)
+
+    bfc.add("test1", os.getcwd() + "/tests/data/test1.txt")
+    rec1 = bfc.get("test1")
+    assert rec1 != None
+
+    bfc.add("test2", os.getcwd() + "/tests/data/test2.txt")
+    rec2 = bfc.get("test2")
+    assert rec2 != None
+
+    bfc.remove("test1")
+    rec1 = bfc.get("test1")
+    assert rec1 == None
+
+    bfc.purge()
