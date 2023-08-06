8c6b0bf544cf393c18327bc462cc1d71b7586c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:33 +0000

    Add package formal-sdk/0.2.0/formal_sdk-0.2.0-py3-none-any.whl

---
+++ packages/formal-sdk/formal_sdk-0.2.0-py3-none-any.whl/formal_sdk/client.py
@@ -0,0 +1,15 @@
+from . import datastore
+
+SERVER_URL = "https://adminv2.api.formalcloud.net"
+
+class Client(object):
+    """Formal Admin API Client"""
+
+    def __init__(self, api_key):
+        """Constructor.
+
+        Args:
+            api_key: Formal API Key
+        """
+
+        self.DataStoreClient = datastore.DataStoreService(SERVER_URL, api_key)
