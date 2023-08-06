7c06becd0c900fd198de7914920b682ea9adaf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:10 +0000

    Add package shapes-recognition/2.0.0/shapes_recognition-2.0.0.tar.gz

---
+++ packages/shapes-recognition/shapes_recognition-2.0.0.tar.gz/shapes_recognition-2.0.0/shapes_recognition/__init__.py
@@ -0,0 +1,4 @@
+from .pure_cython.ai_tables import clear_all, get_self_study_data, get_recognition_data
+from .pure_cython.ai_tables import self_study
+from .pure_cython.ai_tables import recognition
+from .pure_cython.ai_tables import show_self_study_results, show_recognition_results
