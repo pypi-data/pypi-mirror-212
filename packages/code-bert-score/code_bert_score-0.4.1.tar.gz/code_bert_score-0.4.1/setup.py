21cf112eeb7e53a36b84e524c39064bdf92f1072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:16 +0000

    Add package code-bert-score/0.4.1/code_bert_score-0.4.1.tar.gz

---
+++ packages/code-bert-score/code_bert_score-0.4.1.tar.gz/code_bert_score-0.4.1/setup.py
@@ -0,0 +1,42 @@
+from io import open
+from setuptools import find_packages, setup
+
+setup(
+    name="code_bert_score",
+    version='0.4.1',
+    author="Shuyan Zhou, Uri Alon, Sumit Agarwal, and Graham Neubig",
+    author_email="urialon1@gmail.com",
+    description="PyTorch implementation of Code BERT score",
+    long_description=open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    keywords='BERT NLP deep learning google metric',
+    license='MIT',
+    url="https://github.com/neulab/code-bert-score",
+    download_url = 'https://github.com/neulab/code-bert-score/archive/refs/tags/v0.4.1.tar.gz',
+    packages=find_packages(exclude=["*.tests", "*.tests.*",
+                                    "tests.*", "tests"]),
+    install_requires=['torch>=1.0.0',
+                      'numpy',
+                      'pandas>=1.0.1',
+                      'requests',
+                      'tqdm>=4.31.1',
+                      'matplotlib',
+                      'transformers>=3.0.0'
+                      ],
+    entry_points={
+        'console_scripts': [
+            "code-bert-score=code_bert_score_cli.score:main",
+            "code-bert-score-show=code_bert_score_cli.visualize:main",
+        ]
+    },
+    include_package_data=True,
+    python_requires='>=3.6',
+    tests_require=['pytest'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+
+)
