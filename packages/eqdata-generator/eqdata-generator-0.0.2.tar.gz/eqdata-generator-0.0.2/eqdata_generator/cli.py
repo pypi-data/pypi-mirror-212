8994c06f417208f5a0872cd9910720a8d25e41f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:42 +0000

    Add package eqdata-generator/0.0.2/eqdata-generator-0.0.2.tar.gz

---
+++ packages/eqdata-generator/eqdata-generator-0.0.2.tar.gz/eqdata-generator-0.0.2/eqdata_generator/cli.py
@@ -0,0 +1,23 @@
+import click
+import numpy as np
+
+# @click.command()
+# @click.option('--count',default=1,help='number of greetings.')
+# @click.option('--name',prompt='your name', help='the person to greet')
+# def cli(count,name):
+#     for x in range(count):
+#         click.echo(f"hello {name}")
+
+@click.command()
+@click.option('--min', default=10,help='lower bound of the interval')
+@click.option('--max', default=50,help='upper bound of the interval')
+@click.option('--count',default=1000,help='number of counts')
+@click.option('--dist',default='uniform',help='type of distribution')
+def cli(min,max,count,dist):
+    if dist=='uniform':
+        click.echo(np.random.uniform(min,max,count))
+    else:
+        click.echo("I can only do uniform distribution lol")
+
+# cli()
+    
