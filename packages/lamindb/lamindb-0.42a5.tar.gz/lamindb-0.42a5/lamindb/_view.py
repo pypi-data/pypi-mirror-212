6a456d362937cd24100eeb4ae50ab67b85c682ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package lamindb/0.42a5/lamindb-0.42a5.tar.gz

---
+++ packages/lamindb/lamindb-0.42a5.tar.gz/lamindb-0.42a5/lamindb/_view.py
@@ -0,0 +1,43 @@
+import importlib
+from typing import Optional
+
+from IPython.display import display
+from lamin_logger import colors
+from lamindb_setup import settings
+from lamindb_setup.dev._setup_schema import get_schema_module_name
+
+from ._select import select
+
+
+def view(n: int = 10, schema: Optional[str] = None):
+    """View data.
+
+    Args:
+        n: Display the latest n rows of the tables.
+        schema: Schema module to view. Default's to `None` and
+            displays all schema modules.
+    """
+    if schema is not None:
+        schema_names = [schema]
+    else:
+        schema_names = ["core"] + list(settings.instance.schema)
+
+    for schema_name in schema_names:
+        schema_module = importlib.import_module(get_schema_module_name(schema_name))
+
+        tables = [
+            i
+            for i in schema_module.__dict__.values()
+            if i.__class__.__name__ == "SQLModelMetaclass" and hasattr(i, "__table__")
+        ]
+        if len(schema_names) > 1:
+            section = f"* module: {colors.green(colors.bold(schema_name))} *"
+            section_no_color = f"* module: {schema_name} *"
+            print("*" * len(section_no_color))
+            print(section)
+            print("*" * len(section_no_color))
+        for entity in tables:
+            df = select(entity).df()
+            if df.shape[0] > 0:
+                print(colors.blue(colors.bold(entity.__name__)))
+                display(df.iloc[-n:])
---
+++ packages/lamindb/lamindb-0.42a5.tar.gz/lamindb-0.42a5/sub/lamindb-setup/lamindb_setup/_delete.py
@@ -0,0 +1,58 @@
+import shutil
+from pathlib import Path
+
+from lamin_logger import logger
+
+from ._settings import settings
+from .dev._settings_load import load_instance_settings
+from .dev._settings_store import current_instance_settings_file, instance_settings_file
+
+
+def delete(instance_name: str):
+    """Delete an instance."""
+    instance_identifier = f"{settings.user.handle}/{instance_name}"
+    logger.info(f"Deleting instance {instance_identifier}")
+    settings_file = instance_settings_file(instance_name, settings.user.handle)
+    if not settings_file.exists():
+        logger.warning(
+            "Could not delete as instance settings do not exist locally. Did you"
+            " provide a wrong instance name? Could you try loading it?"
+        )
+        return None
+    isettings = load_instance_settings(settings_file)
+
+    delete_settings(settings_file)
+    if settings._instance_exists:
+        if instance_identifier == settings.instance.identifier:
+            current_settings_file = current_instance_settings_file()
+            logger.info(
+                f"    current instance settings {current_settings_file} deleted"
+            )
+            current_settings_file.unlink()
+            settings._instance_settings = None
+    delete_cache(isettings.storage.cache_dir)
+    logger.info(
+        f"    consider deleting your stored data manually: {isettings.storage.root}"
+    )
+    if isettings.dialect == "sqlite":
+        if isettings._sqlite_file.exists():
+            isettings._sqlite_file.unlink()
+            logger.info("    deleted '.lndb' sqlite file")
+        else:
+            logger.info("    '.lndb' sqlite file does not exist")
+    if isettings.is_remote:
+        logger.info("    please manually delete your remote instance on lamin.ai")
+
+
+def delete_cache(cache_dir: Path):
+    if cache_dir is not None and cache_dir.exists():
+        shutil.rmtree(cache_dir)
+        logger.info("    instance cache deleted")
+
+
+def delete_settings(settings_file: Path):
+    if settings_file.exists():
+        settings_file.unlink()
+        logger.info("    instance settings '.env' deleted")
+    else:
+        logger.info("    instance settings '.env' do not exist locally")
