f815db6671c30b4b52e509663d290425505d0532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:40 +0000

    Add package hybrid-graph/0.3/hybrid_graph-0.3-py3-none-any.whl

---
+++ packages/hybrid-graph/hybrid_graph-0.3-py3-none-any.whl/hg/hybrid_graph/models/__init__.py
@@ -0,0 +1,27 @@
+from .gnn.toynet import ToyNet
+from .gnn.baseline import GCNNet, SAGENet
+from .gnn.hybrid import HybridGCN, HybridSAGE
+from .gnn.gat import GATNet, GATV2Net
+from .gnn.hyper import HyperGCN, HyperGAT
+from .gnn.linearprobe import LPGCNHyperGCN, CATTGCNHyperGCN,LPGGATGCN, LPGATHyperGCN, LPHYPERHYPER, LPGCNGCN, LPGGATGAT
+from .ensemble.average_prediction import Average_Ensemble
+
+factory = {
+    'toynet': ToyNet,
+    'gcn': GCNNet,
+    'sage': SAGENet,
+    'gat': GATNet,
+    'gatv2': GATV2Net,
+    'hybrid-gcn': HybridGCN,
+    'hybrid-sage': HybridSAGE,
+    'hyper-gcn': HyperGCN,
+    'hyper-gat': HyperGAT,
+    'ensemble': Average_Ensemble,
+    'lp-gcn-hyper-gcn': LPGCNHyperGCN,
+    'catt-gcn-hyper-gcn': CATTGCNHyperGCN,
+    'lp-gat-hyper-gcn': LPGATHyperGCN,
+    'lp-gat-gcn': LPGGATGCN,
+    'lp-gcn-gcn': LPGCNGCN,
+    'lp-gat-gat': LPGGATGAT,
+    'lp-hyper-hyper': LPHYPERHYPER,
+}
