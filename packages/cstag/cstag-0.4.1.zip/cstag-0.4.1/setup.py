226ee1a96143902d4d5a96a6617d7eeb00667231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:51 +0000

    Add package cstag/0.4.1/cstag-0.4.1.zip

---
+++ packages/cstag/cstag-0.4.1.zip/cstag-0.4.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="cstag",
+    version="0.4.1",
+    author="Akihiro Kuno",
+    author_email="akuno@md.tsukuba.ac.jp",
+    description="Python module to manipulate the minimap2's CS tag",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akikuno/cstag",
+    packages=setuptools.find_packages(
+        where="src",
+    ),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
---
+++ packages/cstag/cstag-0.4.1.zip/cstag-0.4.1/src/cstag/__init__.py
@@ -0,0 +1,11 @@
+"""
+.. include:: ../../README.md
+"""
+
+from .shorten import shorten
+from .lengthen import lengthen
+from .consensus import consensus
+from .to_html import to_html
+from .mask import mask
+
+
