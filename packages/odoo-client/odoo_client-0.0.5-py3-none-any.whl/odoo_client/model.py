dbe88d885d164d7ed147b95b092dab0ffc309edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:08 +0000

    Add package odoo-client/0.0.5/odoo_client-0.0.5-py3-none-any.whl

---
+++ packages/odoo-client/odoo_client-0.0.5-py3-none-any.whl/odoo_client/model.py
@@ -0,0 +1,47 @@
+from xmlrpc.client import ServerProxy
+
+
+class OdooModel:
+    def __init__(self,
+                 url: str,
+                 user_id: int,
+                 database: str,
+                 password: str):
+        self.url = url
+        self.user_id = user_id
+        self.database = database
+        self.password = password
+        self.models = ServerProxy(f"{self.url}/xmlrpc/2/object")
+
+    def create_model(self,
+                     name: str) -> int:
+        payload = {
+            "name": name,
+            "model": f"x_{'_'.join(name.lower().split(' '))}",
+            "state": "manual"
+        }
+        return self.models.execute_kw(self.database,
+                                      self.user_id,
+                                      self.password,
+                                      "ir.model",
+                                      "create",
+                                      payload)
+
+    def create_model_field(self,
+                           model: int,
+                           name: str,
+                           field_type: str,
+                           required: bool) -> int:
+        payload = {
+            "model_id": model,
+            "name": f"x_{name}",
+            "ttype": field_type,
+            "state": "manual",
+            "required": required
+        }
+        return self.models.execute_kw(self.database,
+                                      self.user_id,
+                                      self.password,
+                                      "ir.model.fields",
+                                      "create",
+                                      payload)
