b85108444a119b510f7a2a12057cc1b82f9af258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:46 +0000

    Add package datagen-validation-protocol/0.34.0/datagen_validation_protocol-0.34.0.tar.gz

---
+++ packages/datagen-validation-protocol/datagen_validation_protocol-0.34.0.tar.gz/datagen_validation_protocol-0.34.0/datagen_protocol/schema/humans/__init__.py
@@ -0,0 +1,23 @@
+from datagen_protocol.schema.humans.accessories import (
+    Accessories,
+    Color,
+    Glasses,
+    GlassesPosition,
+    Mask,
+    MaskPosition,
+    MaskTexture,
+)
+from datagen_protocol.schema.humans.human import (
+    Expression,
+    ExpressionName,
+    Eyebrows,
+    Eyes,
+    FacialHair,
+    Gaze,
+    Hair,
+    HairColor,
+    Head,
+    Human,
+    HumanDatapoint,
+    Outfit,
+)
