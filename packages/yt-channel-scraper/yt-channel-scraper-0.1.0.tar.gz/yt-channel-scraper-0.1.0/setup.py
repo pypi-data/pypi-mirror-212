3efc922c96d3becfdcc4bb5f99abb8de6a8ce9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:01 +0000

    Add package yt-channel-scraper/0.1.0/yt-channel-scraper-0.1.0.tar.gz

---
+++ packages/yt-channel-scraper/yt-channel-scraper-0.1.0.tar.gz/yt-channel-scraper-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from pathlib import Path
+
+readme_file = Path(__file__).resolve().parent / "README.md"
+with open(readme_file, encoding="utf-8") as f:
+    readme_content = f.read()
+
+setup(
+    name="yt-channel-scraper",
+    version="0.1.0",
+    description="Fetch channel_id, videos, transcript from a youtube channel",
+    long_description=readme_content,
+    long_description_content_type="text/markdown",
+    author="Priyanshu Panwar",
+    author_email="priyanshu009ch@gmail.com",
+    packages=["yt_channel_scraper"],
+    project_urls={
+        "Source": "https://github.com/priyanshu-panwar/yt-channel-scraper",
+    },
+    install_requires=[
+        "scrapetube",
+        "youtube-transcript-api",
+        "requests",
+        "beautifulsoup4",
+    ],
+)
