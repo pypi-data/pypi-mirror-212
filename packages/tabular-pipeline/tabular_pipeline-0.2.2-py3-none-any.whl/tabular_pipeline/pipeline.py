7d4140e98c1f80cb79c9f6ed7a2fc186390f2e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:08 +0000

    Add package tabular-pipeline/0.2.2/tabular_pipeline-0.2.2-py3-none-any.whl

---
+++ packages/tabular-pipeline/tabular_pipeline-0.2.2-py3-none-any.whl/tabular_pipeline/pipeline.py
@@ -0,0 +1,27 @@
+import uuid
+
+from .steps.conform import Conform
+from .steps.load import Load
+from .read import load_schema
+
+
+def standardise(
+    schema: str,
+    file_path: str | None = None,
+    file_name: str | None = None,
+    file_content: bytes | None = None,
+):
+    session_id = uuid.uuid4()
+    schema = load_schema(session_id, schema)
+    loader = Load(
+        session_id=session_id,
+        file_path=file_path,
+        file_name=file_name,
+        file_content=file_content,
+    )
+    conformer = Conform(session_id=session_id, schema=schema)
+    # TODO - normaliser, validator
+
+    loader.run()
+    conformer.run()
+    return conformer.dataset.export("xlsx")
