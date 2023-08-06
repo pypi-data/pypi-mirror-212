f0e0b8c1160d273092cdf1b7096a7ca990dbe02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:14 +0000

    Add package yaoys-checkin/0.1.7/yaoys_checkin-0.1.7-py3-none-any.whl

---
+++ packages/yaoys-checkin/yaoys_checkin-0.1.7-py3-none-any.whl/yaoys_checkin/cx_freeze_setup.py
@@ -0,0 +1,30 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# 要打包的Python脚本路径
+script = "AutoCheckIn.py"
+
+# 创建可执行文件的配置
+exe = Executable(
+    script=script,
+    base=None,
+    targetName="AutoCheckIn"  # 生成的可执行文件名称
+)
+
+# 打包的参数配置
+options = {
+    "build_exe": {
+        "packages": ['apscheduler'],
+        "excludes": [],
+        'include_files': ['D:\\Anaconda3\\envs\\autocheckin\\Lib\\site-packages\\sklearn\\.libs\\vcomp140.dll',
+                          'D:\\Anaconda3\\envs\\autocheckin\\Lib\\site-packages\\pyzmq.libs\\msvcp140.dll']
+    }
+}
+
+setup(
+    name="AutoCheckIn",
+    version="0.1.3",
+    description="AutoCheckIn",
+    options=options,
+    executables=[exe]
+)
