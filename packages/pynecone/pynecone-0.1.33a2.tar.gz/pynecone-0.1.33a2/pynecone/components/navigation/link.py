6925d3e29e2160f90980468548b57b3c9ac9fce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:30 +0000

    Add package pynecone/0.1.33a2/pynecone-0.1.33a2.tar.gz

---
+++ packages/pynecone/pynecone-0.1.33a2.tar.gz/pynecone-0.1.33a2/pynecone/components/navigation/link.py
@@ -0,0 +1,30 @@
+"""A link component."""
+
+from pynecone.components.libs.chakra import ChakraComponent
+from pynecone.components.navigation.nextlink import NextLink
+from pynecone.utils import imports
+from pynecone.vars import BaseVar, Var
+
+
+class Link(ChakraComponent):
+    """Link to another page."""
+
+    tag = "Link"
+
+    # The rel.
+    rel: Var[str]
+
+    # The page to link to.
+    href: Var[str]
+
+    # The text to display.
+    text: Var[str]
+
+    # What the link renders to.
+    as_: Var[str] = BaseVar.create("{NextLink}", is_local=False)  # type: ignore
+
+    # If true, the link will open in new tab.
+    is_external: Var[bool]
+
+    def _get_imports(self) -> imports.ImportDict:
+        return {**super()._get_imports(), **NextLink.create()._get_imports()}
