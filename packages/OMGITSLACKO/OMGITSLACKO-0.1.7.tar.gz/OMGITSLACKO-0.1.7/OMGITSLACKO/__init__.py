bffc0745f7865e3e799e7bc476a232047437dd6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:09 +0000

    Add package OMGITSLACKO/0.1.7/OMGITSLACKO-0.1.7.tar.gz

---
+++ packages/OMGITSLACKO/OMGITSLACKO-0.1.7.tar.gz/OMGITSLACKO-0.1.7/OMGITSLACKO/__init__.py
@@ -0,0 +1,17 @@
+from .functions import correlation
+from .functions import find_best_model_classification
+from .functions import find_best_model_regression
+from .functions import find_best_clustering_algorithm
+from .functions import find_best_model_regression_2
+from .functions import tune_MLPc_parameters
+from .functions import tune_MLPr_parameters
+from .functions import plot_target_vs_others
+from .functions import plot_target_vs_others_normalized
+from .functions import plot_side_by_side
+
+from .tozsdefunctions import fetch_all_currencies
+from .tozsdefunctions import fetch_all_indexes
+from .tozsdefunctions import fetch_all_futures
+from .tozsdefunctions import update_currencies_csv
+from .tozsdefunctions import update_indexes_csv
+from .tozsdefunctions import update_futures_csv
