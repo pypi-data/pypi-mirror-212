f88d2edf6f9c47b16ce41a080aca2413fb5f8bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:52 +0000

    Add package edc-crf/0.3.43/edc-crf-0.3.43.tar.gz

---
+++ packages/edc-crf/edc-crf-0.3.43.tar.gz/edc-crf-0.3.43/edc_crf/model_mixins/crf_model_mixin.py
@@ -0,0 +1,14 @@
+from edc_model.models import HistoricalRecords
+from edc_sites.models import SiteModelMixin
+from edc_visit_tracking.managers import CrfCurrentSiteManager, CrfModelManager
+
+from .crf_no_manager_model_mixin import CrfNoManagerModelMixin
+
+
+class CrfModelMixin(SiteModelMixin, CrfNoManagerModelMixin):
+    on_site = CrfCurrentSiteManager()
+    objects = CrfModelManager()
+    history = HistoricalRecords(inherit=True)
+
+    class Meta(CrfNoManagerModelMixin.Meta):
+        abstract = True
