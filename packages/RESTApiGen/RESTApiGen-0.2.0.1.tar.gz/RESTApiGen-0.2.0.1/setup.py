7d37cbfcaf1b70f025caf9f2f96388683d150e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:34 +0000

    Add package RESTApiGen/0.2.0.1/RESTApiGen-0.2.0.1.tar.gz

---
+++ packages/RESTApiGen/RESTApiGen-0.2.0.1.tar.gz/RESTApiGen-0.2.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+long_description = long_description[22:]
+setup(
+    name="RESTApiGen",
+    version="0.2.0.1",
+    description="REST API auto-generator.",
+    url="https://github.com/Saiyam-J/RESTApiGen",
+    author="Saiyam Jain",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.10",
+    install_requires=[
+        "pymysql >= 1.0.2",
+        "inflect",
+        "flask",
+        "flask-sqlalchemy",
+        "flask-restful",
+        "flask-blueprint",
+        "flask-marshmallow",
+        "marshmallow-sqlalchemy",
+        "cryptography"
+    ],
+    entry_points = {
+        "console_scripts" : [
+            "RESTApiGen = RESTApiGen:main"
+        ]
+    }
+)
