f0286f3a8dd9925b51b5720dac4c3c9419b1462d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:29 +0000

    Add package ask-smapi-model/1.22.0/ask-smapi-model-1.22.0.tar.gz

---
+++ packages/ask-smapi-model/ask-smapi-model-1.22.0.tar.gz/ask-smapi-model-1.22.0/ask_smapi_model/v1/skill/invocations/__init__.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+#
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License'). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
+#
+from __future__ import absolute_import
+
+from .skill_execution_info import SkillExecutionInfo
+from .invocation_response_status import InvocationResponseStatus
+from .invoke_skill_request import InvokeSkillRequest
+from .request import Request
+from .skill_request import SkillRequest
+from .response import Response
+from .invocation_response_result import InvocationResponseResult
+from .invoke_skill_response import InvokeSkillResponse
+from .end_point_regions import EndPointRegions
+from .metrics import Metrics
