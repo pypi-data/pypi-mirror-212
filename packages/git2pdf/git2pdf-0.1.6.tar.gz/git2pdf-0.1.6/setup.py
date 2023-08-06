01c6027f640022d980b8df35d5c93fcc952f6c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:53 +0000

    Add package git2pdf/0.1.6/git2pdf-0.1.6.tar.gz

---
+++ packages/git2pdf/git2pdf-0.1.6.tar.gz/git2pdf-0.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='git2pdf',
+    version='0.1.6',
+    packages=['git2pdf'],
+    package_data={'git2pdf': ['*.ttf']},
+    install_requires=[
+        'requests',
+        'fpdf2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'git2pdf=git2pdf.main:main',
+        ],
+    },
+)
+
