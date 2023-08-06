71cb8752d080f0dbe3bfbac9c60b06a58924c0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:41 +0000

    Add package euporie/2.4.2/euporie-2.4.2.tar.gz

---
+++ packages/euporie/euporie-2.4.2.tar.gz/euporie-2.4.2/euporie/core/convert/__init__.py
@@ -0,0 +1,29 @@
+"""Sub-module concerned with the conversion of data formats."""
+
+from euporie.core.convert.formats import (
+    ansi,
+    base64,
+    formatted_text,
+    html,
+    jpeg,
+    markdown,
+    pdf,
+    pil,
+    png,
+    rich,
+    sixel,
+)
+
+__all__ = [
+    "ansi",
+    "base64",
+    "jpeg",
+    "html",
+    "markdown",
+    "formatted_text",
+    "pdf",
+    "pil",
+    "png",
+    "rich",
+    "sixel",
+]
---
+++ packages/euporie/euporie-2.4.2.tar.gz/euporie-2.4.2/euporie/core/key_binding/bindings/__init__.py
@@ -0,0 +1,5 @@
+"""Define collections of generic key-bindings which do not belong to widgets."""
+
+from euporie.core.key_binding.bindings import completion, micro
+
+__all__ = ["completion", "micro"]
