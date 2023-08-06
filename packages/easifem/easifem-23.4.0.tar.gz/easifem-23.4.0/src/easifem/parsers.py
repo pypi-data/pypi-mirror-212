66111758a59ba4a83f2e71ed2f559019d3cfd792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:18 +0000

    Add package easifem/23.4.0/easifem-23.4.0.tar.gz

---
+++ packages/easifem/easifem-23.4.0.tar.gz/easifem-23.4.0/src/easifem/parsers.py
@@ -0,0 +1,9 @@
+import easifem.run
+import easifem.test
+import easifem.setenv
+import easifem.install
+
+testParser = easifem.test.parser
+runParser = easifem.run.parser
+setenvParser = easifem.setenv.parser
+installParser = easifem.install.parser
