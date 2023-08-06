5eb392be2637fb8422235d3ce23b77ebbd02de17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:15 +0000

    Add package molimg/0.2.0/molimg-0.2.0.tar.gz

---
+++ packages/molimg/molimg-0.2.0.tar.gz/molimg-0.2.0/README.md
@@ -0,0 +1,43 @@
+# Molecular Imager
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Tests](https://github.com/jdkern11/molimg/workflows/tests/badge.svg)](https://github.com/jdkern11/molimg/actions?workflow=tests)
+[![codecov](https://codecov.io/gh/jdkern11/molimg/branch/main/graph/badge.svg?token=4MU1H8MD94)](https://codecov.io/gh/jdkern11/molimg)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/python/black)
+[![version](https://img.shields.io/badge/Release-0.2.0-blue)](https://github.com/jdkern11/molimg/releases)
+
+Do you ever wish you could easily embed the images of your smiles strings into 
+an excel sheet? Wish no more! molimg is here to do just that!
+
+Take the following data in a csv:
+
+![image of example data](https://raw.githubusercontent.com/jdkern11/molimg/main/images/example_csv.png)
+
+and molimg will convert it like so:
+
+![image of example data](https://raw.githubusercontent.com/jdkern11/molimg/main/images/example_csv_with_images.png)
+
+## Usage
+First, import the data into a pandas dataframe, then pass this dataframe, the 
+columns that you want to convert to images, and the save name of the file to the 
+package:
+
+```Python
+import pandas as pd
+from molimg import excel
+
+df = pd.read_csv('example_data.csv')
+smiles_columns = ['smiles1', 'smiles2']
+excel.write(
+    df=df, 
+    smiles_columns=smiles_columns, 
+    filename='example_data_with_images.xlsx'
+)
+```
+
+The order the columns appear in df.columns is how the columns will be saved in
+the new excel sheet. The new smiles columns with images will always appear to the right
+of the data they originate from with `{original_column}_image` as the new column name.
+
+Any error that occurs when trying to convert a smiles string to an image 
+will appear as a warning log message and the image will not be produced. The excel sheet
+will still be created with the smiles strings that work.
---
+++ packages/molimg/molimg-0.2.0.tar.gz/molimg-0.2.0/molimg/excel.py
@@ -0,0 +1,85 @@
+import shutil
+import uuid
+import logging
+from pathlib import Path
+
+import pandas as pd
+import xlsxwriter
+
+from molimg.helpers import remove_extensions
+from molimg.draw import df_columns_of_smiles_to_pngs
+
+logger = logging.getLogger(__name__)
+
+
+def write(df: pd.DataFrame, smiles_columns: list[str], filename: str):
+    """Writes dataframe to excel file after converting smiles in smiles_columns to images
+
+    Images are embedded in separate columns in the excel file.
+
+    Args:
+        df:
+            Dataframe to save data in
+        smiles_columns:
+            List of columns in the dataframe that contain the smiles
+            strings. These smiles will be converted to images and the images will
+            be embedded in the excel file in a new column.
+        filename:
+            Name of the saved excel files. Extensions will be removed
+            (filename.extension) and filename will be saved as "filename.xlsx".
+    """
+    logger.debug(f"Converting smiles in {smiles_columns} to images")
+    filename = remove_extensions(filename)
+    folder_name = str(uuid.uuid4())
+    # make sure folder doesn't already exist
+    while Path(folder_name).exists():
+        folder_name = str(uuid.uuid4())
+    
+    # record columns before more added
+    columns = df.columns
+    # add uuid for each smiles column so names are unique and not invalid
+    for col in smiles_columns:
+        data = set()
+        for index, row in df.iterrows():
+            name = uuid.uuid4()
+            while name in data:
+                name = uuid.uuid4()
+            data.add(str(name))
+        df[f"{col}_image"] = list(data)
+
+    df_columns_of_smiles_to_pngs(df, smiles_columns, folder_name)
+
+    workbook = xlsxwriter.Workbook(f"{filename}.xlsx")
+    worksheet = workbook.add_worksheet()
+
+    r, c = 0, 0
+    # use previously recorded columns
+    for column in columns:
+        worksheet.write(r, c, column)
+        for index, row in df.iterrows():
+            r += 1
+            if pd.isna(row[column]):
+                worksheet.write_blank(r, c, None)
+            else:
+                worksheet.write(r, c, row[column])
+
+        # image column should come next if column is in smiles column
+        if column in smiles_columns:
+            r = 0
+            c += 1
+            image_column = f"{column}_image"
+            worksheet.write(r, c, image_column)
+            worksheet.set_column_pixels(c, c, 205)
+            for index, row in df.iterrows():
+                r += 1
+                filepath = Path(folder_name) / column / f"{row[image_column]}.png"
+                if filepath.exists():
+                    worksheet.insert_image(
+                        r, c, str(filepath), {"x_offset": 2.5, "y_offset": 2.5}
+                    )
+                    worksheet.set_row_pixels(r, 205)
+        c += 1
+        r = 0
+
+    workbook.close()
+    shutil.rmtree(folder_name)
