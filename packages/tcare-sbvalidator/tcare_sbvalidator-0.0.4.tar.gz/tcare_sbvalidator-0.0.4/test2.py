1b46714a7b7fa720af1c907b01187c10d4809e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:20 +0000

    Add package tcare-sbvalidator/0.0.4/tcare_sbvalidator-0.0.4.tar.gz

---
+++ packages/tcare-sbvalidator/tcare_sbvalidator-0.0.4.tar.gz/tcare_sbvalidator-0.0.4/test2.py
@@ -0,0 +1,12 @@
+import json
+from tcare_sbv import sb_validator
+
+msg = {
+    "recipient_number": "123456",
+    "message": "hello",
+    "message_type": "yo"
+}
+
+msg2 = json.dumps(msg)
+
+print(sb_validator.sms(msg2))
