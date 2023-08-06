0997f9a8c88e761fbf6c9af524276787f794d851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:24 +0000

    Add package lnschema_bionty/0.19a4/lnschema_bionty-0.19a4-py2.py3-none-any.whl

---
+++ packages/lnschema_bionty/lnschema_bionty-0.19a4-py2.py3-none-any.whl/lnschema_bionty/__init__.py
@@ -0,0 +1,58 @@
+"""Biological entities (`zdno`).
+
+Import the package::
+
+   import lnschema_bionty
+
+Biological entities, all initialized via `Bionty <https://lamin.ai/docs/bionty>`__:
+
+.. autosummary::
+   :toctree: .
+
+   Gene
+   Protein
+   CellMarker
+   CellType
+   CellLine
+   Species
+   Tissue
+   Disease
+   Pathway
+   Phenotype
+   Readout
+
+
+Bionty sources:
+
+.. autosummary::
+   :toctree: .
+
+   BiontySource
+"""
+# This is lnschema-module zdno.
+_schema_id = "zdno"
+_name = "bionty"
+__version__ = "0.19a4"  # Denote a release candidate of version 0.1.0 with 0.1rc1
+
+
+from lamindb_setup._check_instance_setup import (
+    check_instance_setup as _check_instance_setup,
+)
+
+_INSTANCE_SETUP = _check_instance_setup()
+
+if _INSTANCE_SETUP:
+    from .models import (  # noqa
+        BiontySource,
+        CellLine,
+        CellMarker,
+        CellType,
+        Disease,
+        Gene,
+        Pathway,
+        Phenotype,
+        Protein,
+        Readout,
+        Species,
+        Tissue,
+    )
