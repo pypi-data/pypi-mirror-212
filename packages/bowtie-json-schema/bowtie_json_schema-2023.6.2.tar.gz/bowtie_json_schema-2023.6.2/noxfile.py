74a3615f203cfd00f723121d8b492b417c02f0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:45 +0000

    Add package bowtie-json-schema/2023.6.2/bowtie_json_schema-2023.6.2.tar.gz

---
+++ packages/bowtie-json-schema/bowtie_json_schema-2023.6.2.tar.gz/bowtie_json_schema-2023.6.2/noxfile.py
@@ -0,0 +1,202 @@
+from pathlib import Path
+import shlex
+
+import nox
+
+ROOT = Path(__file__).parent
+PYPROJECT = ROOT / "pyproject.toml"
+DOCS = ROOT / "docs"
+BOWTIE = ROOT / "bowtie"
+IMPLEMENTATIONS = ROOT / "implementations"
+TESTS = ROOT / "tests"
+
+
+nox.options.sessions = []
+
+
+def session(default=True, **kwargs):
+    def _session(fn):
+        if default:
+            nox.options.sessions.append(kwargs.get("name", fn.__name__))
+        return nox.session(**kwargs)(fn)
+
+    return _session
+
+
+@session(python=["3.8", "3.9", "3.10", "3.11"])
+def tests(session):
+    session.install("-r", ROOT / "test-requirements.txt")
+    session.run("pytest", "--verbosity=3")
+
+
+@session(tags=["build"])
+def build(session):
+    session.install("build")
+    tmpdir = session.create_tmp()
+    session.run("python", "-m", "build", ROOT, "--outdir", tmpdir)
+
+
+@session(tags=["build"])
+def shiv(session):
+    session.install("shiv")
+    if session.posargs:
+        out = session.posargs[0]
+    else:
+        tmpdir = Path(session.create_tmp())
+        out = tmpdir / "bowtie"
+    session.run(
+        "python",
+        "-m",
+        "shiv",
+        "--reproducible",
+        "-c",
+        "bowtie",
+        ROOT,
+        "-o",
+        out,
+    )
+    print(f"Outputted a shiv to {out}.")
+
+
+@session(tags=["style"])
+def readme(session):
+    session.install("build", "twine")
+    tmpdir = session.create_tmp()
+    session.run("python", "-m", "build", ROOT, "--outdir", tmpdir)
+    session.run("python", "-m", "twine", "check", "--strict", tmpdir + "/*")
+
+
+@session(tags=["style"])
+def style(session):
+    session.install("ruff")
+    session.run("python", "-m", "ruff", "check", BOWTIE, TESTS, __file__)
+
+
+@session()
+def typing(session):
+    session.install("pyright", ROOT)
+    session.run("pyright", BOWTIE)
+
+
+@session(tags=["docs"])
+@nox.parametrize(
+    "builder",
+    [
+        nox.param(name, id=name)
+        for name in [
+            "dirhtml",
+            "doctest",
+            "linkcheck",
+            "man",
+            "spelling",
+        ]
+    ],
+)
+def docs(session, builder):
+    session.install("-r", DOCS / "requirements.txt")
+    tmpdir = Path(session.create_tmp())
+    argv = ["-n", "-T", "-W"]
+    if builder != "spelling":
+        argv += ["-q"]
+    session.run(
+        "python",
+        "-m",
+        "sphinx",
+        "-b",
+        builder,
+        DOCS,
+        tmpdir / builder,
+        *argv,
+    )
+
+
+@session(tags=["docs", "style"], name="docs(style)")
+def docs_style(session):
+    session.install(
+        "doc8",
+        "pygments",
+        "pygments-github-lexers",
+    )
+    session.run("python", "-m", "doc8", "--config", PYPROJECT, DOCS)
+
+
+@session(default=False, tags=["perf"], name="bench(info)")
+def bench_info(session):
+    """
+    Time how long ``bowtie info`` takes to run (effectively startup time).
+    """
+    session.install(ROOT)
+    executable = Path(session.bin) / "bowtie"
+
+    cmd = f"{executable} info -i {{implementation}}"
+
+    if session.posargs:
+        args = session.posargs
+    else:
+        args = [
+            "--warmup",
+            "3",
+            "-L",
+            "implementation",
+            ",".join(p.name for p in IMPLEMENTATIONS.iterdir() if p.is_dir()),
+        ]
+
+    session.run("hyperfine", *args, cmd, external=True)
+
+
+@session(default=False, tags=["perf"], name="bench(smoke)")
+def bench_smoke(session):
+    """
+    Time how long ``bowtie smoke`` takes to run (startup + ~2 simple examples).
+    """
+    session.install(ROOT)
+    executable = Path(session.bin) / "bowtie"
+
+    cmd = f"{executable} smoke -i {{implementation}}"
+
+    if session.posargs:
+        args = session.posargs
+    else:
+        args = [
+            "--warmup",
+            "3",
+            "-L",
+            "implementation",
+            ",".join(p.name for p in IMPLEMENTATIONS.iterdir() if p.is_dir()),
+        ]
+
+    session.run("hyperfine", *args, cmd, external=True)
+
+
+@session(default=False, tags=["perf"], name="bench(suite)")
+def bench_suite(session):
+    if not session.posargs:
+        session.error("Provide a test suite to benchmark")
+
+    session.install(ROOT)
+    bowtie = Path(session.bin) / "bowtie"
+
+    posargs = shlex.join(session.posargs)
+    if "-i" not in session.posargs:
+        args = [
+            "-L",
+            "implementation",
+            ",".join(p.name for p in IMPLEMENTATIONS.iterdir() if p.is_dir()),
+            f"{bowtie} suite -i {{implementation}} {posargs}",
+        ]
+    else:
+        args = [f"{bowtie} suite {posargs}"]
+    session.run("hyperfine", "--warmup", "1", *args, external=True)
+
+
+@session(default=False)
+def requirements(session):
+    session.install("pip-tools")
+    for each in [DOCS / "requirements.in", ROOT / "test-requirements.in"]:
+        session.run(
+            "pip-compile",
+            "--resolver",
+            "backtracking",
+            "-U",
+            each.relative_to(ROOT),
+        )
