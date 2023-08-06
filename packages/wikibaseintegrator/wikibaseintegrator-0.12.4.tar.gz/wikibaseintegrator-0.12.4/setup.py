065fb10364f41fdb93a0e23b8500064c1e5a4676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:50 +0000

    Add package wikibaseintegrator/0.12.4/wikibaseintegrator-0.12.4.tar.gz

---
+++ packages/wikibaseintegrator/wikibaseintegrator-0.12.4.tar.gz/wikibaseintegrator-0.12.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+# https://github.com/pallets/flask/blob/main/setup.py
+setup(
+    name="wikibaseintegrator",
+    install_requires=[
+        "backoff >= 1.11.1,< 2.3.0",
+        "mwoauth ~= 0.3.8",
+        "oauthlib ~= 3.2.0",
+        "requests >= 2.27.1,< 2.32.0",
+        "requests-oauthlib ~= 1.3.1",
+        "ujson >= 5.4,< 5.8"
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pylint",
+            "pylint-exit",
+            "mypy",
+            "codespell",
+            "flynt"
+        ],
+        "docs": [
+            "Sphinx >= 4.5,< 7.1",
+            "readthedocs-sphinx-ext >= 2.1.5,< 2.3.0",
+            "sphinx-rtd-theme >= 1.0,< 1.3",
+            "sphinx_github_changelog ~= 1.2.0",
+            "m2r2 ~= 0.3.2",
+            "sphinx-autodoc-typehints >= 1.18.1,< 1.24.0"
+        ],
+        "notebooks": ["jupyter"],
+        "coverage": ["pytest-cov"],
+    },
+)
