54ba9cc999a50f3aed03247d5d0b87eb2b09e7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:10 +0000

    Add package SigProfilerMatrixGenerator/1.2.17/SigProfilerMatrixGenerator-1.2.17-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.17-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.17'
+version = '1.2.17'
+Update = 'v1.2.17: Skip hidden files, CliController update, pytesting, and organization.'
+
+	
