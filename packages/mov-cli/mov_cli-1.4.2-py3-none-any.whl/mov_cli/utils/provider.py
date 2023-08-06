6cad38ec85a41ce14f287b6f725c8e45bf8c2ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:55 +0000

    Add package mov-cli/1.4.2/mov_cli-1.4.2-py3-none-any.whl

---
+++ packages/mov-cli/mov_cli-1.4.2-py3-none-any.whl/mov_cli/utils/provider.py
@@ -0,0 +1,52 @@
+from fzf import fzf_prompt
+
+english = ["actvid", "sflix", "solar", "dopebox", "openloadmov", "remotestream"]
+
+german = ["kinox"]
+
+indian = [
+    "streamblasters",
+    "tamilyogi",
+    "einthusan",
+]
+
+asian = [
+    "viewasian",
+    "watchasian",
+]
+
+anime = ["gogoanime", "animefox"]
+
+tv = ["eja"]
+
+cartoons = ["kisscartoon"]
+
+turkish = ["turkish123", "yoturkish"]
+
+sports = ["scdn"]
+
+update = ["pip install mov-cli -U"]
+
+inter = [
+    "wlext",
+]
+
+preselction = {
+    "English Providers": [english],
+    "German Providers": [german],
+    "Indian Providers": [indian],
+    "Asian Providers": [asian],
+    "LIVE TV Providers": [tv],
+    "Cartoons Providers": [cartoons],
+    "Turkish Providers": [turkish],
+    "Anime Providers": [anime],
+    "Sports Providers": [sports],
+    "International Providers": [inter],
+}
+
+
+def ask():
+    init = fzf_prompt(preselction)
+    get = preselction.get(init)[0]
+    choice = fzf_prompt(get)
+    return choice
