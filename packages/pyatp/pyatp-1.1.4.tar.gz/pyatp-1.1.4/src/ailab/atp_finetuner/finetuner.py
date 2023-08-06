dd2f7ff8b5c23e1774ad3fa0cadc3bcabb0c6916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:51 +0000

    Add package pyatp/1.1.4/pyatp-1.1.4.tar.gz

---
+++ packages/pyatp/pyatp-1.1.4.tar.gz/pyatp-1.1.4/src/ailab/atp_finetuner/finetuner.py
@@ -0,0 +1,32 @@
+from typing import Callable
+from ailab.atp_dataset.dataset import AILabDataset
+from ailab.atp_finetuner.constant import Framework, Task, Model
+from ailab.atp_finetuner.preprossor import AILabPreprocessor
+from ailab.atp_finetuner.model import AILabModel
+from ailab.atp_finetuner.datacollator import AILabDataCollator
+from ailab.atp_finetuner.metric import AILabMetric
+from ailab.atp_finetuner.trainer import AILabTrainer
+from ailab.atp_finetuner.accelerator import AILabAccelerator
+
+class AILabFinetuner:
+    def __init__(self, task: Task, framework: Framework.Pytorch, \
+                 dataset: AILabDataset, model_name: Model, train_progress:Callable, **args):
+        accelerator = AILabAccelerator.from_project_config(**args)
+        preprocessor = AILabPreprocessor.from_pretrained(task, model_name, dataset)
+        preprocessor.accelerator = accelerator
+        tokenized_dataset = preprocessor.process_data()
+
+        model = AILabModel.from_pretrained('cuda', task, model_name, **args)
+        model.accelerator = accelerator
+        model.forward()
+
+        data_collator = AILabDataCollator.from_task_model(task, model_name, framework, preprocessor)
+        metrics = AILabMetric.from_task_model(task, model_name)
+        trainer = AILabTrainer.from_task_model(task, model_name, tokenized_dataset, model, preprocessor, data_collator,
+                                               metrics, train_progress, **args)
+        trainer.accelerator = accelerator
+
+        self._trainer = trainer
+
+    def finetuner(self):
+        self._trainer.train()
