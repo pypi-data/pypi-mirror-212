d024fe6a61e8fb21ca6c42a7964b43688e8b04af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:26 +0000

    Add package dbt-upsolver/1.5.24/dbt_upsolver-1.5.24-py3-none-any.whl

---
+++ packages/dbt-upsolver/dbt_upsolver-1.5.24-py3-none-any.whl/dbt/include/upsolver/macros/materializations/utils/create_table.sql
@@ -0,0 +1,30 @@
+{% macro get_create_table_if_not_exists_sql(target_relation, partition_by, primary_key, options) -%}
+
+{%- set old_relation = adapter.get_relation(identifier=target_relation.identifier,
+                                            schema=target_relation.schema,
+                                            database=target_relation.database) -%}
+
+  {%- set columns_with_types = adapter.get_columns_names_with_types(partition_by + primary_key) -%}
+  {%- set columns_partitioned_by  = adapter.get_columns_names(partition_by) -%}
+  {%- set columns_primary_key  = adapter.get_columns_names(primary_key) -%}
+  {%- set enriched_options = adapter.enrich_options(options, 'datalake', 'target_options') -%}
+  {%- set enriched_editable_options = adapter.filter_options(enriched_options, 'editable') -%}
+
+  {%- if old_relation -%}
+    ALTER TABLE {{target_relation}}
+    {{ render_options(enriched_editable_options, 'alter') }}
+  {%- else -%}
+    CREATE TABLE {{ target_relation }}
+    ({{ columns_with_types }})
+    {%- if partition_by %}
+    PARTITIONED BY
+      {{ columns_partitioned_by }}
+    {%- endif -%}
+    {%- if primary_key %}
+    PRIMARY KEY
+      {{ columns_primary_key }}
+    {%- endif %}
+    {{ render_options(enriched_options, 'create') }}
+  {%- endif -%}
+
+{%- endmacro %}
