5b9276be695dc0981adb5ccf24816d4a6ccbe3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:26 +0000

    Add package lib-for-messanger/1.0.0/lib_for_messanger-1.0.0.tar.gz

---
+++ packages/lib-for-messanger/lib_for_messanger-1.0.0.tar.gz/lib_for_messanger-1.0.0/lib_for_messanger/file_service.py
@@ -0,0 +1,62 @@
+import json
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
+class FileService:
+    gauth = None
+
+    def __init__(self, file_path, data_list, object_class):
+        if FileService.gauth is None:
+            gauth = GoogleAuth()
+            gauth.LocalWebserverAuth()
+
+        self.__file_name = file_path
+        self.__data_list = data_list
+
+        drive = GoogleDrive(FileService.gauth)
+
+        json_array = []
+        file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+        for file in file_list:
+            if file["title"] == self.__file_name:
+                json_array = json.loads(file.GetContentString())
+                break
+
+        for json_object in json_array:
+            self.__data_list.append(object_class.from_json_object(json_object))
+
+        # path = os.path.join(file_path)
+        #
+        #
+        #
+        #
+        # if not os.path.isfile(path):
+        #     my_file = open(file_path, "w")
+        #     my_file.write("[]")
+        #     my_file.close()
+        # else:
+        #     with open(self.__file_name, "r") as file:
+        #         json_array = json.load(file)
+        #         for json_object in json_array:
+        #             self.__data_list.append(object_class.from_json_object(json_object))
+        #
+        #         print(self.__data_list)
+
+    def save_data(self):
+        drive = GoogleDrive(FileService.gauth)
+        file = drive.CreateFile({"title": f"{self.__file_name}"})
+
+        json_array = []
+        for el in self.__data_list:
+            json_array.append(el.to_json_object())
+        file.SetContentString(json.dumps(json_array))
+        file.Upload()
+
+        # with open(self.__file_name, "w") as file:
+        #     json_array = []
+        #     for el in self.__data_list:
+        #         json_array.append(el.to_json_object())
+        #     # print(json_array)
+        #     json.dump(json_array, file)
