9806efa0d62d2c36cc74f49c16e9ad9406485265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:30 +0000

    Add package PyShortsByAbdus/0.3/PyShortsByAbdus-0.3.tar.gz

---
+++ packages/PyShortsByAbdus/PyShortsByAbdus-0.3.tar.gz/PyShortsByAbdus-0.3/PyShorts/__init__.py
@@ -0,0 +1,5 @@
+from Sub_Functions.funcs import mainfuncs
+functions = mainfuncs()
+
+functions.downloder(url='https://www.wikiwrimo.org/w/images/Example.jpg', name='name', chunk=1024, location=r'C:\Users\Intel\Desktop\No-Reason')
+
