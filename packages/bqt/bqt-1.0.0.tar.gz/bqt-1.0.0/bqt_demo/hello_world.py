c4dec57177e25519ac1c7a62f91e536e36b88830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:52 +0000

    Add package bqt/1.0.0/bqt-1.0.0.tar.gz

---
+++ packages/bqt/bqt-1.0.0.tar.gz/bqt-1.0.0/bqt_demo/hello_world.py
@@ -0,0 +1,44 @@
+"""
+A demonstration script showing how to parent a QDialog to the Blender main window.
+Yes, this could be done with QtWidgets.QMessageBox more easily.
+The point is to show how to parent a window to the Blender application.
+
+from bqt_demo import hello_world
+hello_world.demo()
+"""
+
+import PySide2
+
+Qt = PySide2.QtCore.Qt
+QApplication = PySide2.QtWidgets.QApplication
+QDialog = PySide2.QtWidgets.QDialog
+QHBoxLayout = PySide2.QtWidgets.QHBoxLayout
+QLabel = PySide2.QtWidgets.QLabel
+
+
+class HelloWorldDialog(QDialog):
+    """
+    A sample 'Hello World!' QDialog.
+    """
+
+    def __init__(self, parent):
+        super().__init__(parent, Qt.WindowCloseButtonHint | Qt.WindowSystemMenuHint | Qt.WindowTitleHint)
+
+        self.resize(200, 50)
+        self.setWindowTitle("Qt for Python in Blender")
+
+        lbl_hw = QLabel("Hello World!")
+
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(lbl_hw)
+        self.setLayout(main_layout)
+
+
+def demo():
+    main_window = QApplication.instance().blender_widget
+    dlg = HelloWorldDialog(main_window)
+    dlg.show()
+
+
+if __name__ == "__main__":
+    demo()
