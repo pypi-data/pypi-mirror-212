3ce1dc08adb83996cdd1d0c2edcaf2726255a7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:14 +0000

    Add package tldraw/0.1.1/tldraw-0.1.1.tar.gz

---
+++ packages/tldraw/tldraw-0.1.1.tar.gz/tldraw-0.1.1/tldraw/__init__.py
@@ -0,0 +1,8 @@
+from .tldraw import TldrawWidget
+from .tldraw_matplotlib import TldrawMatplotlib
+
+# import importlib.metadata
+
+# __version__: str = importlib.metadata.version(__name__)
+
+# print(f"Jupyter Tldraw v{__version__}")
