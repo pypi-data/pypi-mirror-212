3cfd5e3f80480dafc4ab87520c927dade2cfbf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:37 +0000

    Add package Kanon4txt/0.0.1/kanon4txt-0.0.1-py3-none-any.whl

---
+++ packages/Kanon4txt/kanon4txt-0.0.1-py3-none-any.whl/Kanon4txt/utils/anonym_utils.py
@@ -0,0 +1,408 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.spatial.distance import pdist, squareform
+import re
+from annoy import AnnoyIndex
+import scipy
+
+from . import nlp_utils
+
+# CountVectorizer is defined only once
+vectorizer = CountVectorizer(ngram_range=(1,1)) # to use bigrams ngram_range=(2,2)
+#                           stop_words='english')
+
+def lemmatize_and_remove_stops(corpus, break_doc=False):
+    """
+    Alternative version to nlp_utils.clean_corpus
+    """
+    ccorpus = []
+    for doc in corpus:
+        cdoc = nlp_utils.cleaning(doc, break_doc)
+        ccorpus.append(cdoc)
+        
+    return ccorpus
+
+def remove_stops(corpus):
+    """
+    remove stop words only
+    """
+    ccorpus = []
+    for doc in corpus:
+        if doc:
+            doc_tokenize = nlp_utils.nlp(doc)
+            #print("doc")
+            #print(doc)
+
+            #print("doc_tokenize")
+            #print(doc_tokenize)
+
+            txt = []
+            for token in doc_tokenize:
+                word = re.sub('\W+', '', str(token)).lower()
+                # print("token",token,"word",word)
+
+                if word and (word not in nlp_utils.stopword_list):
+                    txt.append(str(token)) # onclude all the marks that are not words 
+        
+            clean_doc = ' '.join(txt)
+
+        else:
+            clean_doc = doc
+        ccorpus.append(clean_doc)
+
+    return ccorpus
+
+
+
+        
+    return ccorpus
+
+def get_bow(corpus, break_doc = False, lemmatize = True):
+    """ Vectorizes the corpus using CountVectorizer """
+    if lemmatize:
+        cc = lemmatize_and_remove_stops(corpus, break_doc=break_doc)
+    else:
+        cc = remove_stops(corpus)
+    # print(corpus)
+    # print(cc)
+
+    try:
+        # Vectorizing
+        count_data = vectorizer.fit_transform(cc)
+        voc = vectorizer.get_feature_names_out()
+
+    except Exception as e:
+        print('Could not create a bow:', e)
+        count_data, voc = None, None
+    return count_data, voc
+
+
+def get_anonym_degree(docs = None, vecs = None, min_k = None):
+    """ If K not given, returns the minimal current k and the corresponding documents.
+        If k is given, return the documents with k or less neighbohrs  """
+    
+    if docs is not None:
+        # Lemmatizing the documents
+        count_data, voc = get_bow(docs, break_doc = False,lemmatize = False)
+    elif vecs is not None:
+        count_data = vecs
+    else: # No input docs or vecs
+        print('You must supply documents or vectors')
+        return
+    if count_data is not None:
+        # Converting to array if is sparse
+        if scipy.sparse.issparse(count_data):
+            count_data = count_data.toarray()
+        # Converting any number larger than 1 into 1
+        count_data[count_data > 1] = 1
+        # Counting unique values
+        uniq_arr, uniq_cnt = np.unique(count_data, axis=0, return_counts=True)
+
+        if not min_k:
+            min_k = min(uniq_cnt)
+            # All the unique vectors
+            un_anon = uniq_arr[uniq_cnt <= min_k]
+        else:
+            # All the unique vectors
+            un_anon = uniq_arr[uniq_cnt < min_k]
+            min_k = min(uniq_cnt) # For the return value
+
+        # Getting the unique vectore indeces
+        indeces_list = []
+        for row in un_anon:
+            # Get the similar rows
+            similar_vals = np.where((count_data == (row)).all(axis=1))
+            indeces_list.append(similar_vals[0].tolist())
+    else:  # count_data is None
+        min_k, indeces_list = None, None
+
+    return min_k, indeces_list
+
+
+def get_dist_matrix(sparse_mat, metric='Jaccard'):
+    """
+    Calculates the distance matrix
+    """
+    # Create distance matrixe
+    dist_cond = pdist(sparse_mat.todense(), metric)
+    # Convert a vector-form distance vector to a square-form distance matrix
+    dist_mtrx = squareform(dist_cond)
+    # To prevent self-similarity
+    np.fill_diagonal(dist_mtrx, np.inf)
+
+def create_word_list(doc):
+    # Remove stopwords and lemmatize
+    doc = nlp_utils.cleaning(doc)
+    # Remove duplicates
+    words = list(set(doc.split(' ')))
+    return words
+
+
+def get_diff_and_common(doc1, doc2):
+    """
+    Returns the common and non-common words between two lists
+    """
+    l1 = create_word_list(doc1)
+    l2 = create_word_list(doc2)
+    diff = list(set(l1) - set(l2))
+    comm = list(set(l1).intersection(l2))
+    return diff, comm
+
+
+def get_diff(vecs):
+    """
+    Finds the differences between arrays of 0,1.
+    The input is either a list of lists or a matrix
+    """
+    # Creating a matrix if needed
+    if isinstance(vecs, list):
+        mat = np.vstack(vecs)
+    else: 
+        mat = vecs
+    mat[mat > 0] = 1
+    #nparray = sparse_mat.toarray()
+    xsum = np.sum(mat, axis=0)
+
+    #print('xsum\n', xsum)
+
+    union_x = (xsum > 0).astype('int')
+    inter_x = (xsum == mat.shape[0]).astype('int')
+
+    diff = union_x - inter_x
+    return diff
+
+
+def jaccard_index(u, v):
+    """
+    For vectors of 0 and 1
+    Creadit: JDWarner https://gist.github.com/JDWarner/6730886
+    """
+    u[u > 1] = 1
+    v[v > 1] = 1
+    if np.double(np.bitwise_or(u, v).sum()) != 0:
+        j = np.double(np.bitwise_and(u, v).sum()) / np.double(np.bitwise_or(u, v).sum())
+    else:
+        j = 0
+
+    return j
+
+
+def build_annoy_search_tree(n_list):
+    """
+    Builds annoy search tree
+    n_list: A matrix. rows = documents. columns = vocabulary
+    """
+    # Build an Annoy index with 10 trees. angular = cosine similarity
+    annoy_index = AnnoyIndex(n_list.shape[1], metric='angular')
+    for i, x in enumerate(n_list):
+        annoy_index.add_item(i, x)
+    annoy_index.build(10)
+
+    return annoy_index
+    
+
+def get_nearest_neighbors_annoy(item, n_list, k):
+    """ 
+    Find the K nearest neighbors using Annoy package and cosine similarity.
+    item is the single vector to search
+    n_list is a numpy matrix
+    k is the number of neighbors
+    """
+
+    # Build an Annoy index with 10 trees. angular = cosine similarity
+    annoy_index = AnnoyIndex(n_list.shape[1], metric='angular')
+    for i, x in enumerate(n_list):
+        annoy_index.add_item(i, x)
+    annoy_index.build(10)
+
+    # Find the k nearest neighbors to the first sentence
+    nearest_neighbors = annoy_index.get_nns_by_vector(item, k)
+
+    return nearest_neighbors
+
+
+def get_k_unused_items(item, annoy_tree, used_items, k):
+    """
+    Returns k unused items.
+    """
+    # Find the k nearest neighbors to the first sentence
+    k_unused_items = []
+    new_k = k
+
+    i = 0  # TEMP
+
+    while len(k_unused_items) < k:
+        nearest_neighbors = annoy_tree.get_nns_by_vector(item, new_k)
+        for nn in nearest_neighbors:
+            if (nn not in used_items) and (len(k_unused_items) < k) and (nn not in k_unused_items):
+                k_unused_items.append(nn)
+        new_k += 1
+        i += 1  # TEMP
+    return k_unused_items
+
+
+def force_anonym_using_annoy(docs, k):
+    """ 
+    Steps:
+    1. Create BoW representation
+    2. For each document, finds k nearest neighbors
+    3. For each group of neigbors, replace different words in *
+    """
+    annon_docs = docs.copy()
+    used_indexes = set([])
+
+    cdocs = [nlp_utils.lemmatize_doc(doc) for doc in docs]
+    docs = cdocs
+
+    vecs, _ = get_bow(docs, lemmatize = True)
+    vecs = vecs.toarray()  # From sparse matrix to array
+    curr_k, non_anon_indexes = get_anonym_degree(vecs=vecs)
+    # print('Start: get_anonym_degree:', curr_k)
+    
+    temp_docs_emb = vecs.copy()
+
+    neighbor_list = []
+    if k >= curr_k: # if i already curr_k than don't run the following:
+
+        annoy_tree = build_annoy_search_tree(vecs)
+
+        for i, _ in enumerate(docs):
+            #print('i:', i, '\t', used_indexes)
+            # To prevent redandent
+            if i not in used_indexes:
+                similar_doc_ind = get_k_unused_items(vecs[i], annoy_tree, used_indexes, k)
+                #used_indexes.add(i)  # Adding to the used items
+                # similar_doc_ind = get_nearest_neighbors_annoy(temp_docs_emb[i], temp_docs_emb, k)
+                neighbor_list.append(similar_doc_ind)
+                # print('similar_doc_ind', similar_doc_ind)
+                curr_docs = []
+                for sd in similar_doc_ind:
+                    if sd in used_indexes:
+                        print('Error!:', sd, 'was already used.')
+                    # Adding the document to the similar doc list
+                    curr_docs.append(docs[sd])
+                    # Adding the index to the used items
+                    used_indexes.add(sd)  
+                    # Prevent repeating comparison by changing the vector
+                    # temp_docs_emb[sd] = (-1000) * np.random.randint(10, size=len(temp_docs_emb[sd]))
+                    #temp_docs_emb[sd] = [1000] * len(temp_docs_emb[sd])
+                anonym_docs = delete_uncommon_words(curr_docs)
+                i = 0
+                for sd in similar_doc_ind:
+                    annon_docs[sd] = anonym_docs[i]
+                    i += 1
+            if  len(used_indexes) > (len(docs) - k):
+                # print('Breaking after moving over', len(used_indexes), 'of all', len(docs), 'indexes.')
+                #print('Breaking! \tlen(used_indexes)', len(used_indexes), '\tlen(docs)', len(docs), '\tlen(docs)-k', (len(docs) - k))
+                # Deleting the remaining docs
+                unused_indexes = list(set(range(len(docs))) - set(used_indexes))
+                # print('unused_indexes:', unused_indexes)
+                for i in unused_indexes:
+                    annon_docs[i] = '*'
+                break
+        curr_k, _ = get_anonym_degree(docs=annon_docs)
+        # print('End: get_anonym_degree:', curr_k) 
+    else:
+        annon_docs = None
+        neighbor_list = None
+        print(f"we already have k-anonymity for k={k}")
+    return annon_docs, neighbor_list
+
+
+def add_neighbor_list_to_df(df, neighbor_list):
+    """
+    Adds the neigbors for each document
+    """
+    df['neigbors'] = None
+    for k_neighbors in neighbor_list:
+        for n in k_neighbors:
+            df.loc[n, 'neigbors'] = str(k_neighbors)
+    return df
+
+
+def delete_uncommon_words(docs):
+    """
+    Deletes the uncommon words from given documents
+    """
+    # Lemmatizing
+    ldocs = [nlp_utils.lemmatize_doc(d) for d in docs]
+
+    vecs, voc = get_bow(ldocs)
+
+    vecs = vecs.toarray()
+    diff = get_diff(vecs)
+
+    words_to_delete = voc[diff > 0]
+
+    temp_docs = []
+    for d in ldocs:
+        new_d = d
+        for word in words_to_delete:
+            #new_d = new_d.replace(word, '*')
+            new_d = re.sub(rf'\b{word}\b', '*', new_d)
+        temp_docs.append(new_d)
+
+    return temp_docs
+
+
+def get_nearest_neighbors_using_jaccard(n_list, k):
+    """
+    Finds the k nearest neighbors for each item.
+    returns list of neighbor list (list of lists)
+    """
+    # Chossing the jaccard index method (for string of vectors)
+    if isinstance(n_list[0], str):
+        jaccard_func = nlp_utils.jaccard_index
+    else:
+        jaccard_func = jaccard_index
+    
+    all_neighbors = []
+    used_items = []
+    for idx1, doc1 in enumerate(n_list):
+        if idx1 not in used_items:
+            #  Appending to the used items
+            used_items.append(idx1)
+            doc1_neighbors = []
+            for idx2, doc2 in enumerate(n_list):
+                    if idx2 not in used_items:
+                        # Appending a tuple of (distance, index)
+                        doc1_neighbors.append((jaccard_func(doc1, doc2), idx2))
+                        #print(idx1, idx2, jaccard_func(doc1, doc2))
+
+            # Sorting accroding to the first item in the tuple - distance
+            doc1_neighbors.sort(reverse=True)
+            # Looking for k-1 neighbors
+            doc1_neighbors = doc1_neighbors[:k-1] 
+            # Adding the document itself, so 
+            #if doc1 not in 
+            curr_doc_list = [idx1]
+            for curr_doc in doc1_neighbors:
+                # Adding the neighbor
+                curr_doc_list.append(curr_doc[1])
+                # Removing from the availble document list
+                used_items.append(curr_doc[1])
+            all_neighbors.append(curr_doc_list)
+            #all_neighbors.append(free_items)
+    return all_neighbors
+
+
+def delete_word(word_dict, word):
+    """
+    When forcing anonymization, finds the word in the dictionary, and updates the replaced tag.
+    """
+    if word in word_dict:
+        word_dict[word]['replaced'] = '*'
+    else:
+        for w in word_dict.keys():
+            if (word_dict[w]['lemma'] == word) or (word_dict[w]['replaced'] == word):
+                word_dict[w]['replaced'] = '*'
+                break  # Exit for
+
+
+if __name__ == 'main':
+    import sys
+    sys.path.append("utils")
+    from utils import nlp_utils
+
+    print('Hiiiiiiiiiiiiiii')
---
+++ packages/Kanon4txt/kanon4txt-0.0.1-py3-none-any.whl/Kanon4txt/utils/nlp_utils.py
@@ -0,0 +1,376 @@
+# Imports
+import pandas as pd
+import numpy as np
+import re
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+import spacy
+import re
+import bz2
+from collections import defaultdict
+import operator
+from . import models
+
+# nltk.download('stopwords')
+
+stopword_list = None
+
+
+# Defining some global variables
+nlp = spacy.load('en_core_web_sm', disable=['ner', 'parser'])  # disabling Named Entity Recognition for speed
+glove_model = models.glove_model
+
+def init_stopwords(file=None):
+    """
+    Initialized the stopword list
+    """
+    global stopword_list
+
+    stopword_list = stopwords.words('english')
+
+    if file:
+        # Opening file in read mode
+        with open(file, 'r') as file:
+            
+            # reading the file
+            data = file.read()
+            
+            # replacing end splitting the text 
+            # when newline ('\n') is seen.
+            data_into_list = data.split("\n")
+            stopword_list = list(set(stopword_list + data_into_list))
+
+
+def corpus_stop_words(corpus,num_stop):
+    """this function gets as input the text corpus (corpus) - list of texts, and a number (num_stop),
+    it finds the most common words in the corpus and returns them as a dataframe/csv"""
+    # Step 1: Tokenize the texts - make sure the corpus was pre-processed!! (lower/etc)
+    tokenized_corpus = [text.split() for text in corpus]
+
+    # Step 2: Count unique words per document
+    unique_words_per_document = [set(text) for text in tokenized_corpus]
+
+    # Step 3: Count word frequencies
+    word_frequency = defaultdict(int)
+    for words in unique_words_per_document:
+        for word in words:
+            word_frequency[word] += 1
+
+    # Step 4: Sort and select top words
+    most_frequent_words = sorted(word_frequency.items(), key=operator.itemgetter(1), reverse=True)[:num_stop]
+
+    return most_frequent_words # list of most frequent words to set as stop-words
+
+
+def reading_bz_file(train_file):
+    """ Reading the input file and returns a dataframe """
+    # Credit: https://www.kaggle.com/code/pierremegret/gensim-word2vec-tutorial
+
+    # Readling the file to list of comments
+    train_file = bz2.BZ2File(train_file)
+    train_file_lines = train_file.readlines()
+
+    # Converting from raw binary strings to strings that can be parsed
+    train_file_lines = [x.decode('utf-8') for x in train_file_lines]
+
+    # Extracting the labels and sentences
+    train_labels = [0 if x.split(' ')[0] == '__label__1' else 1 for x in train_file_lines]
+    train_sentences = [x.split(' ', 1)[1][:-1].lower() for x in train_file_lines]  # And converting to lower case
+
+    # Converting to df
+    df = pd.DataFrame(list(zip(train_sentences, train_labels)), columns=['txt', 'sentiment'])
+
+    # Adding number of words
+    df['num_of_words'] = df['txt'].apply(lambda x: len(x.split(' ')))
+
+    return df
+
+
+def jaccard_index(sentence1, sentence2):
+    """ Calc Jaccard index for each pair of sentences """
+    words1 = set(sentence1.split())
+    words2 = set(sentence2.split())
+    intersection = len(words1.intersection(words2))
+    union = len(words1.union(words2))
+    if union > 0:
+        jaccard = intersection / union
+    else:
+        # print('Warning: jaccard_index union = 0', sentence1, sentence2)
+        jaccard = 0
+    return jaccard
+
+
+def get_voc(corpus):
+    """ Gets corpus voccabulary """
+    word_set = set([])
+
+    for doc in corpus:
+        doc = doc.split(' ')
+        for word in doc:
+            word_set.add(word)
+    return word_set
+
+
+def get_lemma(word, nlp=nlp):
+    """ Returns the lemma of the given word """
+    word = nlp(word)
+    txt = [token.lemma_ for token in word]
+    lemma = txt[0]
+    return lemma
+
+
+def create_word_dict(corpus):
+    """ Creates a word dictionary. Keys = words. Values = three boolians - stop/lemma/replaced"""
+
+    all_words = get_voc(corpus)
+
+    all_word_dict = {}
+
+    for word in all_words:
+        # Cleaning non AB characters
+        word = replace_non_ab_chars(word)
+        word_dict = {
+            'protected': None,
+            'lemma': None,
+            'replaced': None}
+        if len(word) > 0:
+            if word in stopword_list:
+                word_dict['protected'] = True
+            elif get_lemma(word) != word:
+                word_dict['lemma'] = get_lemma(word)
+            all_word_dict[word] = word_dict
+
+    return all_word_dict
+
+
+def print_doc(doc, all_word_dict):
+    """ Prints document based on the word dictionary """
+    # out_str = 'Legend: (protected) [replaced] {lemmatized}\n'
+    out_str = ''
+    words = doc.split()
+
+    for w in words:
+        print_w = w.lower()
+        w = replace_non_ab_chars(w)
+        if w in all_word_dict:
+            if all_word_dict[w]['protected']:
+                print_w = '(' + print_w + ')'
+            elif all_word_dict[w]['lemma']:
+                print_w = '{' + all_word_dict[w]['lemma'] + '}'
+            if all_word_dict[w]['replaced']:
+                print_w = '[' + all_word_dict[w]['replaced'] +']'
+        out_str = f'{out_str}{print_w} '
+
+    return out_str
+
+
+def replace_non_ab_chars(word):
+    word = re.sub("[^A-Za-z']+", '', str(word)).lower()
+    return word
+
+
+
+def cleaning(doc, break_doc=False):
+    """Lemmatizing and removes stopwords"""
+    # Defining the document
+    if break_doc:
+        doc = doc.replace(' ', '. ' )
+    if doc:
+        doc = nlp(doc)
+
+        # Lemmatizes and removes stopwords
+
+        # For some reason, sometimes the lemmatization is not consistent - 
+        # identical words receive a different lemmatization (for example, acting is sometimes 
+        # changed to act and sometimes remains acting). 
+        # Apperantly, Spacy's lemmatization depends on the part of speech: 
+        # https://stackoverflow.com/a/74176351
+        # To overcome this, we added the break_doc parameter
+
+        txt = []
+        for token in doc:
+            word = re.sub('\W+', '', str(token)).lower()
+            if word and (word not in stopword_list):
+                txt.append(token.lemma_)
+    
+        clean_doc = ' '.join(txt)
+    else:
+        clean_doc = doc
+
+    return clean_doc
+
+
+def lemmatize_doc(doc, break_doc=False):
+    """Lemmatizes document"""
+    # Breaking the document to allow consistent lemmatization (see function cleaning())
+    if break_doc:
+        doc = doc.replace(' ', '. ' )
+    doc = nlp(doc)
+
+    # Lemmatizes and removes stopwords
+    # doc needs to be a spacy Doc object
+    txt = []
+    for token in doc:
+        if str(token) in stopword_list:  # Add stopword as it is
+            txt.append(str(token))
+        else:  # Lemmatize other words
+            txt.append(token.lemma_)
+    clean_doc = ' '.join(txt)
+
+    return clean_doc
+
+
+def clean_corpus(corpus):
+    """ Cleans the corpus """
+    brief_cleaning = (re.sub("[^A-Za-z']+", ' ', str(row)).lower() for row in corpus)
+    corpus_lemmas = [cleaning(doc) for doc in nlp.pipe(brief_cleaning, batch_size=5000)]
+    return corpus_lemmas
+
+
+def get_average_jaccard(corpus, k=1):
+    """ Calculates the avergae Jaccard index by averaging each documents k nearest neighbors """
+
+    cc = clean_corpus(corpus)
+
+    all_neighbors = []
+    for idx1, doc1 in enumerate(cc):
+        doc1_neighbors = []
+        for idx2, doc2 in enumerate(cc):
+            # Avod repeated comparisons
+            if idx2 > idx1:
+                doc1_neighbors.append(jaccard_index(doc1, doc2))
+        doc1_neighbors.sort(reverse=True)
+        all_neighbors += doc1_neighbors[:k]
+    avg = np.average(all_neighbors)
+    return avg
+
+
+def plot_jaccard_hist(df_short_sentences, column = 'txt'):
+    """creat a hist of jaccard scores"""
+
+    indices_list = list(df_short_sentences.index)
+    indices_list_short_1 = indices_list
+
+    # Create a list of sentence texts
+    sentences = list(df_short_sentences[column].loc[indices_list_short_1])
+
+    # Compute the Jaccard index for all pairs of sentences
+    jaccard_index_dict = {(indices_list_short_1[i], indices_list_short_1[j]): jaccard_index(sentences[i], sentences[j])
+                          for i in range(len(sentences)) for j in range(i + 1, len(sentences))}
+
+    # Sort the dictionary by its values in descending order
+    sorted_dict = dict(sorted(jaccard_index_dict.items(), key=lambda item: item[1], reverse=True))
+    print(sorted_dict)
+    # extract the values from the dictionary
+    dic_values = list(sorted_dict.values())
+
+    # plot a histogram of the values
+    plt.hist(dic_values, bins=50, range=(0, 0.3))
+
+    # set labels and title
+    plt.xlabel('Values')
+    plt.ylabel('Counts')
+    plt.title('Histogram of Jaccard Indecs')
+
+    # display the plot
+    plt.show()
+
+
+def get_general_word_from_cluster(word_list, we_model):
+    """ Finds the most similar words usind word embedding"""
+    # glove_words = list(we_model.index_to_key)
+    # For W2V
+    # known_words = [w for w in word_list if w in we_model.vocab]
+    # For GLOVE
+    known_words = [w for w in word_list if w in we_model.index_to_key]
+    print('known words:', len(known_words), 'word_list', len(word_list))
+    if len(known_words) > 0:
+        we_word = we_model.most_similar(known_words, topn=1)[0][0]
+    else:
+        we_word = None
+    return we_word
+
+
+def add_general_word_to_word_dict(word_dict, word):
+    """ Updating that the given words were replaced """
+    word_dict[word] = {
+        'protected': False,
+        'lemma': False,
+        'replaced': True}
+    return word_dict
+
+
+def replace_words_in_df(df_0, cluster_dict, distance_dict, word_dict_0, update_stop=True):
+    """ Replaces the words in the dataframe """
+
+    global stopword_list
+
+    # Working with a copy of the df:
+    df_copy = df_0
+    word_dict_copy = word_dict_0.copy()
+
+    df_copy['anon_txt'] = df_copy['txt'].apply(lambda x: lemmatize_doc(x))
+
+    for key, words in cluster_dict.items():
+        if key >= 0:  # Ignoring the -1 label
+            #if len(cluster_dict[key]) < 20:  # so it will make sense!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            # Getting the general word
+            general_word = get_general_word_from_cluster(words, glove_model)
+
+            if update_stop and (general_word not in stopword_list):
+                stopword_list.append(general_word)  # the list of new words
+            print('distance:', distance_dict[key], '\tcluster size:', len(words),'\treplacing', words, 'in', general_word)
+            words_to_replace = []
+            for word in words:
+                if word not in word_dict_copy:  # Lemmatized word
+                    word_dict_copy[word] = {'protected': False, 'lemma': word, 'replaced':False}
+                    
+                if not word_dict_copy[word]['protected']:
+                    # Updaing the word dictionary that the words were replaced
+                    word_dict_copy[word]['replaced'] = general_word  # Problem: greate in line 2 miss-identified as replaced
+                
+                df_copy['anon_txt'] = df_copy['anon_txt'].replace(fr'\b{word}\b', general_word, regex=True)
+                
+                words_to_replace.append(word)
+
+            # Replacing whole words
+            # rep_str = '\b|\b'.join(words_to_replace)
+            # df_copy['anon_txt'] = df_copy['anon_txt'].replace(fr'\b{rep_str}\b', general_word, regex=True)
+
+    # print('Final average Jaccard index:', get_average_jaccard(df_copy['anon_txt'], k=k))
+    df_copy['anon_txt_history'] = df_copy['txt'].apply(lambda x: print_doc(x, word_dict_copy))
+    # df_copy['num_replaced'] = df_copy['anon_txt_history'].apply(lambda x: len(re.findall(r'\[\w+\]', x)))
+    # df_copy['num_lemmatized'] = df_copy['anon_txt_history'].apply(lambda x: len(re.findall(r'\{\w+\}', x)))
+    # df_copy['num_protected'] = df_copy['anon_txt_history'].apply(lambda x: len(re.findall(r'\(\w+\)', x)))
+    # df_copy['num_no_change'] = df_copy['num_of_words'] - df_copy['num_replaced'] - df_copy['num_lemmatized'] - df_copy['num_protected']
+
+    if update_stop:
+        print('Stop word list was updated')
+
+    return df_copy, word_dict_copy
+
+
+def get_stat(word_dict):
+    """
+    Returns the number of stop words, lemmatized and replaced words
+    """
+    prot_count = 0
+    lemm_count = 0
+    rep_count = 0
+
+    all = 0
+
+    for word in word_dict.values():
+        if word['protected']:
+            prot_count += 1
+        elif word['replaced']:
+            rep_count += 1
+        elif word['lemma']:
+            lemm_count += 1
+        all += 1
+    return prot_count, lemm_count, rep_count, all
+
+
+if __name__ == 'main':
+    print('yehhh')
+
