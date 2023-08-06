8f801cf134e21fb308cf06cfd191d9081e4623d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:28 +0000

    Add package pilot-platform-common/0.4.1/pilot-platform-common-0.4.1.tar.gz

---
+++ packages/pilot-platform-common/pilot-platform-common-0.4.1.tar.gz/pilot-platform-common-0.4.1/common/lineage/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2022-2023 Indoc Research
+#
+# Contact Indoc Research for any questions regarding the use of this source code.
+
+from .lineage_client import LineageClient
+
+__all__ = [
+    'LineageClient',
+]
---
+++ packages/pilot-platform-common/pilot-platform-common-0.4.1.tar.gz/pilot-platform-common-0.4.1/common/logging/logger.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2023 Indoc Research
+#
+# Contact Indoc Research for any questions regarding the use of this source code.
+
+import logging
+
+logger = logging.getLogger('pilot.common')
