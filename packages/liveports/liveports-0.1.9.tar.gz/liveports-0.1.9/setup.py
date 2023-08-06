45e8afeb1a561886d6501de8e742be0631ff1195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:00 +0000

    Add package liveports/0.1.9/liveports-0.1.9.tar.gz

---
+++ packages/liveports/liveports-0.1.9.tar.gz/liveports-0.1.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='liveports',
+    version='0.1.9',
+    description='Give address to your localmachine',
+    author='Abhinav',
+    author_email='abhinavabcd@gmail.com',
+    packages=['liveports'],
+    include_package_data=True,
+    install_requires=[
+        "requests>=2.23.0",
+        "brotlipy>=0.7.0",
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'liveports = liveports.client:main',
+        ],
+    },
+)
