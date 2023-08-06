8a5c4dbadbece1fbc0abf47a1fd1e2b9c989b376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:01 +0000

    Add package docs-ranking-metrics/0.0.4/docs-ranking-metrics-0.0.4.tar.gz

---
+++ packages/docs-ranking-metrics/docs-ranking-metrics-0.0.4.tar.gz/docs-ranking-metrics-0.0.4/src/docs_ranking_metrics/__init__.py
@@ -0,0 +1,4 @@
+from .ranking_metrics import RankingMetrics, Bm25, LaBSE, USE, MsMarcoST, MsMarcoCE
+
+__all__ = ['RankingMetrics', 'Bm25', 'LaBSE', "USE", 'MsMarcoST', 'MsMarcoCE']
+__version__ = "0.0.4"
