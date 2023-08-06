fe74871812e4d1f6aeaa967b14652db5dd5392e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:16 +0000

    Add package jax-relax/0.1.6/jax_relax-0.1.6-py3-none-any.whl

---
+++ packages/jax-relax/jax_relax-0.1.6-py3-none-any.whl/relax/methods/__init__.py
@@ -0,0 +1,8 @@
+from .vanilla import VanillaCFConfig, VanillaCF
+from .diverse import DiverseCFConfig, DiverseCF
+from .proto import ProtoCFConfig, ProtoCF
+from .counternet import CounterNetConfigs, CounterNet
+from .cchvae import CCHVAEConfigs, CCHVAE
+from .clue import CLUEConfigs, CLUE
+from .sphere import GSConfig, GrowingSphere
+from .vaecf import VAECFConfigs, VAECF
