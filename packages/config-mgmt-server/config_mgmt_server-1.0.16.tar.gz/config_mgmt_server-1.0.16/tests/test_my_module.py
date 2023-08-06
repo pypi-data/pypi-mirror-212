34056fc01ba97a84b5e9427f312db4cf7c0bc85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:13 +0000

    Add package config-mgmt-server/1.0.16/config_mgmt_server-1.0.16.tar.gz

---
+++ packages/config-mgmt-server/config_mgmt_server-1.0.16.tar.gz/config_mgmt_server-1.0.16/tests/test_my_module.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+# import config_mgmt_server    # The code to test
+
+
+class TestSrc:
+    def test_build(self) -> None:
+        assert 1
