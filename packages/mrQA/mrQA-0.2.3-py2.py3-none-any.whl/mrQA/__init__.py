5eb058122075764137b7a567b09ffc49acf28f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:11 +0000

    Add package mrQA/0.2.3/mrQA-0.2.3-py2.py3-none-any.whl

---
+++ packages/mrQA/mrQA-0.2.3-py2.py3-none-any.whl/mrQA/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for mrQA."""
+
+__author__ = """Harsh Sinha"""
+__email__ = 'harsh.sinha@pitt.edu'
+__version__ = '0.1.0'
+
+from mrQA.project import check_compliance
+from mrQA.monitor import monitor
+
+from . import _version
+__version__ = _version.get_versions()['version']
