eedf2ac417bc22fb804421f8e3830e2b7d9778f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:53 +0000

    Add package try-pyspark2/0.0.0/try_pyspark2-0.0.0.tar.gz

---
+++ packages/try-pyspark2/try_pyspark2-0.0.0.tar.gz/try_pyspark2-0.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from try_pyspark2.sql_file_picker import FilePicker
+
+VERSION = '0.0.0' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="try_pyspark2", 
+        version=VERSION,
+        author="Jason Dsouza",
+        author_email="<youremail@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        data_files=FilePicker().sql_file_picker(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
---
+++ packages/try-pyspark2/try_pyspark2-0.0.0.tar.gz/try_pyspark2-0.0.0/try_pyspark2/test_pyspark.py
@@ -0,0 +1,5 @@
+from sql_file_picker import FilePicker
+
+def test_pyspark():
+    a = FilePicker()
+    return   a.sql_file_picker()
