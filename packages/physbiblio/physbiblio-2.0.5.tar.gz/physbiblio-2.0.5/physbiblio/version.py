6a2ace8e0faa57835da6b7fbc9a4c0419681da2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:55 +0000

    Add package physbiblio/2.0.5/physbiblio-2.0.5.tar.gz

---
+++ packages/physbiblio/physbiblio-2.0.5.tar.gz/physbiblio-2.0.5/physbiblio/version.py
@@ -0,0 +1,6 @@
+__version__ = "2.0.5"
+__version_date__ = "07/06/2023"
+
+__recent_changes__ = """<br>* fixes for python 3.11<br>
+* ISBN2bibtex scripts dropped (the webservice is no longer working)<br>
+"""
