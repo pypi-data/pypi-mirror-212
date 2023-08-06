27632493a253a109c0f2016eb08033787bd65504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:59 +0000

    Add package SATOSA/8.3.0/SATOSA-8.3.0.tar.gz

---
+++ packages/SATOSA/SATOSA-8.3.0.tar.gz/SATOSA-8.3.0/setup.py
@@ -0,0 +1,44 @@
+"""
+setup.py
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='SATOSA',
+    version='8.3.0',
+    description='Protocol proxy (SAML/OIDC).',
+    author='DIRG',
+    author_email='satosa-dev@lists.sunet.se',
+    license='Apache 2.0',
+    url='https://github.com/SUNET/SATOSA',
+    packages=find_packages('src/'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "pyop >= v3.4.0",
+        "pysaml2 >= 6.5.1",
+        "pycryptodomex",
+        "requests",
+        "PyYAML",
+        "gunicorn",
+        "Werkzeug",
+        "click",
+        "chevron",
+        "cookies-samesite-compat",
+    ],
+    extras_require={
+        "ldap": ["ldap3"],
+        "pyop_mongo": ["pyop[mongo]"],
+        "pyop_redis": ["pyop[redis]"],
+    },
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    entry_points={
+        "console_scripts": ["satosa-saml-metadata=satosa.scripts.satosa_saml_metadata:construct_saml_metadata"]
+    }
+)
