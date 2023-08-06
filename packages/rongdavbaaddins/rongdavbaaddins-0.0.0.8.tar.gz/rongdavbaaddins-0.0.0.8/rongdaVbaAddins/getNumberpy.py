b1edd3ce2f07ea77ade9b6f4a381c98b62aef69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:24 +0000

    Add package rongdavbaaddins/0.0.0.8/rongdavbaaddins-0.0.0.8.tar.gz

---
+++ packages/rongdavbaaddins/rongdavbaaddins-0.0.0.8.tar.gz/rongdavbaaddins-0.0.0.8/rongdaVbaAddins/getNumberpy.py
@@ -0,0 +1,9 @@
+from getNumber import run
+import  sys
+if __name__ == "__main__":
+    # functionName = sys.argv[1]
+    # token = sys.argv[2]
+    functionName = "vbarongdatools001"
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMDAxNzMyIiwiaXAiOiIxNzIuMTYuNzMuMSIsIm1hYyI6IjAwMmI2N2UyMmQ1OCJ9.nuphngVh-HEXUhQeiADFvvEOKMwASpCFxWb2VtC0AH4'
+
+    run(functionName, token)
