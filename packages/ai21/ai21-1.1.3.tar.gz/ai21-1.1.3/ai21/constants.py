31ca85c4fe22ac62fa3679cbda05a07ffdc68c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:03 +0000

    Add package ai21/1.1.3/ai21-1.1.3.tar.gz

---
+++ packages/ai21/ai21-1.1.3.tar.gz/ai21-1.1.3/ai21/constants.py
@@ -0,0 +1,7 @@
+DEFAULT_API_VERSION = 'v1'
+STUDIO_HOST = 'https://api.ai21.com'
+
+SAGEMAKER_ENDPOINT_KEY = 'sm_endpoint'
+DESTINATION_KEY = 'destination'
+BEDROCK_HOST_FORMAT = 'bedrock.us-east-1.amazonaws.com'
+BEDROCK_URL_FORMAT = f'https://{BEDROCK_HOST_FORMAT}'
