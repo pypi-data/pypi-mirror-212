dc825066a5e4f434eee96441eccfa2a58ee2b922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:47 +0000

    Add package kelpie-personal-trainer/0.2.12/kelpie_personal_trainer-0.2.12.tar.gz

---
+++ packages/kelpie-personal-trainer/kelpie_personal_trainer-0.2.12.tar.gz/kelpie_personal_trainer-0.2.12/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.12'
+DESCRIPTION = 'Personal Trainer Library'
+
+
+# Setting up
+setup(
+    name="kelpie_personal_trainer",
+    version=VERSION,
+    author="Ariel",
+    author_email="<arillesmana2001@gmail.com>",
+    long_description=open('README.md').read(),
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_data={
+        'kelpie_personal_trainer': ['models/*']
+    },
+    include_package_data=True,
+    install_requires=['scikit-learn', 'pandas', 'opencv-python', 'mediapipe', 'numpy', 'tensorflow', 'keras', 'matplotlib'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
