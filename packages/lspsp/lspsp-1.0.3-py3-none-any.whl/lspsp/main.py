7d5026b862840e7896f57da4bb18f56298ac31d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:35 +0000

    Add package lspsp/1.0.3/lspsp-1.0.3-py3-none-any.whl

---
+++ packages/lspsp/lspsp-1.0.3-py3-none-any.whl/lspsp/main.py
@@ -0,0 +1,11 @@
+import sys
+from lspsp.lspmon import Lspmon
+from lspsp.monexec import MonitorExecutable
+
+
+def main():
+    MonitorExecutable(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
