1247f500c5fd3a835a1b44542243b6f95804f99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:43 +0000

    Add package datagen-protocol-core/0.34.0/datagen_protocol_core-0.34.0.tar.gz

---
+++ packages/datagen-protocol-core/datagen_protocol_core-0.34.0.tar.gz/datagen_protocol_core-0.34.0/datagen_protocol/schema/humans/presets/expression_presets/happiness.py
@@ -0,0 +1,17 @@
+HAPPINESS_PRESET = {
+    "brow_inner_up": 0.3,
+    "brow_outer_up_left": 0.2,
+    "brow_outer_up_right": 0.2,
+    "cheek_squint_left": 1.0,
+    "cheek_squint_right": 1.0,
+    "mouth_smile_right": 1.0,
+    "mouth_smile_left": 1.0,
+    "eye_squint_left": 0.5,
+    "eye_squint_right": 0.5,
+    "mouth_left": 0.21,
+    "mouth_right": 0.21,
+    "mouth_stretch_left": 0.15,
+    "mouth_stretch_right": 0.15,
+    "mouth_press_left": 0.3,
+    "mouth_press_right": 0.3
+}
