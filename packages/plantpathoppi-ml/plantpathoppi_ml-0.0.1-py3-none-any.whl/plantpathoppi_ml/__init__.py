01c6be5c5831ac342ddd23562ab64b26994182e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:46 +0000

    Add package plantpathoppi-ml/0.0.1/plantpathoppi_ml-0.0.1-py3-none-any.whl

---
+++ packages/plantpathoppi-ml/plantpathoppi_ml-0.0.1-py3-none-any.whl/plantpathoppi_ml/__init__.py
@@ -0,0 +1,3 @@
+""" plantpathoppi_ml predicts protein-protein interaction between the given pair of plant and pathogen protein sequences"""
+    
+from .prediction import predict, predict_proba, gen_file
