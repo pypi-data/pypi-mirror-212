a6b1ec79d785a03a618ad527e87e49112457d03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:36 +0000

    Add package pyrocko/2023.6.7/pyrocko-2023.6.7-cp38-cp38-win_amd64.whl

---
+++ packages/pyrocko/pyrocko-2023.6.7-cp38-cp38-win_amd64.whl/pyrocko/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = 'cea84da041c5345569c77a43e6a0261fd24b06a9'
+local_modifications = False
+version = '2023.06.07'
+long_version = 'pyrocko-2023.06.07-cea84da041c5345569c77a43e6a0261fd24b06a9-2023-06-07_04:06:24'  # noqa
+installed_date = '2023-06-07_04:06:24'
+src_path = 'C:\\Windows\\TEMP\\drone-Z8sy19nOjcI3nlgR\\drone\\src'
