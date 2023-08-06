d98a8596c151f94644a1a276545c1e5cf39655a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:54 +0000

    Add package openedx-webhooks/0.2.1/openedx_webhooks-0.2.1.tar.gz

---
+++ packages/openedx-webhooks/openedx_webhooks-0.2.1.tar.gz/openedx_webhooks-0.2.1/CHANGELOG.rst
@@ -0,0 +1,20 @@
+Change Log
+##########
+
+Version 0.2.1 (2023-06-06)
+**********************************************
+
+* Renamed package to openedx_webhooks. Upload to PyPI.
+
+Version 0.1.1 (2023-06-05)
+**********************************************
+
+* Improve documentation
+
+0.1.0 – 2023-05-31
+**********************************************
+
+Added
+=====
+
+* First release on PyPI.
---
+++ packages/openedx-webhooks/openedx_webhooks-0.2.1.tar.gz/openedx_webhooks-0.2.1/openedx_webhooks/utils.py
@@ -0,0 +1,34 @@
+"""
+Utilities used by Open edX Events Receivers.
+"""
+from collections.abc import MutableMapping
+
+from opaque_keys.edx.locator import CourseLocator
+
+
+def flatten_dict(dictionary, parent_key="", sep="_"):
+    """
+    Generate a flatten dictionary-like object.
+
+    Taken from:
+    https://stackoverflow.com/a/6027615/16823624
+    """
+    items = []
+    for key, value in dictionary.items():
+        new_key = parent_key + sep + key if parent_key else key
+        if isinstance(value, MutableMapping):
+            items.extend(flatten_dict(value, new_key, sep=sep).items())
+        else:
+            items.append((new_key, str(value)))
+    return dict(items)
+
+
+def serialize_course_key(inst, field, value):  # pylint: disable=unused-argument
+    """
+    Serialize instances of CourseLocator.
+
+    When value is anything else returns it without modification.
+    """
+    if isinstance(value, CourseLocator):
+        return str(value)
+    return value
