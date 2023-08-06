e8544d51cb180b10a52014ed6a791e96855bd8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:13 +0000

    Add package image-stitching/1.0.0/image_stitching-1.0.0.tar.gz

---
+++ packages/image-stitching/image_stitching-1.0.0.tar.gz/image_stitching-1.0.0/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+setup(
+      name = 'image_stitching',
+      version = '1.0.0',
+      author = 'zyq',
+      py_modules=['image_stitching.stitch_image', 'image_stitching.exceptions', 'image_stitching.util'] #记得改名字
+
+)
