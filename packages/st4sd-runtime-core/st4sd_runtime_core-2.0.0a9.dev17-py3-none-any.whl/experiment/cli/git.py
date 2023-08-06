f7090aaad1de123958f97c840b304bb0dff70b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:47 +0000

    Add package st4sd-runtime-core/2.0.0a9.dev17/st4sd_runtime_core-2.0.0a9.dev17-py3-none-any.whl

---
+++ packages/st4sd-runtime-core/st4sd_runtime_core-2.0.0a9.dev17-py3-none-any.whl/experiment/cli/git.py
@@ -0,0 +1,72 @@
+# Copyright IBM Inc. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Author: Alessandro Pomponio
+
+import subprocess
+from pathlib import Path
+
+import typer
+from rich.console import Console
+
+from experiment.cli.exit_codes import STPExitCodes
+
+stderr = Console(stderr=True)
+stdout = Console()
+
+
+def get_git_toplevel_path(path: Path):
+    try:
+        toplevel_path = (
+            subprocess.check_output(
+                ["git", "rev-parse", "--show-toplevel"], cwd=path.parent
+            )
+            .decode()
+            .strip()
+        )
+    except subprocess.CalledProcessError as e:
+        stderr.print(
+            "[red]Error:[/red]:\tUnable to retrieve top level path for the experiment via git.\n"
+            "[italic]Tip:\tAre you sure the path belongs to a git repository?[/italic]"
+        )
+        raise typer.Exit(code=STPExitCodes.GIT_ERROR)
+
+    return toplevel_path
+
+
+def get_git_origin_url(path: Path):
+    try:
+        origin_url = (
+            subprocess.check_output(
+                ["git", "remote", "get-url", "origin"], cwd=path.parent
+            )
+            .decode()
+            .strip()
+        )
+        if origin_url.endswith(".git"):
+            origin_url = origin_url[:-4]
+    except subprocess.CalledProcessError as e:
+        stderr.print(
+            "[red]Error:[/red]:\tUnable to retrieve origin url for the experiment via git.\n"
+            "[italic]Tip:\tAre you sure the path belongs to a git repository?[/italic]"
+        )
+        raise typer.Exit(code=STPExitCodes.GIT_ERROR)
+
+    return origin_url
+
+
+def get_git_head_commit(path: Path):
+    try:
+        head_commit = (
+            subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=path.parent)
+            .decode()
+            .strip()
+        )
+    except subprocess.CalledProcessError as e:
+        stderr.print(
+            "[red]Error:[/red]:\tUnable to retrieve the head commit for the experiment via git."
+            "[italic]Tip:\tAre you sure the path belongs to a git repository?[/italic]"
+        )
+        raise typer.Exit(code=STPExitCodes.GIT_ERROR)
+
+    return head_commit
