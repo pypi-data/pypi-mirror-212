4c892c7680c702796bba6ff76f778b509edf7a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:13 +0000

    Add package pyarmor/8.2.3/pyarmor-8.2.3.zip

---
+++ packages/pyarmor/pyarmor-8.2.3.zip/pyarmor-8.2.3/src/cli/finger.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#############################################################
+#                                                           #
+#      Copyright @ 2023 -  Dashingsoft corp.                #
+#      All rights reserved.                                 #
+#                                                           #
+#      Pyarmor                                              #
+#                                                           #
+#      Version: 8.0.1 -                                     #
+#                                                           #
+#############################################################
+#
+#
+#  @File: cli/finger.py
+#
+#  @Author: Jondy Zhao (pyarmor@163.com)
+#
+#  @Create Date: Fri May  5 19:00:33 CST 2023
+#
+import os
+
+from subprocess import check_output
+
+from . import logger
