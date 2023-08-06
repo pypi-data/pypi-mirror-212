06ef7f9b0a75e9d2879b89204551d9a29b5b0a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:32 +0000

    Add package autogluon.bench/0.0.4/autogluon.bench-0.0.4-py3-none-any.whl

---
+++ packages/autogluon.bench/autogluon.bench-0.0.4-py3-none-any.whl/autogluon/bench/frameworks/multimodal/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eo pipefail
+
+GIT_URI=$1
+BRANCH=$2
+DIR=$3  # from root of benchmark run
+AG_BENCH_VER=$4
+
+if [ ! -d $DIR ]; then
+  mkdir -p $DIR
+fi
+repo_name=$(basename -s .git $(echo $GIT_URI))
+git clone --depth 1 --single-branch --branch ${BRANCH} --recurse-submodules ${GIT_URI} $DIR/$repo_name
+
+# create virtual env
+python3 -m venv $DIR/.venv
+source $DIR/.venv/bin/activate
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade setuptools wheel
+
+python3 -m pip install autogluon.bench==$AG_BENCH_VER
+
+cd $DIR/$repo_name
+
+python3 -m pip install -e common/[tests]
+python3 -m pip install -e core/[all,tests]
+python3 -m pip install -e features/
+python3 -m pip install -e multimodal/[tests]
+
+python3 -m mim install -q mmcv-full
+python3 -m pip install "mmdet>=2.28, <=3.0.0"
---
+++ packages/autogluon.bench/autogluon.bench-0.0.4-py3-none-any.whl/autogluon/bench/frameworks/tabular/exec.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eo pipefail
+
+framework=${1}
+benchmark=${2}
+constraint=${3}
+DIR=${4}  # from root of project
+shift 4
+
+while getopts "t:c:" opt; do
+    case $opt in
+        t) task=$OPTARG;;
+        c) custom_dir=$OPTARG;;
+        :\?) echo "Error: invaled option -$OPTARG"; exit1;;
+        :) echo "Error: option -$OPTARG requires an argument"; exit1;;
+    esac
+done
+
+amlb_args="$framework $benchmark $constraint -s force"
+
+if [ -n "$task" ]; then
+    amlb_args+=" -t $task"
+fi
+
+if [ -n "$custom_dir" ]; then
+    cp -r $custom_dir $DIR
+    amlb_args+=" -u $custom_dir"
+fi
+
+cd $DIR
+source .venv/bin/activate
+
+python3 ./automlbenchmark/runbenchmark.py  $amlb_args
