2fc39e46504fc817ef3e4cee86ac777873a5b07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:54 +0000

    Add package static-frame-pyodide/0.1.5/static_frame_pyodide-0.1.5-py3-none-any.whl

---
+++ packages/static-frame-pyodide/static_frame_pyodide-0.1.5-py3-none-any.whl/static_frame_pyodide/__init__.py
@@ -0,0 +1,52 @@
+import sys
+import asyncio
+
+__version__ = '0.1.5'
+_SF = '1.4.5'
+_AK = '0.4.8'
+_AM = '0.1.9'
+_EMS = '3_1_32' # emscripten version
+
+if sys.platform != 'emscripten':
+    raise RuntimeError('This package is only for use in emscripten environments')
+
+_URL = 'https://flexatone.s3.us-west-1.amazonaws.com/packages/'
+
+async def _load():
+    print('micropiping modules')
+    import micropip
+
+    await micropip.install([
+        'sqlite3',
+        f'{_URL}arraymap-{_AM}-cp311-cp311-emscripten_{_EMS}_wasm32.whl',
+        f'{_URL}arraykit-{_AK}-cp311-cp311-emscripten_{_EMS}_wasm32.whl',
+        ])
+
+    await micropip.install(f'static-frame=={_SF}')
+    sf = __import__('static_frame')
+    for name in dir(sf):
+        if not name.startswith('_'):
+            setattr(sys.modules[__name__], name, getattr(sf, name))
+    await asyncio.sleep(0)
+
+try:
+    loop = asyncio.get_running_loop()
+except RuntimeError:
+    loop = None
+
+if loop and loop.is_running():
+    print('loop already running')
+    loop.create_task(_load())
+else:
+    print('starting new event loop')
+    asyncio.run(_load())
+
+# delegate interface
+# attempt = 3
+# while attempt > 0:
+#     try:
+#         from static_frame import *
+#         break
+#     except ModuleNotFoundError:
+#         attempt -= 1
+
