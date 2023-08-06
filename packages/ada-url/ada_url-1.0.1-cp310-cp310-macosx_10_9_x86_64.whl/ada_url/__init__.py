b9bebdc699767131780258c2f3655694f5091e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:02 +0000

    Add package ada-url/1.0.1/ada_url-1.0.1-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/ada-url/ada_url-1.0.1-cp310-cp310-macosx_10_9_x86_64.whl/ada_url/__init__.py
@@ -0,0 +1,17 @@
+from ada_url.ada_adapter import (
+    URL,
+    check_url,
+    join_url,
+    normalize_url,
+    parse_url,
+    replace_url,
+)
+
+__all__ = [
+    'URL',
+    'check_url',
+    'join_url',
+    'normalize_url',
+    'parse_url',
+    'replace_url',
+]
