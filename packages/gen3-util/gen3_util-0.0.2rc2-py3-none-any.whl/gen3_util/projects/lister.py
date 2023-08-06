c6a42bb873581b141ceb64938fe6d6e67f4b14c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:32 +0000

    Add package gen3-util/0.0.2rc2/gen3_util-0.0.2rc2-py3-none-any.whl

---
+++ packages/gen3-util/gen3_util-0.0.2rc2-py3-none-any.whl/gen3_util/projects/lister.py
@@ -0,0 +1,38 @@
+
+from gen3.submission import Gen3Submission
+
+from gen3_util.config import Config, ensure_auth
+from gen3_util.projects import get_user, ProjectSummaries, get_projects, ProjectSummary
+
+
+def ls(config: Config) -> ProjectSummaries:
+    """List projects."""
+    auth = ensure_auth(config.gen3.refresh_file)
+    user = get_user(auth=auth)
+    return ProjectSummaries(**{
+        'endpoint': auth.endpoint,
+        'projects': [_ for _ in user['authz'].keys() if _.startswith('/programs')],
+    })
+
+
+def ls(config: Config):
+    """List projects."""
+
+    auth = ensure_auth(config.gen3.refresh_file)
+    submission = Gen3Submission(auth)
+
+    msgs = []
+    projects = get_projects(auth, submission)
+
+    project_messages = {}
+    for _program in projects:
+        for _project in projects[_program]['projects']:
+            project_messages[
+                f"/programs/{_program}/projects/{_project}"
+            ] = ProjectSummary(exists=projects[_program]['projects'][_project])
+
+    return ProjectSummaries(**{
+        'endpoint': auth.endpoint,
+        'projects': project_messages,
+        'messages': msgs
+    })
