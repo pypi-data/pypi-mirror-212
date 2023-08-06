59d60d33e064e38d7cbacebd9aeb8c18195abb46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:46 +0000

    Add package datagen-validation-protocol/0.34.0/datagen_validation_protocol-0.34.0-py3-none-any.whl

---
+++ packages/datagen-validation-protocol/datagen_validation_protocol-0.34.0-py3-none-any.whl/datagen_protocol/schema/humans/presets/expression_presets/fear.py
@@ -0,0 +1,14 @@
+FEAR_PRESET = {
+    "brow_down_left": 1.0,
+    "brow_down_right": 1.0,
+    "brow_inner_up": 1.0,
+    "brow_outer_up_left": 1.0,
+    "brow_outer_up_right": 1.0,
+    "mouth_frown_left": 0.2,
+    "mouth_frown_right": 0.2,
+    "eye_wide_left": 1.0,
+    "eye_wide_right": 1.0,
+    "mouth_stretch_left": 1.0,
+    "mouth_stretch_right": 1.0,
+    "jaw_open": 1.0
+}
---
+++ packages/datagen-validation-protocol/datagen_validation_protocol-0.34.0-py3-none-any.whl/datagen_protocol/schema/humans/presets/expression_presets/surprise.py
@@ -0,0 +1,8 @@
+SURPRISE_PRESET = {
+    "brow_outer_up_left": 1.0,
+    "brow_outer_up_right": 1.0,
+    "brow_inner_up": 1.0,
+    "eye_wide_left": 1.0,
+    "eye_wide_right": 1.0,
+    "jaw_open": 1.0
+}
