a0ed3475e3b7be4cc250a0f5726c57e55513161b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:45 +0000

    Add package neuroimager/0.0.4/neuroimager-0.0.4.tar.gz

---
+++ packages/neuroimager/neuroimager-0.0.4.tar.gz/neuroimager-0.0.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="neuroimager",
+    version="0.0.4",
+    description="A collection of utilities used for MRI data analysis",
+    author="Wetiqe",
+    author_email="jzni132134@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Wetiqe/neuroimager",
+
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scipy",
+        "sklearn",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
