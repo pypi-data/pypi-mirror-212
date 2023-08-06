412a2ea1673dd844e0bbfdfd36e1af9d72b6f6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:56 +0000

    Add package mi-libreria/0.4/mi_libreria-0.4.tar.gz

---
+++ packages/mi-libreria/mi_libreria-0.4.tar.gz/mi_libreria-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="mi_libreria",
+    version="0.4",
+    py_modules=["mi_libreria"],
+    entry_points={
+        "console_scripts": [
+            "mi_libreria = mi_libreria.__main__:main"
+        ]
+    }
+)
