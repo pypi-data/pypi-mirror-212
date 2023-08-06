d9cdf0ae3e78ae3ee635c502f9eb499719e30f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:25 +0000

    Add package byu-pytest-utils/0.6.3/byu_pytest_utils-0.6.3.tar.gz

---
+++ packages/byu-pytest-utils/byu_pytest_utils-0.6.3.tar.gz/byu_pytest_utils-0.6.3/byu_pytest_utils/__init__.py
@@ -0,0 +1,39 @@
+import inspect
+from pathlib import Path
+import pytest
+
+pytest.register_assert_rewrite("byu_pytest_utils.io_checker")
+pytest.register_assert_rewrite("byu_pytest_utils.dialog")
+
+from .utils import run_python_script, with_import, ensure_missing
+from .decorators import max_score, visibility, tags
+from .dialog import dialog
+
+# Type stubs to make these variables discoverable
+# Actual values are supplied by __getattr__
+this_folder: Path
+test_files: Path
+
+
+def _get_caller_file() -> Path:
+    s = inspect.stack()
+    # Find index of the first frame outside byu-pytest-utils
+    index = 0
+    while s[index].filename == __file__:
+        index += 1
+    return Path(s[index].filename).absolute()
+
+
+# We want `this_folder` to be the folder of the caller
+def _get_caller_folder() -> Path:
+    caller_file = _get_caller_file()
+    return caller_file.parent
+
+
+def __getattr__(name):
+    if name == 'this_folder':
+        return _get_caller_folder()
+    elif name == 'test_files':
+        return _get_caller_folder() / 'test_files'
+    else:
+        return globals()[name]
