32230a8c55cf479efaf307cfadb7c75cef181514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:48 +0000

    Add package ksmm/1.0.0/ksmm-1.0.0-py3-none-any.whl

---
+++ packages/ksmm/ksmm-1.0.0-py3-none-any.whl/ksmm/kernel_schema.py
@@ -0,0 +1,38 @@
+kernel_schema = """{
+  "title": "Kernelspec Management Menu",
+  "type": "object",
+  "properties": {
+    "argv": { "type": "array", "items": { "type": "string" }, "title": "" },
+    "env": {
+      "type": "object",
+      "title": "object",
+      "properties": {
+        "EnvVar": { "type": "string" }
+      },
+      "additionalProperties": {
+        "type": "string"
+      }
+    },
+    "display_name": { "type": "string", "title": "Display Name" },
+    "language": { "type": "string", "title": "Programming Language" },
+    "interrupt_mode": {
+      "type": "string",
+      "title": "Interrupt Mode",
+      "enum": ["signal", "message"]
+    },
+    "parameters": {
+      "type": "object",
+      "properties": {
+      }
+    },
+    "metadata": { "type": "object", "title": "" }
+  },
+  "required": [
+    "argv",
+    "display_name",
+    "env",
+    "interrupt_mode",
+    "language",
+    "metadata"
+  ]
+}"""
