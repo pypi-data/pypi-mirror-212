12c6c89618f6ff297669e73c9be6a417d4d4a2a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:28 +0000

    Add package sd-meh/0.5.0/sd_meh-0.5.0.tar.gz

---
+++ packages/sd-meh/sd_meh-0.5.0.tar.gz/sd_meh-0.5.0/sd_meh/merge_methods.py
@@ -0,0 +1,168 @@
+import torch
+from torch import Tensor
+
+
+__all__ = [
+    "weighted_sum",
+    "weighted_subtraction",
+    "tensor_sum",
+    "add_difference",
+    "sum_twice",
+    "triple_sum",
+    "euclidean_add_difference",
+    "multiply_difference",
+    "transmogrify_distribution",
+    "similarity_add_difference",
+    "distribution_crossover",
+    "ties_add_difference",
+]
+
+
+EPSILON = 1e-10  # Define a small constant EPSILON to prevent division by zero
+
+
+def weighted_sum(a: Tensor, b: Tensor, alpha: float, **kwargs) -> Tensor:
+    return (1 - alpha) * a + alpha * b
+
+
+def weighted_subtraction(
+    a: Tensor, b: Tensor, alpha: float, beta: float, **kwargs
+) -> Tensor:
+    # Adjust beta if both alpha and beta are 1.0 to avoid division by zero
+    if alpha == 1.0 and beta == 1.0:
+        beta -= EPSILON
+
+    return (a - alpha * beta * b) / (1 - alpha * beta)
+
+
+def tensor_sum(a: Tensor, b: Tensor, alpha: float, beta: float, **kwargs) -> Tensor:
+    if alpha + beta <= 1:
+        tt = a.clone()
+        talphas = int(a.shape[0] * beta)
+        talphae = int(a.shape[0] * (alpha + beta))
+        tt[talphas:talphae] = b[talphas:talphae].clone()
+    else:
+        talphas = int(a.shape[0] * (alpha + beta - 1))
+        talphae = int(a.shape[0] * beta)
+        tt = b.clone()
+        tt[talphas:talphae] = a[talphas:talphae].clone()
+    return tt
+
+
+def add_difference(a: Tensor, b: Tensor, c: Tensor, alpha: float, **kwargs) -> Tensor:
+    return a + alpha * (b - c)
+
+
+def sum_twice(
+    a: Tensor, b: Tensor, c: Tensor, alpha: float, beta: float, **kwargs
+) -> Tensor:
+    return (1 - beta) * ((1 - alpha) * a + alpha * b) + beta * c
+
+
+def triple_sum(
+    a: Tensor, b: Tensor, c: Tensor, alpha: float, beta: float, **kwargs
+) -> Tensor:
+    return (1 - alpha - beta) * a + alpha * b + beta * c
+
+
+def euclidean_add_difference(
+    a: Tensor, b: Tensor, c: Tensor, alpha: float, **kwargs
+) -> Tensor:
+    distance = (a - c) ** 2 + alpha * (b - c) ** 2
+    try:
+        distance = torch.sqrt(distance)
+    except RuntimeError:
+        distance = torch.sqrt(distance.float()).half()
+    distance = torch.copysign(distance, (a - c) + alpha * (b - c))
+
+    a_norm = torch.linalg.norm(a - c)
+    b_norm = torch.linalg.norm(b - c)
+    target_norm = (1 - alpha / 2) * a_norm + (alpha / 2) * b_norm
+    return c + distance / torch.linalg.norm(distance) * target_norm
+
+
+def multiply_difference(
+    a: Tensor, b: Tensor, c: Tensor, alpha: float, **kwargs
+) -> Tensor:
+    difference = torch.abs((a - c) * (b - c))
+    try:
+        difference = torch.sqrt(difference)
+    except RuntimeError:
+        difference = torch.sqrt(difference.float()).half()
+    difference = torch.copysign(torch.sqrt(difference), a + b - 2 * c)
+    return c + alpha * difference
+
+
+def transmogrify_distribution(a: Tensor, b: Tensor, alpha: float, **kwargs) -> Tensor:
+    a_dist = torch.msort(torch.flatten(a))
+    b_indices = torch.argsort(torch.flatten(b), stable=True)
+    a_redist = torch.gather(a_dist, 0, torch.argsort(b_indices))
+    a_trans = a_redist.reshape(a.shape)
+    return (1 - alpha) * b + alpha * a_trans
+
+
+def similarity_add_difference(
+    a: Tensor, b: Tensor, c: Tensor, alpha: float, beta: float, **kwargs
+) -> Tensor:
+    threshold = torch.maximum(torch.abs(a), torch.abs(b))
+    similarity = ((a * b / threshold**2) + 1) / 2
+    similarity = torch.nan_to_num(similarity * beta, nan=beta)
+
+    ab_diff = a + alpha * (b - c)
+    ab_sum = (1 - alpha / 2) * a + (alpha / 2) * b
+    return (1 - similarity) * ab_diff + similarity * ab_sum
+
+
+def distribution_crossover(
+    a: Tensor, b: Tensor, c: Tensor, alpha: float, beta: float, **kwargs
+):
+    if a.shape == ():
+        return alpha * a + (1 - alpha) * b
+
+    c_indices = torch.argsort(torch.flatten(c))
+    a_dist = torch.gather(torch.flatten(a), 0, c_indices)
+    b_dist = torch.gather(torch.flatten(b), 0, c_indices)
+
+    a_dft = torch.fft.rfft(a_dist.float())
+    b_dft = torch.fft.rfft(b_dist.float())
+
+    dft_filter = torch.arange(0, torch.numel(a_dft), device=a_dft.device).float()
+    dft_filter /= torch.numel(a_dft)
+    if beta > EPSILON:
+        dft_filter = (dft_filter - alpha) / beta + 1 / 2
+        dft_filter = torch.clamp(dft_filter, 0.0, 1.0)
+    else:
+        dft_filter = (dft_filter >= alpha).float()
+
+    x_dft = (1 - dft_filter) * a_dft + dft_filter * b_dft
+    x_dist = torch.fft.irfft(x_dft, a_dist.shape[0])
+    x_values = torch.gather(x_dist, 0, torch.argsort(c_indices))
+    return x_values.reshape_as(a)
+
+
+def ties_add_difference(
+    a: Tensor, b: Tensor, c: Tensor, alpha: float, beta: float, **kwargs
+) -> Tensor:
+    deltas = []
+    signs = []
+    for m in [a, b]:
+        deltas.append(filter_top_k(m - c, beta))
+        signs.append(torch.sign(deltas[-1]))
+
+    signs = torch.stack(signs, dim=0)
+    final_sign = torch.sign(torch.sum(signs, dim=0))
+    delta_filters = (signs == final_sign).float()
+
+    res = torch.zeros_like(c, device=c.device)
+    for delta_filter, delta in zip(delta_filters, deltas):
+        res += delta_filter * delta
+
+    param_count = torch.sum(delta_filters, dim=0)
+    return c + alpha * torch.nan_to_num(res / param_count)
+
+
+def filter_top_k(a: Tensor, k: float):
+    k = max(int((1 - k) * torch.numel(a)), 1)
+    k_value, _ = torch.kthvalue(torch.abs(a.flatten()).float(), k)
+    top_k_filter = (torch.abs(a) >= k_value).float()
+    return a * top_k_filter
