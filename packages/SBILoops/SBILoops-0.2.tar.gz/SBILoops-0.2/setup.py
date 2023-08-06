a8be299ff662111f1cbb2e5f8ea6e10d5efc107f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:55 +0000

    Add package SBILoops/0.2/SBILoops-0.2.tar.gz

---
+++ packages/SBILoops/SBILoops-0.2.tar.gz/SBILoops-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='SBILoops',
+    version='0.2',
+    license='MIT',
+    author="Patrick Gohl",
+    author_email='patrick.gohl@upf.edu',
+    packages=find_packages('SBI'),
+    package_dir={'': 'SBI'},
+    url='https://github.com/structuralbioinformatics/SBI',
+    keywords='Structural Bioinformatics, Loops',
+    install_requires=[
+          'numpy','pandas','pynion','Requests','scipy','six'
+      ],
+
+)
+
