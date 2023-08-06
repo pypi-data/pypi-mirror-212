4bde01805497430007ebf13f11382e775b8ebd66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:58 +0000

    Add package defillamaAPI/0.0.2/defillamaAPI-0.0.2.tar.gz

---
+++ packages/defillamaAPI/defillamaAPI-0.0.2.tar.gz/defillamaAPI-0.0.2/defillamaAPI/__version__.py
@@ -0,0 +1,9 @@
+__title__ = 'defillamaAPI'
+__description__ = ''
+__url__ = 'https://github.com/jafark92/defillamaAPI'
+__version__ = '0.0.2'
+# __build__ = 0x010001
+__author__ = 'Jafar Sadiq'
+__author_email__ = 'jsadiqk92@gmail.com'
+__license__ = 'Apache License 2.0'
+__copyright__ = 'Copyright 2023 Jafar Sadiq'
