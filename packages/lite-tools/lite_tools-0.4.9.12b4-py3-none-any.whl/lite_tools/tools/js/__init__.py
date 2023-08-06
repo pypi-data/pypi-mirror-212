9bd3c2d3fa696a4d164780ea88842b09a6a7ba98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:05 +0000

    Add package lite-tools/0.4.9.12b4/lite_tools-0.4.9.12b4-py3-none-any.whl

---
+++ packages/lite-tools/lite_tools-0.4.9.12b4-py3-none-any.whl/lite_tools/tools/js/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author  : Lodge
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+from lite_tools.tools.js.js_code import atob, btoa
+from lite_tools.tools.js.js_code import to_string_36
