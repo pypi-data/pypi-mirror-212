c1cb57f6b41646c69cd6172277103814ab1ffeb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:15 +0000

    Add package StrangeSerializerLab3/15.0/StrangeSerializerLab3-15.0.tar.gz

---
+++ packages/StrangeSerializerLab3/StrangeSerializerLab3-15.0.tar.gz/StrangeSerializerLab3-15.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="StrangeSerializerLab3",
+    version="15.0",
+    description="Serializer for lab_3",
+    author="Mihail Kornievich",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent"
+    ],
+    packages=["StrangeSerializerLab3", "StrangeSerializerLab3/StrangeJSON", "StrangeSerializerLab3/StrangeXML"],
+    include_package_data=True
+)
