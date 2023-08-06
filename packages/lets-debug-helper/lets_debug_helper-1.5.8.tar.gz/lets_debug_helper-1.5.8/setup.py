7a3553f803f3b0069716cef986ebb00c107148d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:18 +0000

    Add package lets-debug-helper/1.5.8/lets_debug_helper-1.5.8.tar.gz

---
+++ packages/lets-debug-helper/lets_debug_helper-1.5.8.tar.gz/lets_debug_helper-1.5.8/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['letsdebughelper', 'letsdebughelper.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['argparse>=1.4.0,<2.0.0',
+ 'requests>=2.28.2,<3.0.0',
+ 'rich>=13.3.2,<14.0.0',
+ 'six>=1.16.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['lets-debug = letsdebughelper.letsdebug:main']}
+
+setup_kwargs = {
+    'name': 'lets-debug-helper',
+    'version': '1.5.8',
+    'description': "This is a cli tool that interacts with the Let's Debug API",
+    'long_description': 'None',
+    'author': 'Jeffrey Crane',
+    'author_email': 'jediknight11206@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
