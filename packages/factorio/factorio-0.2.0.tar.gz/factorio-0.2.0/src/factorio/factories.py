0c0f861bd00ee4d84cdfca4cbb07322edbe31c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:46 +0000

    Add package factorio/0.2.0/factorio-0.2.0.tar.gz

---
+++ packages/factorio/factorio-0.2.0.tar.gz/factorio-0.2.0/src/factorio/factories.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import Any
+
+from factorio.fields import AbstractField
+
+
+class Factory:
+    class Meta:
+        model: type
+        fields: dict[str, Any]
+
+    @classmethod
+    def build(cls, **kwargs: Any) -> Any:
+        fields: dict[str, Any] = {}
+        for key, value in cls.Meta.fields.items():
+            if key in kwargs:
+                value = kwargs[key]  # noqa: PLW2901
+
+            fields[key] = value() if isinstance(value, AbstractField) else value
+
+        return cls.Meta.model(**fields)
