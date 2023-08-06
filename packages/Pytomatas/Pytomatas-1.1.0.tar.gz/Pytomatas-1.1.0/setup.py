6e7a96f9d5cda9fc8013ffbd5570f4b9c4c14ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:05 +0000

    Add package Pytomatas/1.1.0/Pytomatas-1.1.0.tar.gz

---
+++ packages/Pytomatas/Pytomatas-1.1.0.tar.gz/Pytomatas-1.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setup(
+    name="Pytomatas",
+    version="1.1.0",
+    author="arhcoder",
+    author_email="arhcoder@gmail.com",
+    description="Simulates Automatons Acceptors DFA, NFA, PDA and Turing Machines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arhcoder/Pytomatas",
+    license="MIT License",
+    packages=find_packages(),
+    include_package_data=True,
+    project_urls={
+        "Bug Tracker": "https://github.com/arhcoder/Pytomatas/issues",
+        "Contribution": "https://github.com/arhcoder/Pytomatas/pulls",
+        "Author": "https://github.com/arhcoder"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.6"
+)
