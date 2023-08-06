f6ef269408901ea5dce3aca0776ac1a6990a5d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:14 +0000

    Add package flippers/0.0.1b4/flippers-0.0.1b4.tar.gz

---
+++ packages/flippers/flippers-0.0.1b4.tar.gz/flippers-0.0.1b4/flippers/models/__init__.py
@@ -0,0 +1,6 @@
+"""Groups label generative models."""
+
+from ._base import BalancedVoter, Voter  # noqa: F403
+from ._snorkel import SnorkelModel
+
+__all__ = ["Voter", "BalancedVoter", "SnorkelModel"]
