19bb6660c29d5d564fbba1a2346aa2f07ef999a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:15 +0000

    Add package dynetx/0.3.2/dynetx-0.3.2.tar.gz

---
+++ packages/dynetx/dynetx-0.3.2.tar.gz/dynetx-0.3.2/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__author__ = 'Giulio Rossetti'
+__license__ = "BSD"
+__email__ = "giulio.rossetti@gmail.com"
+
+# Get the long description from the README file
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+
+setup(name='dynetx',
+      version='0.3.2',
+      license='BSD-2-Clause',
+      description='Dynamic Network library',
+      url='https://github.com/GiulioRossetti/dynetx',
+      author='Giulio Rossetti',
+      author_email='giulio.rossetti@gmail.com',
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 4 - Beta ',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: BSD License',
+
+          "Operating System :: OS Independent",
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3'
+      ],
+      keywords='dynamic-networks',
+      install_requires=requirements,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test", "dynetx.test", "dynetx.test.*"]),
+      )
