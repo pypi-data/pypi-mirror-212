04e8279f46c55666c41f2c0640f0a9830d7c3b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:14 +0000

    Add package mount-azure-blob/0.0.7/mount_azure_blob-0.0.7.tar.gz

---
+++ packages/mount-azure-blob/mount_azure_blob-0.0.7.tar.gz/mount_azure_blob-0.0.7/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = "mount_azure_blob",
+    version = "0.0.7",
+    author = "Lalkrishna",
+    url = "https://github.com/lkarjun/mount-azure-blob-storage/",
+    description = "Mount Azure blob storage in google colab.",
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    py_modules=['mount_azure_blob'],
+    install_requires = ["ipywidgets", "python-dotenv"],
+    python_requires = ">=3.7",
+    zip_safe = False,
+)
