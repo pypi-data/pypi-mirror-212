a9d5d99ff02fd51f212688fd31f8f2dcf4f4b78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:37 +0000

    Add package plaquette-graph/0.0.1a0/plaquette_graph-0.0.1a0-cp311-cp311-win_amd64.whl

---
+++ packages/plaquette-graph/plaquette_graph-0.0.1a0-cp311-cp311-win_amd64.whl/plaquette_graph/__init__.py
@@ -0,0 +1,7 @@
+"""Top level plaquette_graph module."""
+from plaquette_graph_bindings import SparseGraphRow
+from plaquette_graph_bindings import SparseGraph
+from plaquette_graph_bindings import DecodingGraph
+from plaquette_graph_bindings import MultiGraph
+
+__version__ = "0.0.1-alpha"
