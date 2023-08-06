fb0633ab33d96c814287b129e336bab1877c03ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:06 +0000

    Add package pwl-chat-python/1.4.5/pwl-chat-python-1.4.5.tar.gz

---
+++ packages/pwl-chat-python/pwl-chat-python-1.4.5.tar.gz/pwl-chat-python-1.4.5/src/utils/utils.py
@@ -0,0 +1,22 @@
+HOST = 'https://fishpi.cn'
+UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+
+HELP = '输入#help获得命令提示列表'
+
+COMMAND_GUIDE = '''
+[#cli] 进入命令交互模式
+[#chatroom] 进入聊天室模式
+[#checked] 查看当前是否签到
+[#reward] 领取昨日活跃奖励
+[#point] 查看当前个人积分
+[#online-users] 查看当前在线的用户列表
+[#user username] 输入 #user 用户名 可查看此用户详细信息 (#user Gakkiyomi)
+[#blacklist] 查看黑名单列表
+[#ban username] 将某人送入黑名单
+[#unban username] 将某人解除黑名单
+[#liveness] 查看当前活跃度(⚠️慎用，如果频繁请求此命令(最少间隔30s)，登录状态会被直接注销,需要重启脚本！)
+'''
+
+RPS_SUCCESS = '![](https://file.fishpi.cn/2023/01/XN3Y9-cf3997b7.jpeg)'
+RPS_LOSED = '![](https://pwl.stackoverflow.wiki/2022/04/MB2SCYFZFVT2DQ0GI7-c6447c10.jpg)'
+RPS_ZERO = '![](https://file.fishpi.cn/2023/05/1683183148506-4c31497e.png)'
