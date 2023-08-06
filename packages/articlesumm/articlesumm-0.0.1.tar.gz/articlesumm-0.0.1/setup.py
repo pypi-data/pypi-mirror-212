5e6237470b359622ac983b81caffa37b824c95a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:31 +0000

    Add package articlesumm/0.0.1/articlesumm-0.0.1.tar.gz

---
+++ packages/articlesumm/articlesumm-0.0.1.tar.gz/articlesumm-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='articlesumm',
+  version='0.0.1',
+  description='Custom scientific/research article summarization library based on Statistical features',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Maxwell Tetteh',
+  author_email='tettehmaxwell11@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='text summarization, scientific articles', 
+  packages=find_packages(),
+  install_requires=[
+    'requests>=2.26.0',
+    'nltk>=3.6.5',
+    ]
+)
