fe9560a587753279f675093fbbde0d27030db1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:31 +0000

    Add package makerpack/0.1.0/makerpack-0.1.0.tar.gz

---
+++ packages/makerpack/makerpack-0.1.0.tar.gz/makerpack-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='makerpack',
+version='0.1.0',
+description='Package for maker kit and every other makers.',
+long_description=long_description,
+long_description_content_type='text/markdown',
+author='step maker',
+author_email='pack-dev@stepmaker.co',
+license='CC BY-NC-SA 4.0',
+keywords=['stepmaker', 'step', 'maker', 'micropython', 'makerpack', 'pack', 'takeup'],
+packages=['makerpack']
+)
