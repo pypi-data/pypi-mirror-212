356170e85ee0dd3599da8aea53f5d34641995a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:34 +0000

    Add package tcare-sbvalidator/0.0.8/tcare_sbvalidator-0.0.8-py3-none-any.whl

---
+++ packages/tcare-sbvalidator/tcare_sbvalidator-0.0.8-py3-none-any.whl/tcare_sbvalidator/sms_handler.py
@@ -0,0 +1,38 @@
+import json, asyncio
+from datetime import datetime
+from .models.cloudevents import SmsServiceBusMessage
+from azure.identity.aio import DefaultAzureCredential
+from azure.servicebus import ServiceBusMessage
+from azure.servicebus.aio import ServiceBusClient
+
+class Sms:
+
+    def validate_json(self, message):
+        parsed_json = json.loads(message)
+        return SmsServiceBusMessage(**parsed_json)
+
+    def build_json(self, id, recipient, sender, content, options=None):
+        try:
+            return json.dumps({
+                "specversion": "1.0.0",
+                "type": "servicebusevent",
+                "id": id,
+                "time": datetime.now(),
+                "datacontenttype": "application/json",
+                "data": {
+                    "type": "sms",
+                    "content": content,
+                    "recipient": recipient,
+                    "sender": sender
+                },
+                "options": options,
+            }, default=str)
+        except Exception as e:
+            print(e)
+
+    async def publish_message(self, namespace, topic, message, cred=DefaultAzureCredential()):
+        async with ServiceBusClient(namespace, cred) as client:
+            async with client.get_topic_sender(topic) as sender:
+                msg = ServiceBusMessage(message)
+                await sender.send_messages(msg)
+                print(f"message sent with content: {message}")
