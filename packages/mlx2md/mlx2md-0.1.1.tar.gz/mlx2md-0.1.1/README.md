f1430a019fec0a06afeec4f41c3602546f2ba004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:28 +0000

    Add package mlx2md/0.1.1/mlx2md-0.1.1.tar.gz

---
+++ packages/mlx2md/mlx2md-0.1.1.tar.gz/mlx2md-0.1.1/README.md
@@ -0,0 +1,11 @@
+<h1>MATLAB `.mlx` to Markdown `.md` Converter</h1>
+
+<h2>Installation</h2>
+```
+pip install mlx2md
+```
+
+<h2>Usage</h2>
+```
+python -m mlx2md -i example.mlx
+```
---
+++ packages/mlx2md/mlx2md-0.1.1.tar.gz/mlx2md-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='mlx2md',
+    version='0.1.1',    
+    description='Matlab mlx to Markdown format converter',
+    url='https://github.com/alient12/mlx2md',
+    author='Ali Entezari',
+    author_email='ali_ent12@yahoo.com',
+    license='BSD 2-clause',
+    packages=['mlx2md'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
