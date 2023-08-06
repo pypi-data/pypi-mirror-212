7b942ce56be3dbd9c5efb860a624dd20ce0de516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:02 +0000

    Add package nwebclient/1.0.150/nwebclient-1.0.150.tar.gz

---
+++ packages/nwebclient/nwebclient-1.0.150.tar.gz/nwebclient-1.0.150/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nwebclient",
+    version="1.0.150",
+    author="Bjoern Salgert",
+    author_email="bjoern.salgert@hs-duesseldorf.de",
+    description="NWebClient via HTTP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bsnx.net/4.0/group/pynwebclient",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'nx-sdb = nwebclient.sdb:main',
+            'nx-sdb-count =  nwebclient.sdb:count',
+            'nx-c =  nwebclient:main',
+            'npy-ticker = nwebclient.ticker:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["usersettings>=1.0.7"]
+)
