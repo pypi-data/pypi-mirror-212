d2fbf3632deaed4d243993532d4019de45b51151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:31 +0000

    Add package cohere-core/4.0b0/cohere_core-4.0b0.tar.gz

---
+++ packages/cohere-core/cohere_core-4.0b0.tar.gz/cohere_core-4.0b0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+      name='cohere_core',
+      author = 'Barbara Frosik, Ross Harder',
+      author_email = 'bfrosik@anl.gov',
+      url='https://github.com/advancedPhotonSource/cohere',
+      version='4.0-beta',
+      packages=setuptools.find_packages(),
+      install_requires=['numpy',
+                        'scikit-learn',
+                        'tifffile',
+                        ],
+      classifiers=[
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+      ],
+
+)
