6ae4e757f7fed4b0f26a302218f98ec1ceb8ba4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:16 +0000

    Add package pyrocko/2023.6.7/pyrocko-2023.6.7-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyrocko/pyrocko-2023.6.7-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrocko/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = 'cea84da041c5345569c77a43e6a0261fd24b06a9'
+local_modifications = False
+version = '2023.06.07'
+long_version = 'pyrocko-2023.06.07-cea84da041c5345569c77a43e6a0261fd24b06a9-2023-06-07_10:52:10'  # noqa
+installed_date = '2023-06-07_10:52:10'
+src_path = '/drone/src'
