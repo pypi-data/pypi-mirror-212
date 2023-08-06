fed8e55f661be204bd7defd3c03358f424281c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:17 +0000

    Add package bionty/0.17a3/bionty-0.17a3.tar.gz

---
+++ packages/bionty/bionty-0.17a3.tar.gz/bionty-0.17a3/bionty/dev/__init__.py
@@ -0,0 +1,15 @@
+"""Developer API.
+
+.. autosummary::
+   :toctree: .
+
+   ontology_info
+   latest_db_version
+"""
+from ._fix_index import (
+    check_if_index_compliant,
+    get_compliant_index_from_column,
+    explode_aggregated_column_to_expand,
+)  # noqa
+
+from ._ontology import ontology_info
