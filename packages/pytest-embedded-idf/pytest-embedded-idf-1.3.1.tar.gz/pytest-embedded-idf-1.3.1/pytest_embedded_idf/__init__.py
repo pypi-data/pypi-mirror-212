84afc3edd3f6cf4c1bf5ead4435db2768c015f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:21 +0000

    Add package pytest-embedded-idf/1.3.1/pytest-embedded-idf-1.3.1.tar.gz

---
+++ packages/pytest-embedded-idf/pytest-embedded-idf-1.3.1.tar.gz/pytest-embedded-idf-1.3.1/pytest_embedded_idf/__init__.py
@@ -0,0 +1,32 @@
+import importlib
+
+from pytest_embedded.utils import lazy_load
+
+from .app import IdfApp
+from .linux import LinuxDut, LinuxSerial
+from .unity_tester import CaseTester, UnittestMenuCase
+
+__getattr__ = lazy_load(
+    importlib.import_module(__name__),
+    {
+        'IdfApp': IdfApp,
+        'LinuxDut': LinuxDut,
+        'LinuxSerial': LinuxSerial,
+        'CaseTester': CaseTester,
+    },
+    {
+        'IdfSerial': '.serial',
+        'IdfDut': '.dut',
+    },
+)
+
+
+__all__ = [
+    'IdfApp',
+    'IdfSerial',
+    'IdfDut',
+    'CaseTester',
+    'LinuxSerial',
+    'LinuxDut',
+    'UnittestMenuCase',
+]
