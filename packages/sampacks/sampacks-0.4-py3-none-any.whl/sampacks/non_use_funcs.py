38a698abecac0d19569c71a6efed98ace6236b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:55 +0000

    Add package sampacks/0.4/sampacks-0.4-py3-none-any.whl

---
+++ packages/sampacks/sampacks-0.4-py3-none-any.whl/sampacks/non_use_funcs.py
@@ -0,0 +1,53 @@
+# Sub_Funcs 
+import os
+import requests
+
+def image_downloader(url, extension, **kwargs):
+    """
+        Downloads a file from a given url.
+        Args:
+            url: Url For The File You Wanna Download.
+        Kwargs:
+            name: Enter The Name Of The Image.
+            chunk: Chunck_Size For Iter_Content.
+            location: Where To Save The Image.
+        Returns:
+            The Downloaded Image
+    """
+
+    if 'name' in kwargs:
+        name = kwargs['name'] + extension
+    else:
+        name = 'default.png' 
+    
+    if 'chunk' in kwargs:
+        chunk = kwargs['chunk']
+    else:
+        chunk = 500
+
+    if 'location' in kwargs:
+        location = kwargs['location']
+    else:
+        location = ''
+
+    try:
+        os.chdir(location)
+    except OSError:
+        pass
+    request = requests.get(url, stream=True)
+    with open(name, 'wb') as file:
+        for i in request.iter_content(chunk_size=chunk):
+            file.write(i)
+
+
+def image_check(url):
+    if url.split('/')[2] == 'encrypted-tbn0.gstatic.com' or url.endswith('.png') or ''.join(url.split('/')[1]).startswith('png'): 
+        extension = '.png'
+        return [True, extension]
+    elif url.endswith('.jpg') or ''.join(url.split('/')[1]).startswith('jpg'): 
+        extension = '.jpg'
+        return [True, extension]
+    else:
+        return False
+
+
