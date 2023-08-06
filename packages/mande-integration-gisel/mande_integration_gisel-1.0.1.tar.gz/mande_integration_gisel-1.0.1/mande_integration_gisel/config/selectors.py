0304d5f0ee5978979ca4892d5df58b84940b6700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:55 +0000

    Add package mande-integration-gisel/1.0.1/mande_integration_gisel-1.0.1.tar.gz

---
+++ packages/mande-integration-gisel/mande_integration_gisel-1.0.1.tar.gz/mande_integration_gisel-1.0.1/mande_integration_gisel/config/selectors.py
@@ -0,0 +1,18 @@
+
+
+class BrowserSelector():
+  
+  LOGIN_SUBMIT_BUTTON = "input[type='submit']"
+  LOGIN_USERNAME = "input[name='user']"
+  LOGIN_PASSWORD = "input[name='password']"
+  
+  GRANT_MENU_BAR = 'actiontabbar'
+  GRANT_LIST_CSS_SELECTOR = 'a[id="214"]'
+  GRANT_TABLE_DATA_ROW_CSS_SELECTOR = 'tr'
+  GRANT_TABLE_DATA_COLUMN_CSS_SELECTOR = 'td'
+  GRANT_PROJECT_CODE = "//td[normalize-space()='{}']"
+  GRANT_LIST_TABLE_ID='table_1890'
+  CUSTABCONTAINER = 'custabcontainer'
+  
+  
+  
