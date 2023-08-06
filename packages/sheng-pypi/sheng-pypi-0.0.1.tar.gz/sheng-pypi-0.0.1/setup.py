0fd329920a68ea4a318ed27ef719b587cb1e8a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:54 +0000

    Add package sheng-pypi/0.0.1/sheng-pypi-0.0.1.tar.gz

---
+++ packages/sheng-pypi/sheng-pypi-0.0.1.tar.gz/sheng-pypi-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'sheng-pypi'
+
+# Setting up
+setup(
+    name="sheng-pypi",
+    version=VERSION,
+    author="Sheng",
+    author_email="shengpang@lbl.gov",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
