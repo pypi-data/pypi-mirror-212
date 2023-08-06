a4ddc0bc81c8b4a2a874b4e990080a41bbfeb316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:48 +0000

    Add package porespy/2.3.0/porespy-2.3.0.tar.gz

---
+++ packages/porespy/porespy-2.3.0.tar.gz/porespy-2.3.0/porespy/filters/imagej/__init__.py
@@ -0,0 +1,20 @@
+r"""
+
+imagej
+######
+
+**Access ImageJ Filters**
+
+.. currentmodule:: porespy
+
+.. autosummary::
+   :template: mybase.rst
+   :toctree: generated/
+
+    filters.imagej.imagej_wrapper
+    filters.imagej.imagej_plugin
+
+"""
+
+from ._funcs import imagej_wrapper
+from ._funcs import imagej_plugin
