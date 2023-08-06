9f5f49e17cd064ea8c052121a7d6418131f3084a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:30 +0000

    Add package generalfile/2.5.16/generalfile-2.5.16.tar.gz

---
+++ packages/generalfile/generalfile-2.5.16.tar.gz/generalfile-2.5.16/setup.py
@@ -0,0 +1,42 @@
+
+from setuptools import setup, find_namespace_packages
+from pathlib import Path
+
+try:
+    long_description = (Path(__file__).parent / 'README.md').read_text(encoding='utf-8')
+except FileNotFoundError:
+    long_description = 'Readme missing'
+
+setup(
+    name="generalfile",
+    author='Rickard "Mandera" Abraham',
+    author_email="rickard.abraham@gmail.com",
+    version="2.5.16",
+    description="Easily manage files cross platform.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'generalimport',
+        'generallibrary',
+        'send2trash',
+        'appdirs',
+        'dill',
+    ],
+    url="https://github.com/ManderaGeneral/generalfile",
+    license="mit",
+    packages=find_namespace_packages(exclude=("build*", "dist*")),
+    extras_require={
+        'spreadsheet': ['pandas'],
+        'full': ['pandas'],
+    },
+    classifiers=[
+        'Topic :: Desktop Environment :: File Managers',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+    ],
+)
