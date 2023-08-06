8497921358bbfdddecb4a93658aa91b5852928ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:31 +0000

    Add package Tool-Charaf/0.1/Tool-Charaf-0.1.tar.gz

---
+++ packages/Tool-Charaf/Tool-Charaf-0.1.tar.gz/Tool-Charaf-0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name= 'Tool-Charaf',
+    version= '0.1',
+    author= 'Saaoudi_Charaf',
+    description= 'simple library in python for test',
+    packages=setuptools.find_packages(),
+    classifiliers=["Programming Language :: 3",
+    "Operating System :: Os indenpendent",
+    "License :: OSI Approved :: MIT License"])
