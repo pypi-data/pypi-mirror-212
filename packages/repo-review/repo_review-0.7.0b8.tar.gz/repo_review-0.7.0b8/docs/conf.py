f925ea2a76955b8163a89e7722f49c9eaec89349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:52 +0000

    Add package repo-review/0.7.0b8/repo_review-0.7.0b8.tar.gz

---
+++ packages/repo-review/repo_review-0.7.0b8.tar.gz/repo_review-0.7.0b8/docs/conf.py
@@ -0,0 +1,32 @@
+import importlib.metadata
+
+project = "repo-review"
+copyright = "2022, Henry Schreiner"
+author = "Henry Schreiner"
+release = importlib.metadata.version("repo-review").split("+")[0]
+version = ".".join(release.split(".")[:2])
+
+extensions = [
+    "myst_parser",
+    "sphinxcontrib.programoutput",
+    "sphinxext.opengraph",
+    "sphinx_copybutton",
+]
+
+source_suffix = [".rst", ".md"]
+
+exclude_patterns = [
+    "_build",
+    "**.ipynb_checkpoints",
+    "Thumbs.db",
+    ".DS_Store",
+    ".env",
+    ".venv",
+]
+
+html_theme = "furo"
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
