92a0fa97c4512f6249eaacf64f691b257ef003e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:43 +0000

    Add package apapi/0.5.0/apapi-0.5.0.tar.gz

---
+++ packages/apapi/apapi-0.5.0.tar.gz/apapi-0.5.0/apapi/connection.py
@@ -0,0 +1,19 @@
+"""
+apapi.connection
+
+This module provides Connection class, which contains all available API functions.
+"""
+
+from .alm import ALMConnection
+from .audit import AuditConnection
+from .bulk import BulkConnection
+from .transactional import TransactionalConnection
+
+
+class Connection(
+    ALMConnection,
+    AuditConnection,
+    BulkConnection,
+    TransactionalConnection,
+):
+    """Anaplan connection session. Provides all available API functions."""
