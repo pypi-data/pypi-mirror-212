ab4f1bb06743553696dd54a2fad48b2792b38b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:56 +0000

    Add package hojichar/0.5.3/hojichar-0.5.3.tar.gz

---
+++ packages/hojichar/hojichar-0.5.3.tar.gz/hojichar-0.5.3/hojichar/__init__.py
@@ -0,0 +1,23 @@
+"""
+.. include:: ../README.md
+"""
+from .core.composition import Compose
+from .core.filter_interface import Filter, TokenFilter
+from .core.models import Document, Token
+from .filters import deduplication, document_filters, token_filters, tokenization
+
+__version__ = "0.5.3"  # Replaced by poetry-dynamic-versioning when deploying
+
+__all__ = [
+    "core",
+    "filters",
+    "Compose",
+    "Filter",
+    "TokenFilter",
+    "Document",
+    "Token",
+    "deduplication",
+    "document_filters",
+    "token_filters",
+    "tokenization",
+]
