aaf719edfcecf6ec1e5c54dd17232c111594dec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:31 +0000

    Add package brunotest/0.1/brunotest-0.1.tar.gz

---
+++ packages/brunotest/brunotest-0.1.tar.gz/brunotest-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name="brunotest",
+    version="0.1",
+    author="Robert Scheidegger",
+    author_email="robert_scheidegger@brown.edu",
+    license="MIT",
+    description="<short description for the tool>",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Brown-Deep-Learning/brunotest",
+    py_modules=["brunotest"],
+    packages=find_packages(),
+    install_requires=[requirements],
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    entry_points="""
+        [console_scripts]
+        brunotest=brunotest:brunotest_cli_entry
+    """,
+)
