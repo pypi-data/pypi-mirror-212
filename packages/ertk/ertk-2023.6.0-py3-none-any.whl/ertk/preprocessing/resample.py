1e70386f7a04713f340494778c442e0457c96fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:21 +0000

    Add package ertk/2023.6.0/ertk-2023.6.0-py3-none-any.whl

---
+++ packages/ertk/ertk-2023.6.0-py3-none-any.whl/ertk/preprocessing/resample.py
@@ -0,0 +1,42 @@
+"""Audio resampling using resampy."""
+
+from dataclasses import dataclass
+from typing import List
+
+import numpy as np
+import resampy
+
+from ertk.config import ERTKConfig
+
+from ._base import AudioClipProcessor
+
+__all__ = ["ResampleConfig", "Resampler"]
+
+
+@dataclass
+class ResampleConfig(ERTKConfig):
+    """Resample configuration."""
+
+    sample_rate: int = 16000
+    """Target sample rate."""
+    filter: str = "kaiser_fast"
+    """Resampling filter."""
+
+
+class Resampler(AudioClipProcessor, fname="resample", config=ResampleConfig):
+    """Audio resampler using resampy."""
+
+    config: ResampleConfig
+
+    def process_instance(self, x: np.ndarray, **kwargs) -> np.ndarray:
+        return resampy.resample(
+            x,
+            kwargs.pop("sr"),
+            self.config.sample_rate,
+            filter=self.config.filter,
+            parallel=True,
+        )
+
+    @property
+    def feature_names(self) -> List[str]:
+        return ["pcm"]
