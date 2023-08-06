fa8833d7c08bc861c1a5d68dac4763492c44504c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:54 +0000

    Add package kimspdf/1.0/kimspdf-1.0.tar.gz

---
+++ packages/kimspdf/kimspdf-1.0.tar.gz/kimspdf-1.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="kimspdf",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["text", "data"])
+
+
+
+
+
+)
