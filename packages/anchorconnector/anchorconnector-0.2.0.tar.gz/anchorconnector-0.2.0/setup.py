8a2b77b04d169a3bef6645e39eb1bd71907a2a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:56 +0000

    Add package anchorconnector/0.2.0/anchorconnector-0.2.0.tar.gz

---
+++ packages/anchorconnector/anchorconnector-0.2.0.tar.gz/anchorconnector-0.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="anchorconnector",
+    packages=find_packages(include=["anchorconnector"]),
+    version="0.2.0",
+    description="Anchor Connector for Podcast Data",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Open Podcast",
+    license="MIT",
+    entry_points={
+        "console_scripts": [
+            "anchorconnector = anchorconnector.__main__:main",
+        ]
+    },
+    install_requires=["requests", "loguru", "PyYAML", "tenacity"],
+)
