aba08ced0bd84f6da574955ba14afcbb401c3f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:50 +0000

    Add package wikibaseintegrator/0.12.4/wikibaseintegrator-0.12.4-py3-none-any.whl

---
+++ packages/wikibaseintegrator/wikibaseintegrator-0.12.4-py3-none-any.whl/wikibaseintegrator/__init__.py
@@ -0,0 +1,21 @@
+"""
+WikibaseIntegrator Library
+~~~~~~~~~~~~~~~~~~~~~
+
+WikibaseIntegrator is a Wikibase library, written in Python, for human beings.
+Basic read usage:
+
+   >>> from wikibaseintegrator import WikibaseIntegrator
+   >>> from wikibaseintegrator.wbi_config import config
+   >>> config['USER_AGENT'] = 'Item Get Notebook'
+   >>> wbi = WikibaseIntegrator()
+   >>> q42 = wbi.item.get('Q42')
+   >>> q42.labels.get('en').value
+   'Douglas Adams'
+
+Full documentation is available at <https://wikibaseintegrator.readthedocs.io/>.
+"""
+
+from .wikibaseintegrator import WikibaseIntegrator
+
+__version__ = '0.12.4'
---
+++ packages/wikibaseintegrator/wikibaseintegrator-0.12.4-py3-none-any.whl/wikibaseintegrator/models/sitelinks.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import Dict, List, Optional
+
+from wikibaseintegrator.models.basemodel import BaseModel
+
+
+class Sitelinks(BaseModel):
+    def __init__(self) -> None:
+        self.sitelinks: Dict[str, Sitelink] = {}
+
+    def get(self, site: Optional[str] = None) -> Optional[Sitelink]:
+        if site in self.sitelinks:
+            return self.sitelinks[site]
+
+        return None
+
+    def set(self, site: str, title: Optional[str] = None, badges: Optional[List[str]] = None) -> Sitelink:
+        sitelink = Sitelink(site, title, badges)
+        self.sitelinks[site] = sitelink
+        return sitelink
+
+    def from_json(self, json_data: Dict[str, Dict]) -> Sitelinks:
+        for sitelink in json_data:
+            self.set(site=json_data[sitelink]['site'], title=json_data[sitelink]['title'], badges=json_data[sitelink]['badges'])
+
+        return self
+
+    def __len__(self):
+        return len(self.sitelinks)
+
+
+class Sitelink(BaseModel):
+    def __init__(self, site: Optional[str] = None, title: Optional[str] = None, badges: Optional[List[str]] = None):
+        self.site = site
+        self.title = title
+        self.badges: List[str] = badges or []
+
+    def __str__(self):
+        return self.title
