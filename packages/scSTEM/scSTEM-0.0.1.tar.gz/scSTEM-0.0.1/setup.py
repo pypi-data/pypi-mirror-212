e0bd7e64abe82a3dd9f83c94ea3551f8f7269567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:26 +0000

    Add package scSTEM/0.0.1/scSTEM-0.0.1.tar.gz

---
+++ packages/scSTEM/scSTEM-0.0.1.tar.gz/scSTEM-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scSTEM",
+    version="0.0.1",
+    author="whirl",
+    author_email="hmsh653@gmail.com",
+    description="A Method for Mapping Single-cell and Spatial Transcriptomics Data with Transfer Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/WhirlFirst/STEM",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "pytorch>=1.13.1",
+        "scanpy>=1.8.2",
+        "tqdm>=4.60.0",
+        "seaborn>=0.11.1",
+    ],
+)
