c5aab5ac283176660a654c3619bd6a6425d5c1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:10 +0000

    Add package rosys/0.8.4/rosys-0.8.4.tar.gz

---
+++ packages/rosys/rosys-0.8.4.tar.gz/rosys-0.8.4/rosys/hardware/communication/serial_communication.py
@@ -0,0 +1,93 @@
+import os
+from typing import Optional
+
+import serial
+from nicegui import ui
+from nicegui.events import ValueChangeEventArguments
+
+from ... import rosys
+from .communication import Communication
+
+
+class SerialCommunication(Communication):
+    """This module implements a communication via a serial device with a given baud rate.
+
+    It contains a list of search paths for finding the serial device.
+    """
+
+    search_paths: list[str] = [
+        '/dev/tty.SLAB_USBtoUART',
+        '/dev/ttyTHS1',
+        '/dev/ttyUSB0',
+    ]
+
+    def __init__(self, baud_rate: int = 115200) -> None:
+        super().__init__()
+        self.device_path = self.get_device_path()
+        if self.device_path is None:
+            raise Exception('No serial port found')
+        self.log.debug(f'connecting serial on {self.device_path} with baud rate {baud_rate}')
+        self.serial = serial.Serial(self.device_path, baud_rate)
+        self.buffer = ''
+        self.log_io: bool = False
+
+    @staticmethod
+    def is_possible() -> bool:
+        return SerialCommunication.get_device_path() is not None
+
+    @staticmethod
+    def get_device_path() -> Optional[str]:
+        for device_path in SerialCommunication.search_paths:
+            if os.path.exists(device_path) and os.stat(device_path).st_gid > 0:
+                return device_path
+
+    def connect(self) -> None:
+        if not self.serial.isOpen():
+            self.serial.open()
+            self.log.debug(f'reconnected serial on {self.device_path}')
+
+    def disconnect(self) -> None:
+        if self.serial.isOpen():
+            self.serial.close()
+            self.log.debug(f'disconnected serial on {self.device_path}')
+
+    async def read(self) -> Optional[str]:
+        if not self.serial.isOpen():
+            return
+        s = self.serial.read_all()
+        try:
+            s = s.decode()
+            self.buffer += s
+            if '\n' in self.buffer:
+                line, self.buffer = self.buffer.split('\r\n', 1)
+                if self.log_io:
+                    self.log.debug(f'read: {line}')
+                return line
+        except Exception:
+            self.log.exception(f'Could not decode serial data: {s}')
+
+    async def send(self, line: str) -> None:
+        if not self.serial.isOpen():
+            return
+        self.serial.write(f'{line}\n'.encode())
+        if self.log_io:
+            self.log.debug(f'send: {line}')
+
+    def debug_ui(self) -> None:
+        super().debug_ui()
+
+        async def submit_input() -> None:
+            await self.send(input.value)
+            input.value = ''
+
+        def toggle(e: ValueChangeEventArguments) -> None:
+            if e.value:
+                self.connect()
+                rosys.notify('connected to Lizard')
+            else:
+                self.disconnect()
+                rosys.notify('disconnected from Lizard')
+
+        ui.switch('Serial Communication', value=self.serial.isOpen(), on_change=toggle)
+        ui.switch('Serial Logging').bind_value(self, 'log_io')
+        input = ui.input('Serial Command', on_change=submit_input)
---
+++ packages/rosys/rosys-0.8.4.tar.gz/rosys-0.8.4/rosys/helpers.py
@@ -0,0 +1,70 @@
+import inspect
+import time
+from contextlib import contextmanager
+from typing import Any, Awaitable, Callable, Generator
+
+import numpy as np
+
+
+async def invoke(handler: Callable, *args: Any) -> Any:
+    result = handler(*args)
+    if isinstance(result, Awaitable):
+        result = await result
+    return result
+
+
+def measure(*, reset: bool = False, ms: bool = False) -> None:
+    global t
+    if 't' in globals() and not reset:
+        dt = time.perf_counter() - t
+        line = inspect.stack()[1][0].f_lineno
+        output = f'{dt * 1000:7.3f} ms' if ms else f'{dt:7.3f} s'
+        print(f'{inspect.stack()[1].filename}:{line}', output, flush=True)
+    if reset:
+        print('------------', flush=True)
+    t = time.perf_counter()
+
+
+def angle(yaw0: float, yaw1: float) -> float:
+    return eliminate_2pi(yaw1 - yaw0)
+
+
+def eliminate_pi(angle: float) -> float:
+    return (angle + np.pi / 2) % np.pi - np.pi / 2
+
+
+def eliminate_2pi(angle: float) -> float:
+    return (angle + np.pi) % (2 * np.pi) - np.pi
+
+
+def ramp(x: float, in_min: float, in_max: float, out_min: float, out_max: float, clip: bool = False) -> float:
+    if clip and x < in_min:
+        return out_min
+    if clip and x > in_max:
+        return out_max
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def remove_indentation(text: str) -> str:
+    """Remove indentation from a multi-line string based on the indentation of the first line."""
+    lines = text.splitlines()
+    while lines and not lines[0].strip():
+        lines.pop(0)
+    if not lines:
+        return ''
+    indentation = len(lines[0]) - len(lines[0].lstrip())
+    return '\n'.join(line[indentation:] for line in lines)
+
+
+class ModificationContext:
+
+    @contextmanager
+    def set(self, **kwargs) -> Generator[None, None, None]:
+        backup = {key: getattr(self, key) for key in kwargs.keys()}
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        try:
+            yield
+        finally:
+            for key, value in backup.items():
+                setattr(self, key, value)
