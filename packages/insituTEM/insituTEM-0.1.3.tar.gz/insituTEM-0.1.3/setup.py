2c7330bc84bd30095a4faba5f5a4f2f3eae63d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:59 +0000

    Add package insituTEM/0.1.3/insituTEM-0.1.3.tar.gz

---
+++ packages/insituTEM/insituTEM-0.1.3.tar.gz/insituTEM-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='insituTEM',
+    version='0.1.3',
+    author='Meng Li',
+    description='A package of tools for processing in situ TEM data',
+    packages=['insituTEM'],
+    install_requires=[
+        'tifffile',
+        'moviepy',
+        'tqdm',
+        'numpy',
+        'opencv-python',
+        'scipy',
+        'PIL',
+        'matplotlib'
+    ],
+)
