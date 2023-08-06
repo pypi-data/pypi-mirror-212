137dd8dcf5e70e9e49f29c0f896090ce315eb274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:57 +0000

    Add package static-frame-pyodide/1.4.5/static-frame-pyodide-1.4.5.tar.gz

---
+++ packages/static-frame-pyodide/static-frame-pyodide-1.4.5.tar.gz/static-frame-pyodide-1.4.5/README.md
@@ -0,0 +1,21 @@
+# static-frame-pyodide
+StaticFrame packaged for use in Pyodide, PyScript, and WebAssembly/Emscripten environments.
+
+This package, on import, uses `micropip` to install versions of StaticFrame dependencies (`arraykit` and `arraymap`) compiled for Emscripten, and then delegates the StaticFrame API to this module.
+
+## Usage in Pyodide:
+
+Try StaticFrame in Pyodide here: https://pyodide.org/en/stable/console.html
+
+```python-repl
+>>> import micropip; await micropip.install('static-frame-pyodide')
+>>> import static_frame_pyodide as sf
+```
+
+While ``pyodide.http.open_url()`` will not work with any URL, it works with the sample provided by the
+[``pyscript-ice-cream``](https://github.com/Cheukting/pyscript-ice-cream) project.
+
+
+![Pyodide screen shot](https://github.com/static-frame/static-frame-pyodide/blob/main/doc/sf-pyodide.png)
+
+
---
+++ packages/static-frame-pyodide/static-frame-pyodide-1.4.5.tar.gz/static-frame-pyodide-1.4.5/static_frame_pyodide/__init__.py
@@ -0,0 +1,74 @@
+import sys
+import asyncio
+
+__version__ = _SF = '1.4.5'
+
+_AK = '0.4.8'
+_AM = '0.1.9'
+
+_EMS = '3_1_32' # emscripten version
+_URL = 'https://flexatone.s3.us-west-1.amazonaws.com/packages/'
+
+_MODULE = sys.modules[__name__]
+
+async def _micropip_and_import() -> bool:
+
+    micropip = __import__('micropip')
+
+    await micropip.install([
+        'sqlite3',
+        f'{_URL}arraymap-{_AM}-cp311-cp311-emscripten_{_EMS}_wasm32.whl',
+        f'{_URL}arraykit-{_AK}-cp311-cp311-emscripten_{_EMS}_wasm32.whl',
+        f'static-frame=={_SF}',
+        ])
+
+    try:
+        sf = __import__('static_frame')
+    except ModuleNotFoundError:
+        print("Cannot import statc-frame into this namespace, try `import static_frame as sf`")
+        sf = None
+
+    if sf:
+        for name in dir(sf):
+            if not name.startswith('_'):
+                setattr(_MODULE, name, getattr(sf, name))
+
+#-------------------------------------------------------------------------------
+if sys.platform != 'emscripten':
+    raise RuntimeError('This package is only for use in emscripten environments')
+
+try:
+    loop = asyncio.get_running_loop()
+except RuntimeError:
+    loop = None
+
+if loop and loop.is_running():
+    loop.create_task(asyncio.wait_for(_micropip_and_import(), None))
+else:
+    asyncio.run(_micropip_and_import())
+
+
+
+# new_loop = asyncio.new_event_loop()
+# new_loop.run_until_complete(_micropip_and_import())
+
+# loop.create_task(_schedule_and_await(loop))
+
+# for coro in asyncio.as_completed((_micropip_and_import(),)):
+#     # loop.create_task(coro)
+#     print(coro)
+
+# def finished(_):
+#     print('finished')
+# future = asyncio.run_coroutine_threadsafe(_micropip_and_import(), loop)
+# future.add_done_callback(finished)
+# assert future.result(20) == True
+
+# loop.call_soon(_micropip_and_import())
+
+# import threading
+# def target():
+#     return asyncio.run(_micropip_and_import())
+# t = threading.Thread(target=target)
+# t.start()
+# t.join()
