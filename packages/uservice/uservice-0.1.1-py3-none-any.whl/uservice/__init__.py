986044e34098656c90ba0dff1a973f7359e221b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:46 +0000

    Add package uservice/0.1.1/uservice-0.1.1-py3-none-any.whl

---
+++ packages/uservice/uservice-0.1.1-py3-none-any.whl/uservice/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.1.1"
+
+
+from uservice.main import main
+from uservice.service import Service
+from uservice.amqp.events import AmqpEventPublisher as EventPublisher
+from uservice.amqp.rpc import AmqpRpcProxy as RpcProxy
+
+__all__ = [
+    "main",
+    "Service",
+    "EventPublisher",
+    "RpcProxy",
+]
