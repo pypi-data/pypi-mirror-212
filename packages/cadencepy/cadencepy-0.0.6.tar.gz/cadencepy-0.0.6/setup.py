c89e75ad2f49020b06071d7540034ce8fa9be914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:43 +0000

    Add package cadencepy/0.0.6/cadencepy-0.0.6.tar.gz

---
+++ packages/cadencepy/cadencepy-0.0.6.tar.gz/cadencepy-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def readall(path):
+    with open(path) as fp:
+        return fp.read()
+
+setup(
+    name="cadencepy",
+    version="0.0.6",
+    author="joetho786",
+    author_email="thomas.2@iitj.ac.in",
+    description="A python SDK for running simulation and reading data from Ocean Cadence",
+    long_description=readall("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/user/reponame",
+    packages=find_packages(),
+    install_requires=["requests","numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
