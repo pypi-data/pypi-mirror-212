4344ed148577a41f7d3037f710e185e12144a6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:12 +0000

    Add package customgpt-client/1.0.0/customgpt_client-1.0.0-py3-none-any.whl

---
+++ packages/customgpt-client/customgpt_client-1.0.0-py3-none-any.whl/custom_gpt_client/api/project_settings/update_project_settings.py
@@ -0,0 +1,127 @@
+from http import HTTPStatus
+from typing import Any, Dict, Optional
+
+import httpx
+
+from ... import errors
+from ...client import CustomGPT, CustomGPTClient
+from ...models.update_project_settings_multipart_data import UpdateProjectSettingsMultipartData
+from ...types import Response
+
+
+def _get_kwargs(
+    project_id: int,
+    *,
+    client: CustomGPT,
+    multipart_data: UpdateProjectSettingsMultipartData,
+) -> Dict[str, Any]:
+    url = "{}/api/v1/projects/{projectId}/settings".format(client.base_url, projectId=project_id)
+
+    headers: Dict[str, str] = client.get_headers()
+    cookies: Dict[str, Any] = client.get_cookies()
+
+    multipart_multipart_data = multipart_data.to_multipart()
+
+    return {
+        "method": "post",
+        "url": url,
+        "headers": headers,
+        "cookies": cookies,
+        "timeout": client.get_timeout(),
+        "follow_redirects": client.follow_redirects,
+        "files": multipart_multipart_data,
+    }
+
+
+def _parse_response(*, client: CustomGPTClient, response: httpx.Response) -> Optional[Any]:
+    if response.status_code == HTTPStatus.OK:
+        return None
+    if response.status_code == HTTPStatus.UNAUTHORIZED:
+        return None
+    if response.status_code == HTTPStatus.NOT_FOUND:
+        return None
+    if response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR:
+        return None
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(*, client: CustomGPTClient, response: httpx.Response) -> Response[Any]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    project_id: int,
+    *,
+    client: CustomGPT,
+    multipart_data: UpdateProjectSettingsMultipartData,
+) -> Response[Any]:
+    """Update project settings.
+
+     Update project settings of a specific project by `projectId`.
+
+    Args:
+        project_id (int):
+        multipart_data (UpdateProjectSettingsMultipartData):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Any]
+    """
+
+    kwargs = _get_kwargs(
+        project_id=project_id,
+        client=client,
+        multipart_data=multipart_data,
+    )
+
+    response = httpx.request(
+        verify=client.verify_ssl,
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio_detailed(
+    project_id: int,
+    *,
+    client: CustomGPT,
+    multipart_data: UpdateProjectSettingsMultipartData,
+) -> Response[Any]:
+    """Update project settings.
+
+     Update project settings of a specific project by `projectId`.
+
+    Args:
+        project_id (int):
+        multipart_data (UpdateProjectSettingsMultipartData):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Any]
+    """
+
+    kwargs = _get_kwargs(
+        project_id=project_id,
+        client=client,
+        multipart_data=multipart_data,
+    )
+
+    async with httpx.AsyncClient(verify=client.verify_ssl) as _client:
+        response = await _client.request(**kwargs)
+
+    return _build_response(client=client, response=response)
