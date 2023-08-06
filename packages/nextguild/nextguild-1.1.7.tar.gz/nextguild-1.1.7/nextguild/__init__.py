61c47dcf3931a6fc94b2eb4bdd53f1080a456570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:56 +0000

    Add package nextguild/1.1.7/nextguild-1.1.7.tar.gz

---
+++ packages/nextguild/nextguild-1.1.7.tar.gz/nextguild-1.1.7/nextguild/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .channel import Channel
+from .client import Client
+from .embed import Embed
+from .events import Events
+from .message import Message
+from .reaction import Reaction, CalendarReaction, ForumTopicCommentReaction
