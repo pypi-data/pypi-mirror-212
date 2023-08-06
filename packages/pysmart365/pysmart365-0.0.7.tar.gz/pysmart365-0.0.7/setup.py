0d61926cda9842675cb8e3ba1243f5ab03afd8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:54 +0000

    Add package pysmart365/0.0.7/pysmart365-0.0.7.tar.gz

---
+++ packages/pysmart365/pysmart365-0.0.7.tar.gz/pysmart365-0.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import *
+setup(
+    author="Runkang",
+    author_email="informatic3650@gmail.com",
+    name="pysmart365",
+    packages=['pysmart365'],
+    version='0.0.7',
+    long_description='''This module is to simplify the functions of MicroSoftware and SmartSoft.''',
+    description='This module is to simplify the functions of MicroSoftware and SmartSoft.',
+    install_requires=[
+        'screeninfo',
+        'pandas',
+        'pyautogui',
+        'pytube',
+        'pytube3',
+        'firebase_admin',
+        'customtkinter',
+        'requests',
+        'flask',
+        'ttkbootstrap',
+        'configparse'
+        ],
+    url="https://github.com/informatic365/PySmart365/"
+)
