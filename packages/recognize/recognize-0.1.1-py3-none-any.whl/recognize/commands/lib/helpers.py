70a517b7b6b15d166aa60dba852598fd9c2acde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:08 +0000

    Add package recognize/0.1.1/recognize-0.1.1-py3-none-any.whl

---
+++ packages/recognize/recognize-0.1.1-py3-none-any.whl/recognize/commands/lib/helpers.py
@@ -0,0 +1,28 @@
+import os
+import re
+from pathlib import Path
+from typing import Generator
+
+import httpx
+
+
+async def upload_file(file_path: str, url: str) -> str:
+    """Asynchronously upload a file to a given URL."""
+    with open(file_path, 'rb') as fp:
+        files = {'file': (fp.name, fp)}
+
+        async with httpx.AsyncClient() as client:
+            async with client.stream('POST', url, files=files) as response:
+                response.raise_for_status()  # Ensure we get a 2xx response
+                return file_path
+
+
+def find_files(filepath: Path) -> Generator[str, None, None]:
+    pattern = re.compile(r'\.(jpeg|jpg|png|mp4|mov)$')
+
+    for root, dirs, files in os.walk(filepath.absolute()):
+        for file in files:
+            lowered = file.lower()
+
+            if re.search(pattern, lowered):
+                yield os.path.join(root, file)
