b397ddc9eab35be6ec4768c8044b546cdac765e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:34 +0000

    Add package ahmetturk-probability/1.0/ahmetturk_probability-1.0.tar.gz

---
+++ packages/ahmetturk-probability/ahmetturk_probability-1.0.tar.gz/ahmetturk_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ahmetturk_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ahmetturk_probability'],
+      author='Ahmet Türk',
+      author_mail='ahmetturk.pc@gmail.com',
+      zip_safe=False)
