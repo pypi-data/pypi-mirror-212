f202fa69897c2ba4c9446603f731f9c4480bfb6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:49 +0000

    Add package predictapi/0.0.12/predictapi-0.0.12.tar.gz

---
+++ packages/predictapi/predictapi-0.0.12.tar.gz/predictapi-0.0.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="predictapi",
+      version="0.0.12",
+      author="The predict API",
+      description="The predcit API",
+      install_requires=["requests"],
+      py_modules=['predictapi'],
+      python_requires='>=3.7')
+
+#packages=find_packages(where="src"),
