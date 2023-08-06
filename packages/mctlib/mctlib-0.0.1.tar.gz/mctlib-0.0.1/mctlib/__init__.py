1ee290dcba432170c8b74b2a8b29f5cc15473a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:10 +0000

    Add package mctlib/0.0.1/mctlib-0.0.1.tar.gz

---
+++ packages/mctlib/mctlib-0.0.1.tar.gz/mctlib-0.0.1/mctlib/__init__.py
@@ -0,0 +1,22 @@
+def get_con_input(q):
+    return(input(q))
+
+def add(a, b):
+    return(a + b)
+
+def sub(a, b):
+    return(a - b)
+
+def mul(a, b):
+    return(a * b)
+
+def div(a, b):
+    return(a / b)
+
+def sqr(a, b):
+    return(a ** b)
+
+def test(testnum):
+    print(f'test {testnum}')
+    return(f'test {testnum}')
+
