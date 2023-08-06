a3288ffcbc3621bb716cf31e749d0653c441c06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:40 +0000

    Add package pyLiang/1.0.0/pyLiang-1.0.0.tar.gz

---
+++ packages/pyLiang/pyLiang-1.0.0.tar.gz/pyLiang-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+__author__ = "Hanyu Jin"
+__version__ = '1.0.0'
+__email__ = "henrychin2006@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright Hanyu Jin"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "pyLiang",
+    version = __version__,
+    author = __author__,
+    author_email = __email__,
+    description = ("A python package for liang-klleeman infomation flow causality test."),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Koni2020/pyLiang",
+    packages = ["pyLiang"],
+    license = __license__,
+    install_requires = ["numpy"],
+    classifiers = [
+		"Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+		"Intended Audience :: Science/Research",
+		"Operating System :: OS Independent",
+		"Topic :: Scientific/Engineering",
+    ]
+)
+
