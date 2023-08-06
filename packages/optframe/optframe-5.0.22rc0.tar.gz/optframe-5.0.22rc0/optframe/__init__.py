33a4a688de888bcbffb15f2e2a10d64a048d01c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:31 +0000

    Add package optframe/5.0.22rc0/optframe-5.0.22rc0.tar.gz

---
+++ packages/optframe/optframe-5.0.22rc0.tar.gz/optframe-5.0.22rc0/optframe/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '5.0.22rc0'
+from optframe.core import SearchOutput
+from optframe.core import SearchStatus
+from optframe.engine import Engine
+from optframe.engine import CheckCommandFailCode
+from optframe.engine import APILevel
+from optframe.engine import LogLevel
+from optframe.components import IdInitialSearch
