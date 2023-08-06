2bbfef7dec7bf4d37bbb7d66f0d71f4542998901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:31 +0000

    Add package bettershot/0.1.2/bettershot-0.1.2.tar.gz

---
+++ packages/bettershot/bettershot-0.1.2.tar.gz/bettershot-0.1.2/bettershot/__init__.py
@@ -0,0 +1,18 @@
+__version__ = '0.1.2'
+import requests
+import json 
+
+url = "https://bettershot-w6mm.zeet-berri.zeet.app/openai_listener"
+
+def log(messages, completion, openai_api_key): 
+    payload = {
+    "messages": messages,
+    "completion": completion,
+    "openai_api_key": openai_api_key
+    }
+    headers = {
+        "Content-Type": "application/json",
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    print(response)
+    return {"response" : response}
