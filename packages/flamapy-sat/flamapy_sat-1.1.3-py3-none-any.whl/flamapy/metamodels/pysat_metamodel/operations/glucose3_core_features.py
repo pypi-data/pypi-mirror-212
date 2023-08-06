7313f062d662d8da028e6c515644bc6ed234fb8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:42 +0000

    Add package flamapy-sat/1.1.3/flamapy_sat-1.1.3-py3-none-any.whl

---
+++ packages/flamapy-sat/flamapy_sat-1.1.3-py3-none-any.whl/flamapy/metamodels/pysat_metamodel/operations/glucose3_core_features.py
@@ -0,0 +1,34 @@
+from typing import Any, cast
+
+from pysat.solvers import Solver
+
+from flamapy.core.operations import CoreFeatures
+from flamapy.metamodels.pysat_metamodel.models.pysat_model import PySATModel
+
+
+class Glucose3CoreFeatures(CoreFeatures):
+
+    def __init__(self) -> None:
+        self.core_features: list[list[Any]] = []
+        self.solver = Solver(name='glucose3')
+
+    def get_core_features(self) -> list[list[Any]]:
+        return self.core_features
+
+    def get_result(self) -> list[list[Any]]:
+        return self.get_core_features()
+
+    def execute(self, model: VariabilityModel) -> 'Glucose3CoreFeatures':
+        model=cast(PySATModel, model)
+        for clause in model.get_all_clauses():  # AC es conjunto de conjuntos
+            self.solver.add_clause(clause)  # añadimos la constraint
+
+        core_features = []
+        if self.solver.solve():
+            for variable in model.variables.items():
+                if not self.solver.solve(assumptions=[-variable[1]]):
+                    core_features.append(variable[0])
+
+        self.core_features = core_features
+        self.solver.delete()
+        return self
