131961a2394de821c0e9f8e79b9c0e0af4562c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:19 +0000

    Add package tvv/0.2/tvv-0.2.tar.gz

---
+++ packages/tvv/tvv-0.2.tar.gz/tvv-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='tvv',
+      version='0.2',
+      description='none',
+      packages=['tvv'],
+      author_email='zuszusov676@gmail.com',
+      zip_safe=False)
