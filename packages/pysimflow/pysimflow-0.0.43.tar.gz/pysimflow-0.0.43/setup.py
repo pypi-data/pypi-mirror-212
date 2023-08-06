43e56df994fa1b2933af717801d53d7f9480daf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:37 +0000

    Add package pysimflow/0.0.43/pysimflow-0.0.43.tar.gz

---
+++ packages/pysimflow/pysimflow-0.0.43.tar.gz/pysimflow-0.0.43/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='pysimflow',
+    version='0.0.43',
+    author='sl.truman',
+    author_email='sl.truman@live.com',
+    url='',
+    description=u'',
+    packages=['digitaltwin','digitaltwin_data'],
+    install_requires=[
+        'numpy',
+        'pybullet',
+        'scipy'
+    ],
+    data_files=[]
+)
