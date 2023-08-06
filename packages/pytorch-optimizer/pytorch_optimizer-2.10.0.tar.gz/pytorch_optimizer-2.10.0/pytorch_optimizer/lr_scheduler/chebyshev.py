49b6d2fd08a6d4dadd9da1c6b327006c6550d3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:07 +0000

    Add package pytorch-optimizer/2.10.0/pytorch_optimizer-2.10.0.tar.gz

---
+++ packages/pytorch-optimizer/pytorch_optimizer-2.10.0.tar.gz/pytorch_optimizer-2.10.0/pytorch_optimizer/lr_scheduler/chebyshev.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def chebyshev_steps(small_m: float, big_m: float, num_epochs: int) -> np.ndarray:
+    r"""Chebyshev steps.
+
+    :param small_m: float. stands for 'm' notation.
+    :param big_m:  float. stands for 'M' notation.
+    :param num_epochs: int. stands for 'T' notation.
+    :return: np.array. chebyshev_steps.
+    """
+    c, r = (big_m + small_m) / 2.0, (big_m - small_m) / 2.0
+    thetas = (np.arange(num_epochs) + 0.5) / num_epochs * np.pi
+
+    return 1.0 / (c - r * np.cos(thetas))
+
+
+def chebyshev_perm(num_epochs: int) -> np.ndarray:
+    r"""Chebyshev permutation."""
+    perm = np.array([0])
+    while len(perm) < num_epochs:
+        perm = np.vstack([perm, 2 * len(perm) - 1 - perm]).T.flatten()
+    return perm
+
+
+def get_chebyshev_schedule(num_epochs: int) -> np.ndarray:
+    r"""Get Chebyshev schedules."""
+    steps: np.ndarray = chebyshev_steps(0.1, 1, num_epochs - 2)
+    perm: np.ndarray = chebyshev_perm(num_epochs - 2)
+    return steps[perm]
