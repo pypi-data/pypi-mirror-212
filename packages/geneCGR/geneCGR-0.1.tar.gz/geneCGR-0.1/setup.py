4540cab47708bf976aff3d2ba69e49f44336ba3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:12 +0000

    Add package geneCGR/0.1/geneCGR-0.1.tar.gz

---
+++ packages/geneCGR/geneCGR-0.1.tar.gz/geneCGR-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='geneCGR',
+    version='0.1',
+    description='A test PyPI package',
+    author='akhil',
+    author_email='cppractice000@gmail.com',
+    packages=['geneCGR'],
+)
