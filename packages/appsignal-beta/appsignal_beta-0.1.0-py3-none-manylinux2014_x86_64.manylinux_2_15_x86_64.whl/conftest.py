a0d2159187462145ee7cf9e6784cdf7b52ea573c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:16 +0000

    Add package appsignal-beta/0.1.0/appsignal_beta-0.1.0-py3-none-manylinux2014_x86_64.manylinux_2_15_x86_64.whl

---
+++ packages/appsignal-beta/appsignal_beta-0.1.0-py3-none-manylinux2014_x86_64.manylinux_2_15_x86_64.whl/conftest.py
@@ -0,0 +1,40 @@
+import os
+import logging
+import pytest
+import platform
+import tempfile
+from appsignal.agent import _reset_agent_active
+
+
+@pytest.fixture(scope="function", autouse=True)
+def reset_environment_between_tests():
+    old_environ = dict(os.environ)
+
+    yield
+
+    os.environ.clear()
+    os.environ.update(old_environ)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def remove_logging_handlers_after_tests():
+    yield
+
+    logger = logging.getLogger("appsignal")
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def reset_agent_active_state():
+    _reset_agent_active()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def stop_agent():
+    tmp_path = "/tmp" if platform.system() == "Darwin" else tempfile.gettempdir()
+    working_dir = os.path.join(tmp_path, "appsignal")
+    if os.path.isdir(working_dir):
+        os.system(r"rm -rf {working_dir}")
+
+    yield
