3ee40a73620b86826aa911c394c6a02171f01ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:01 +0000

    Add package digispark/0.1.0/digispark-0.1.0.tar.gz

---
+++ packages/digispark/digispark-0.1.0.tar.gz/digispark-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='digispark',
+    version='0.1.0',    
+    description='ATtiny-85 Digispark module',
+    url='https://github.com/netgian/digispark',
+    author='Gian Glen',
+    author_email='gianlopez1605@gmail.com',
+    license='GNU General Public License v3.0',
+    packages=['digispark'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3',
+    ],
+)
