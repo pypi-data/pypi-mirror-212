2959599410a01b2d697322a29f30bce1298a46f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:35 +0000

    Add package kontrol/1.0.0a0/kontrol-1.0.0a0-py3-none-any.whl

---
+++ packages/kontrol/kontrol-1.0.0a0-py3-none-any.whl/kontrol/__init__.py
@@ -0,0 +1,22 @@
+# from . import frequency_series
+# from .frequency_series import FrequencySeries
+
+
+# from .frequency_series.frequency_series import FrequencySeries
+from .frequency_series.frequency_series import FrequencySeries
+from .complementary_filter.complementary_filter import ComplementaryFilter
+from .transfer_function import *
+from .sensact.matrix  import Matrix, SensingMatrix
+from .sensact.optical_lever import (
+    OpticalLeverSensingMatrix, HorizontalOpticalLeverSensingMatrix,
+    VerticalOpticalLeverSensingMatrix)
+from .core import spectral, foton
+from . import dmd
+
+
+## for ad hoc/optional packages
+import importlib
+ezca_spec = importlib.util.find_spec("ezca")
+ezca_exist = ezca_spec is not None
+if ezca_exist:
+    from .ezca import Ezca
