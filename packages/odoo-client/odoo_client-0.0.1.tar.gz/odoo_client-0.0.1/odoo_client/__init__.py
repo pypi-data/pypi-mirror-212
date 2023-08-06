561faf58e95f3b2d3f9108d226dc8ff472210035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:58 +0000

    Add package odoo-client/0.0.1/odoo_client-0.0.1.tar.gz

---
+++ packages/odoo-client/odoo_client-0.0.1.tar.gz/odoo_client-0.0.1/odoo_client/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Lie-Nielsen Toolworks"
+__email__ = "mike@lie-nielsen.com"
+__version__ = "0.0.1"
+
+from .client import (
+    OdooClient, OdooObject, OdooModel
+)
---
+++ packages/odoo-client/odoo_client-0.0.1.tar.gz/odoo_client-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "Simple API wrapper for Odoo's External API"
+LONG_DESCRIPTION = "Simple API wrapper for Odoo's External API"
+
+setup(
+    name="odoo_client",
+    version=VERSION,
+    author="Michael Farwell",
+    author_email="mike@lie-nielsen.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["python", "odoo", "api"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
