f183026cd3cd9470e63bfe90e176c9a7ddc00e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:24 +0000

    Add package agenta/0.1.7/agenta-0.1.7.tar.gz

---
+++ packages/agenta/agenta-0.1.7.tar.gz/agenta-0.1.7/agenta/docker/docker-assets/main.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from uvicorn import run
+import agenta
+import app  # This will register the routes with the FastAPI application
+
+if __name__ == "__main__":
+    
+
+    run("agenta:app", host="0.0.0.0", port=80)
+
