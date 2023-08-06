74f146b46ab188e5771e5b679be7dc9dc21f29e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:54 +0000

    Add package spPersist/0.2.10/spPersist-0.2.10-py3-none-any.whl

---
+++ packages/spPersist/spPersist-0.2.10-py3-none-any.whl/spPersist/hc.py
@@ -0,0 +1,22 @@
+from . import ph
+
+def homological_classification(X, ptype:str):
+  '''
+  homological_classification takes a point cloud X
+  and ptype indicating the persistence computed,
+  and return a dictionary of homologies of X where
+  the keys indicate connected components or n-dimensional
+  holes and the values indicate the number of connected
+  components or the number of holes of a given dimension.
+  '''
+
+  hc = {}
+  st = ph.compute_simplicial_complex(X,ptype=ptype)
+  bn = st.betti_numbers()
+  for i in range(bn):
+    if i == 0:
+      hc['connected components'] = bn[i]
+    else:
+      hc[str(i)+'-dimensional holes'] = bn[i]
+
+  return hc
