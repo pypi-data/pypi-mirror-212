6020969214c340464f9627abc35995683e4dfce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:14 +0000

    Add package polars-lts-cpu/0.18.1/polars_lts_cpu-0.18.1.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.18.1.tar.gz/polars_lts_cpu-0.18.1/docs/source/reference/functions.rst
@@ -0,0 +1,52 @@
+=================
+Functions
+=================
+.. currentmodule:: polars
+
+Conversion
+~~~~~~~~~~
+.. autosummary::
+   :toctree: api/
+
+    from_arrow
+    from_dataframe
+    from_dict
+    from_dicts
+    from_numpy
+    from_pandas
+    from_records
+    from_repr
+
+Eager/Lazy functions
+~~~~~~~~~~~~~~~~~~~~
+.. autosummary::
+   :toctree: api/
+
+    arg_where
+    concat
+    date_range
+    ones
+    time_range
+    zeros
+
+Miscellaneous
+~~~~~~~~~~~~~~~~~~~~
+.. autosummary::
+   :toctree: api/
+
+    align_frames
+
+Parallelization
+~~~~~~~~~~~~~~~
+.. autosummary::
+   :toctree: api/
+
+   collect_all
+
+StringCache
+~~~~~~~~~~~
+.. autosummary::
+   :toctree: api/
+
+    enable_string_cache
+    StringCache
---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.18.1.tar.gz/polars_lts_cpu-0.18.1/local_dependencies/polars-arrow/src/kernels/rolling/no_nulls/mean.rs
@@ -0,0 +1,76 @@
+use no_nulls::{rolling_apply_agg_window, RollingAggWindowNoNulls};
+
+use super::sum::SumWindow;
+use super::*;
+
+pub struct MeanWindow<'a, T> {
+    sum: SumWindow<'a, T>,
+}
+
+impl<
+        'a,
+        T: NativeType + IsFloat + std::iter::Sum + AddAssign + SubAssign + Div<Output = T> + NumCast,
+    > RollingAggWindowNoNulls<'a, T> for MeanWindow<'a, T>
+{
+    fn new(slice: &'a [T], start: usize, end: usize, params: DynArgs) -> Self {
+        Self {
+            sum: SumWindow::new(slice, start, end, params),
+        }
+    }
+
+    unsafe fn update(&mut self, start: usize, end: usize) -> T {
+        let sum = self.sum.update(start, end);
+        sum / NumCast::from(end - start).unwrap()
+    }
+}
+
+pub fn rolling_mean<T>(
+    values: &[T],
+    window_size: usize,
+    min_periods: usize,
+    center: bool,
+    weights: Option<&[f64]>,
+    _params: DynArgs,
+) -> ArrayRef
+where
+    T: NativeType + Float + std::iter::Sum<T> + SubAssign + AddAssign + IsFloat,
+{
+    match (center, weights) {
+        (true, None) => rolling_apply_agg_window::<MeanWindow<_>, _, _>(
+            values,
+            window_size,
+            min_periods,
+            det_offsets_center,
+            None,
+        ),
+        (false, None) => rolling_apply_agg_window::<MeanWindow<_>, _, _>(
+            values,
+            window_size,
+            min_periods,
+            det_offsets,
+            None,
+        ),
+        (true, Some(weights)) => {
+            let weights = no_nulls::coerce_weights(weights);
+            no_nulls::rolling_apply_weights(
+                values,
+                window_size,
+                min_periods,
+                det_offsets_center,
+                no_nulls::compute_mean_weights,
+                &weights,
+            )
+        }
+        (false, Some(weights)) => {
+            let weights = no_nulls::coerce_weights(weights);
+            no_nulls::rolling_apply_weights(
+                values,
+                window_size,
+                min_periods,
+                det_offsets,
+                no_nulls::compute_mean_weights,
+                &weights,
+            )
+        }
+    }
+}
