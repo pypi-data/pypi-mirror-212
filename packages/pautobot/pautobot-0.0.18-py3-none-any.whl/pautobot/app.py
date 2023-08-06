e1fda6f09d10aa5d3e9593e5f060352d0ed824ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:19 +0000

    Add package pautobot/0.0.18/pautobot-0.0.18-py3-none-any.whl

---
+++ packages/pautobot/pautobot-0.0.18-py3-none-any.whl/pautobot/app.py
@@ -0,0 +1,44 @@
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+from pautobot import globals
+from pautobot.app_info import DATA_ROOT, __appname__, __description__, __version__
+from pautobot.routers import bot, contexts, documents
+from pautobot.utils import extract_frontend_dist
+
+
+def main():
+    print(f"Starting {__appname__}...")
+    print(f"Version: {__version__}")
+    static_folder = os.path.abspath(os.path.join(DATA_ROOT, "frontend-dist"))
+    extract_frontend_dist(static_folder)
+
+    globals.init()
+
+    app = FastAPI(
+        title=__appname__,
+        description=__description__,
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(bot.router)
+    app.include_router(contexts.router)
+    app.include_router(documents.router)
+    app.mount("/", StaticFiles(directory=static_folder, html=True), name="static")
+
+    uvicorn.run(app, host="0.0.0.0", port=5678, reload=False, workers=1)
+
+
+if __name__ == "__main__":
+    main()
