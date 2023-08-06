4095b52bfdafe5d2378dc5a95dae12cb497270e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:05 +0000

    Add package ariadne-codegen/0.7.1/ariadne_codegen-0.7.1-py2.py3-none-any.whl

---
+++ packages/ariadne-codegen/ariadne_codegen-0.7.1-py2.py3-none-any.whl/ariadne_codegen/exceptions.py
@@ -0,0 +1,34 @@
+class CodeGenException(Exception):
+    """Generic graphql sdk gen exception."""
+
+
+class ConfigFileNotFound(CodeGenException):
+    """Config file not found."""
+
+
+class MissingConfiguration(CodeGenException):
+    """Configuration not present."""
+
+
+class InvalidConfiguration(CodeGenException):
+    """Configuration not valid."""
+
+
+class InvalidGraphqlSyntax(CodeGenException):
+    """Invalid graphql syntax."""
+
+
+class NotSupported(CodeGenException):
+    """Not supported."""
+
+
+class ParsingError(CodeGenException):
+    """Parsing error."""
+
+
+class IntrospectionError(CodeGenException):
+    """Introspection error."""
+
+
+class PluginImportError(CodeGenException):
+    """Error occurred during the plugin lookup."""
