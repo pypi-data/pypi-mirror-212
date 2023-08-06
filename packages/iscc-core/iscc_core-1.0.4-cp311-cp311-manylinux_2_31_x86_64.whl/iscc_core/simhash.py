3c131ffb7ef4606704d18bce58fd4d113eabfa5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:10 +0000

    Add package iscc-core/1.0.4/iscc_core-1.0.4-cp311-cp311-manylinux_2_31_x86_64.whl

---
+++ packages/iscc-core/iscc_core-1.0.4-cp311-cp311-manylinux_2_31_x86_64.whl/iscc_core/simhash.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+def alg_simhash(hash_digests):
+    # type: (list[bytes]) -> bytes
+    """
+    Creates a similarity preserving hash from a sequence of equal sized hash digests.
+
+    :param list hash_digests: A sequence of equaly sized byte-hashes.
+    :returns: Similarity byte-hash
+    :rtype: bytes
+    """
+
+    n_bytes = len(hash_digests[0])
+    n_bits = n_bytes * 8
+    vector = [0] * n_bits
+
+    for digest in hash_digests:
+        h = int.from_bytes(digest, "big", signed=False)
+
+        for i in range(n_bits):
+            vector[i] += h & 1
+            h >>= 1
+
+    minfeatures = len(hash_digests) * 1.0 / 2
+    shash = 0
+
+    for i in range(n_bits):
+        shash |= int(vector[i] >= minfeatures) << i
+
+    return shash.to_bytes(n_bytes, "big", signed=False)
