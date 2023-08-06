27f5bdddca5c3564e8f1235b977ec84e9055b6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:56 +0000

    Add package custom-flet/0.1.0/custom_flet-0.1.0.tar.gz

---
+++ packages/custom-flet/custom_flet-0.1.0.tar.gz/custom_flet-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Subhradeep Rang",
+    author_email="srang992@gmail.com",
+    name="custom_flet",
+    version="0.1.0",
+    packages=find_packages(),
+)
