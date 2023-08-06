6e208a10c1dfebc22d27985db715b54843350b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:35 +0000

    Add package psychoanalyze/0.2.1/psychoanalyze-0.2.1.tar.gz

---
+++ packages/psychoanalyze/psychoanalyze-0.2.1.tar.gz/psychoanalyze-0.2.1/README.md
@@ -0,0 +1,27 @@
+# PsychoAnalyze
+
+Psychophysics analysis in Python.
+
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/psychoanalyze)
+![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)
+[![Documentation Status](https://readthedocs.org/projects/psychoanalyze/badge/?version=latest)](https://psychoanalyze.readthedocs.io/en/latest/?badge=latest)
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)]()
+[![security: bandit](https://img.shields.io/badge/security-bandit-yellow.svg)](https://github.com/PyCQA/bandit)
+[![Open in Remote - Containers](https://img.shields.io/static/v1?label=Remote%20-%20Containers&message=Open&color=blue&logo=visualstudiocode)](https://vscode.dev/redirect?url=vscode://ms-vscode-remote.remote-containers/cloneInVolume?url=https://github.com/psychoanalyze/psychoanalyze)
+
+
+## Installation
+pip:
+```console
+pip install psychoanalyze
+```
+poetry:
+```console
+poetry add psychoanalyze
+```
+
+## Dashboard
+See what `psychoanalyze` can do by [viewing our dashboard](https://app.psychoanalyze.io/).
+
+## Documentation
+View the full documentation [here](https://docs.psychoanalyze.io).
---
+++ packages/psychoanalyze/psychoanalyze-0.2.1.tar.gz/psychoanalyze-0.2.1/psychoanalyze/__init__.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from psychoanalyze import (
+    schemas,
+    sessions,
+    blocks,
+    points,
+    plot,
+    data,
+    trials,
+    weber,
+    strength_duration,
+    subjects,
+    stimulus,
+    simulate,
+)
+
+pd.options.plotting.backend = "plotly"
+
+__version__ = "0.2.1"
+
+__all__ = [
+    "schemas",
+    "plot",
+    "data",
+    "trial",
+    "blocks",
+    "sessions",
+    "weber",
+    "points",
+    "trials",
+    "blocks",
+    "strength_duration",
+    "subjects",
+    "stimulus",
+    "simulate",
+]
+
+# def curve(trials: pd.DataFrame) -> pd.Series:
+#     """Arrange *method of constant stimuli* performance curves using trial data"""
+#     df = trials.groupby("x").mean().rename(columns={"Result": "Hit Rate"})
+#     return df
+
+
+def weber_coefficient(curves: pd.DataFrame) -> float:
+    """Calculate weber coefficient for a set of psychometric curves"""
+    return 1
+
+
+def psi(threshold=0, slope=1, lambda_=0, gamma=0, x_range=(-3, 3)) -> pd.Series:
+    """Basic sigmoid psychometric function psi (Ψ) = expit/logistic"""
+    x = np.linspace(x_range[0], x_range[1])
+    y = gamma + (1 - lambda_ - gamma) * 1 / (1 + np.exp((-slope) * (x - threshold)))
+    return pd.Series(y, index=x)
+
+
+def fit(data):
+    return {"Fit": [1, 1, 0, 0]}
