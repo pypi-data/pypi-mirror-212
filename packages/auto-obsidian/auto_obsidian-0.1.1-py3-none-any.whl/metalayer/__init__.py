ecbd14e87dd79f4ba67043adef1a8e8564fc2463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:30 +0000

    Add package auto-obsidian/0.1.1/auto_obsidian-0.1.1-py3-none-any.whl

---
+++ packages/auto-obsidian/auto_obsidian-0.1.1-py3-none-any.whl/metalayer/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+METALAYER_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(METALAYER_PATH, 'data/')
+
+ACTIVITY_LOG_DIR = os.path.join(DATA_DIR, 'activity_log/')
+CAPTURE_DIR = os.path.join(DATA_DIR, 'captures/')
+ICON_DIR = os.path.join(DATA_DIR, 'icons/')
+THUMBNAIL_DIR = os.path.join(DATA_DIR, 'thumbnails/')
+
+for dir_name in [ACTIVITY_LOG_DIR, CAPTURE_DIR, ICON_DIR, THUMBNAIL_DIR]:
+    os.makedirs(dir_name, exist_ok=True)
+
+print(METALAYER_PATH)
