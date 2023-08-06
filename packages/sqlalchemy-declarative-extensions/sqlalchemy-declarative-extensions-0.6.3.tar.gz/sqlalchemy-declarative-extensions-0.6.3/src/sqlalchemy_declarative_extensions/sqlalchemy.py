e51ec338be1da40c8d4aeada9715e7d62c1eba4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:34 +0000

    Add package sqlalchemy-declarative-extensions/0.6.3/sqlalchemy-declarative-extensions-0.6.3.tar.gz

---
+++ packages/sqlalchemy-declarative-extensions/sqlalchemy-declarative-extensions-0.6.3.tar.gz/sqlalchemy-declarative-extensions-0.6.3/src/sqlalchemy_declarative_extensions/sqlalchemy.py
@@ -0,0 +1,86 @@
+import sqlalchemy
+from sqlalchemy import MetaData, Table
+from sqlalchemy.engine import Connection
+from typing_extensions import Protocol
+
+version = getattr(sqlalchemy, "__version__", "1.3")
+
+
+class HasMetaData(Protocol):
+    metadata: MetaData
+
+
+class HasTable(Protocol):
+    __table__: Table
+
+
+def dialect_dispatch(postgresql=None, sqlite=None, mysql=None):
+    dispatchers = {
+        "postgresql": postgresql,
+        "sqlite": sqlite,
+        "mysql": mysql,
+    }
+
+    def dispatch(connection: Connection, *args, **kwargs):
+        dialect_name = connection.dialect.name
+        if dialect_name == "pmrsqlite":
+            dialect_name = "sqlite"
+
+        dispatcher = dispatchers.get(dialect_name)
+        if dispatcher is None:  # pragma: no cover
+            raise NotImplementedError(
+                f"'{dialect_name}' is not yet supported for this operation."
+            )
+
+        return dispatcher(connection, *args, **kwargs)
+
+    return dispatch
+
+
+def escape_params(query: str) -> str:
+    return query.replace(":", r"\:")
+
+
+if version.startswith("1.3"):
+    from sqlalchemy.ext.declarative import (  # type: ignore
+        DeclarativeMeta,
+        declarative_base,
+        instrument_declarative,
+    )
+
+    def select(*args):
+        return sqlalchemy.select(list(args))
+
+    def create_mapper(cls, metadata):
+        return instrument_declarative(cls, {}, metadata)
+
+else:
+    from sqlalchemy.orm import DeclarativeMeta, declarative_base, registry
+
+    select = sqlalchemy.select
+
+    def create_mapper(cls, metadata):
+        reg = registry(metadata=metadata)
+        return reg.mapped(cls)
+
+
+if version.startswith(("1.4", "2")):
+
+    def row_to_dict(row):
+        return row._asdict()
+
+else:
+
+    def row_to_dict(row):
+        return dict(row)
+
+
+__all__ = [
+    "create_mapper",
+    "declarative_base",
+    "DeclarativeMeta",
+    "dialect_dispatch",
+    "HasMetaData",
+    "row_to_dict",
+    "select",
+]
