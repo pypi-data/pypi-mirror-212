d67a930de86989c523e9297dfd388ff6cac026d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:18 +0000

    Add package composabl-dev/0.2.1.dev8/composabl_dev-0.2.1.dev8-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/composabl-dev/composabl_dev-0.2.1.dev8-cp38-cp38-macosx_11_0_arm64.whl/composabl/main.py
@@ -0,0 +1,13 @@
+# Copyright (C) Composabl, Inc - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+
+
+# @todo: placeholder method that will be called through `python -m composabl`
+# we will use this file for conversion from JSON to an Agent from the Web App
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
