709f13a8cbd19b45198a69a56d1d50025b202d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:05 +0000

    Add package bayes-lol-client/1.0.2/bayes_lol_client-1.0.2.tar.gz

---
+++ packages/bayes-lol-client/bayes_lol_client-1.0.2.tar.gz/bayes_lol_client-1.0.2/bayes_lol_client/__init__.py
@@ -0,0 +1,7 @@
+# Bayes LoL Client
+__version__ = "1.0.1"
+__author__ = "Santiago Kozak"
+
+from bayes_lol_client.api import BayesAPIClient
+from bayes_lol_client.emh import BayesEMH
+from bayes_lol_client.historic import BayesHistoric
