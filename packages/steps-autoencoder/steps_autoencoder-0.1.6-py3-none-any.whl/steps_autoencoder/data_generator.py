633549fc50f43d0bb0aad561f4e173d414be17b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:02 +0000

    Add package steps-autoencoder/0.1.6/steps_autoencoder-0.1.6-py3-none-any.whl

---
+++ packages/steps-autoencoder/steps_autoencoder-0.1.6-py3-none-any.whl/steps_autoencoder/data_generator.py
@@ -0,0 +1,17 @@
+class Dirac():
+    def __init__(self, vector_length):
+        self.vector_length = vector_length
+
+    def generate(self, sample_size):
+        dirac_x = np.zeros((sample_size, self.vector_length))
+        for i in range(sample_size):
+            dirac_x[i, np.random.randint(self.vector_length)] = 1
+        return dirac_x
+    
+    def plot(self, dirac_x, save_path, n=10):
+        fig, ax = plt.subplots(n, 1, figsize=(5, n))
+        for i in range(n):
+            ax[i].plot(dirac_x[i])
+            ax[i].set_xticks([])
+        plt.savefig(save_path)
+        plt.close()
