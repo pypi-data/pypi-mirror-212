2d122aec5d2b49935395a87e3fa1fc3e3cd46888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:52 +0000

    Add package zhousf-lib/0.6/zhousf_lib-0.6-py2.py3-none-any.whl

---
+++ packages/zhousf-lib/zhousf_lib-0.6-py2.py3-none-any.whl/zhousflib/util/singleton.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Author  : zhousf
+# @Date    : 2022/3/16 
+# @Function: 线程安全的单例模式
+import threading
+
+
+class Singleton(type):
+    """
+    class A(metaclass=Singleton)
+    """
+    _instance_lock = threading.Lock()
+
+    def __call__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            with Singleton._instance_lock:
+                if not hasattr(cls, "_instance"):
+                    cls._instance = super(Singleton,cls).__call__(*args, **kwargs)
+        return cls._instance
