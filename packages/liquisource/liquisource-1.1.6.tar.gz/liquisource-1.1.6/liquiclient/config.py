aecbb5fd0002d3b71712462ee896d8dd11ad6b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:47 +0000

    Add package liquisource/1.1.6/liquisource-1.1.6.tar.gz

---
+++ packages/liquisource/liquisource-1.1.6.tar.gz/liquisource-1.1.6/liquiclient/config.py
@@ -0,0 +1,45 @@
+#!/usr/local/bin/python3
+# coding: utf8
+import sys
+from jproperties import Properties
+import os
+
+config = Properties()
+
+
+# 获取配置信息
+def get_config(filepath):
+    with open(filepath, 'rb') as prop:
+        config.load(prop)
+
+
+# 默认找项目路径的文件
+def auto_config():
+    # 获取当前工作目录
+    current_working_directory = os.getcwd()
+
+    # 获取父级目录
+    parent_directory = os.path.dirname(current_working_directory)
+
+    # 在父级目录中拼接给定的文件名
+    file_path = os.path.join(parent_directory, "liquibase.properties")
+    print(file_path)
+    with open(file_path, 'rb') as prop:
+        config.load(prop)
+    print(get_property("ck.host"))
+
+
+# 获取配置的某一个key
+def get_property(name):
+    if len(name) == 0:
+        return config
+    return config[str(name[0])].data
+
+
+# 获取根据租户分库分表
+def get_tenant_shard(key):
+    kfuin = get_property("parameter.tenant")
+    return key + "_" + kfuin
+
+
+auto_config()
