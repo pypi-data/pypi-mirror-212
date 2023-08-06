a0cf5f095a274d0e0c6e91aa279ce9744a34d5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:46 +0000

    Add package cdnmon/0.2.2/cdnmon-0.2.2-py3-none-any.whl

---
+++ packages/cdnmon/cdnmon-0.2.2-py3-none-any.whl/cdnmon/__init__.py
@@ -0,0 +1,19 @@
+import requests
+import config
+
+class Endpoint:
+    def __init__(self, access_token):
+        self._access_token = access_token
+        self.auth()
+
+    def auth(self):
+        self.session = requests.Session()
+        self.session.headers["Authorization"] = f"Bearer {self._access_token}"
+
+    def list_cdns(self):
+        api_endpoint = config.settings["API_ENDPOINT"]
+        url = f"https://{api_endpoint}/api/v1/providers"
+        return self.session.get(url).json()
+
+    def find_cdn(name):
+        pass
