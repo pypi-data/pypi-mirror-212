452a41ce2ff2c47352119c89d11758cc29abb4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:10 +0000

    Add package Kirill-SerializerAndFuncs/0.1.0/Kirill_SerializerAndFuncs-0.1.0-py3-none-any.whl

---
+++ packages/Kirill-SerializerAndFuncs/Kirill_SerializerAndFuncs-0.1.0-py3-none-any.whl/Kirill_SerializerAndFuncs/base_serializer.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class BaseSerializer(ABC):
+    @abstractmethod
+    def dumps(self, obj) -> str:
+        pass
+
+    @abstractmethod
+    def loads(self, string: str):
+        pass
+
+    def dump(self, obj, source_file):
+        source_file.write(self.dumps(obj))
+
+    def load(self, source_file):
+        return self.loads(source_file.read())
