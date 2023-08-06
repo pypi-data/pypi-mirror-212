f05bef76702483f2ed207306dd020b421a2837a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:10 +0000

    Add package macro-kit/0.4.3/macro_kit-0.4.3-cp311-cp311-win_amd64.whl

---
+++ packages/macro-kit/macro_kit-0.4.3-cp311-cp311-win_amd64.whl/macrokit/head.py
@@ -0,0 +1,63 @@
+from enum import Enum
+
+
+class Head(Enum):
+    """Head of Expr."""
+
+    empty = "empty"
+    getattr = "getattr"
+    getitem = "getitem"
+    del_ = "del"
+    call = "call"
+    assign = "assign"
+    kw = "kw"
+    tuple = "tuple"
+    list = "list"
+    braces = "braces"
+    comment = "comment"
+    assert_ = "assert"
+    unop = "unop"
+    binop = "binop"
+    aug = "aug"
+    block = "block"
+    function = "function"
+    lambda_ = "lambda"
+    return_ = "return"
+    yield_ = "yield"
+    raise_ = "raise"
+    if_ = "if"
+    elif_ = "elif"
+    for_ = "for"
+    while_ = "while"
+    generator = "generator"
+    filter = "filter"
+    annotate = "annotate"
+    import_ = "import"
+    from_ = "from"
+    as_ = "as"
+    with_ = "with"
+    class_ = "class"
+    star = "star"
+    decorator = "decorator"
+
+
+EXEC = (
+    Head.assign,
+    Head.assert_,
+    Head.comment,
+    Head.del_,
+    Head.block,
+    Head.function,
+    Head.return_,
+    Head.raise_,
+    Head.if_,
+    Head.elif_,
+    Head.for_,
+    Head.while_,
+    Head.annotate,
+    Head.import_,
+    Head.from_,
+    Head.as_,
+    Head.with_,
+    Head.class_,
+)
