995bb2d5874c33fff68e76ed6d0c0ef4cb6d7801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:35 +0000

    Add package bullmq/0.4.3/bullmq-0.4.3.tar.gz

---
+++ packages/bullmq/bullmq-0.4.3.tar.gz/bullmq-0.4.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# Get the long description from the README file
+with open(path.join(".", 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bullmq',
+    version='0.4.3',    
+    description='BullMQ for Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://bullmq.io',
+    author='Taskforce.sh Inc.',
+    author_email='manast@taskforce.sh',
+    license='MIT',
+    packages=['bullmq'],
+    package_data={'bullmq': ['commands/*.lua', 'types/*']},
+    install_requires=[
+        'redis',
+        'msgpack',
+        'semver',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
