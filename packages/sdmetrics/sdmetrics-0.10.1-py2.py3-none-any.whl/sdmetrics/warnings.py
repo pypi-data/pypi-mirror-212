7b70433a4ee752e1b08c7734e511a3a9ec16992c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:45 +0000

    Add package sdmetrics/0.10.1/sdmetrics-0.10.1-py2.py3-none-any.whl

---
+++ packages/sdmetrics/sdmetrics-0.10.1-py2.py3-none-any.whl/sdmetrics/warnings.py
@@ -0,0 +1,14 @@
+"""Warnings for sdmetrics."""
+
+
+class SDMetricsWarning(RuntimeWarning):
+    """Class to represent SDMetrics warnings."""
+
+    pass
+
+
+class ConstantInputWarning(SDMetricsWarning):
+    """Thrown when the input data has all the same values."""
+
+    def __init__(self, message):
+        self.message = message
