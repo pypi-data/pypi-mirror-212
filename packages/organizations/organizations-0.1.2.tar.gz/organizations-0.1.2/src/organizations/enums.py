d5b61909aec297bdfacbf676fab3d2f4e5201dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:08 +0000

    Add package organizations/0.1.2/organizations-0.1.2.tar.gz

---
+++ packages/organizations/organizations-0.1.2.tar.gz/organizations-0.1.2/src/organizations/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class OrganizationsSwitches(str, Enum):
+    Organizations = "organizations manager"
+    OrganizationsIssuer = "organizations issuer manager"
+    OrganizationsIntermediary = "organizations intermediary manager"
