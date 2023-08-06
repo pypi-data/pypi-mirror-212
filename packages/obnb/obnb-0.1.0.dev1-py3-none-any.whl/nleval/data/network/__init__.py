bede041fc5787d2c7a55acbca3856704ba0c93c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:06 +0000

    Add package obnb/0.1.0.dev1/obnb-0.1.0.dev1-py3-none-any.whl

---
+++ packages/obnb/obnb-0.1.0.dev1-py3-none-any.whl/nleval/data/network/__init__.py
@@ -0,0 +1,34 @@
+"""Network data."""
+from nleval.data.network.biogrid import BioGRID
+from nleval.data.network.bioplex import BioPlex
+from nleval.data.network.comppi import ComPPIHumanInt
+from nleval.data.network.consensuspathdb import ConsensusPathDB
+from nleval.data.network.funcoup import FunCoup
+from nleval.data.network.hippie import HIPPIE
+from nleval.data.network.humanbase import HumanBaseTopGlobal
+from nleval.data.network.humannet import HumanNet
+from nleval.data.network.humap import HuMAP
+from nleval.data.network.huri import HuRI
+from nleval.data.network.omnipath import OmniPath
+from nleval.data.network.pcnet import PCNet
+from nleval.data.network.proteomehd import ProteomeHD
+from nleval.data.network.signor import SIGNOR
+from nleval.data.network.string import STRING
+
+__all__ = [
+    "BioGRID",
+    "BioPlex",
+    "ComPPIHumanInt",
+    "ConsensusPathDB",
+    "FunCoup",
+    "HIPPIE",
+    "HuRI",
+    "HuMAP",
+    "HumanBaseTopGlobal",
+    "HumanNet",
+    "OmniPath",
+    "PCNet",
+    "ProteomeHD",
+    "SIGNOR",
+    "STRING",
+]
