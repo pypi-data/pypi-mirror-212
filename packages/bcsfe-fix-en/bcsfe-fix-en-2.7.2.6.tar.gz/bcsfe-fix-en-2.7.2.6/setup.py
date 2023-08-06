a51a41c2df5938a1adf353b4c9688b65064b0bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:29 +0000

    Add package bcsfe-fix-en/2.7.2.6/bcsfe-fix-en-2.7.2.6.tar.gz

---
+++ packages/bcsfe-fix-en/bcsfe-fix-en-2.7.2.6.tar.gz/bcsfe-fix-en-2.7.2.6/setup.py
@@ -0,0 +1,42 @@
+"""Setup for installing the package."""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+
+setuptools.setup(
+    name="bcsfe-fix-en",
+    version="2.7.2.6",
+    author="CintagramABP",
+    description="A battle cats save file editor FIX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fieryhenry/BCSFE-Python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    install_requires=[
+        "colored",
+        "tk",
+        "python-dateutil",
+        "requests",
+        "pyyaml",
+    ],
+    include_package_data=True,
+    extras_require={
+        "testing": [
+            "pytest",
+            "pytest-cov",
+        ],
+    },
+    package_data={"BCSFE_FIX": ["py.typed"]},
+    flake8={"max-line-length": 160},
+)
