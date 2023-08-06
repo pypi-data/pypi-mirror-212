7940895512e4fdb0ed8e90f93d149f58eb19285a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:04 +0000

    Add package flask-useful/0.2.4/flask_useful-0.2.4-py3-none-any.whl

---
+++ packages/flask-useful/flask_useful-0.2.4-py3-none-any.whl/flask_useful/sqla/__init__.py
@@ -0,0 +1,12 @@
+from .session import *
+from .utils import *
+
+
+__all__ = (
+    'generate_slug',
+    'get_primary_columns',
+    'get_sqla_session',
+    'normalize_pk',
+    'sqla_session',
+    'SessionMixin',
+)
