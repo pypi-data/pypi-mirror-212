9ee016518089477376b55ac91f0b2eca84ccde6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:12 +0000

    Add package api-console-solver/0.0.26/api-console-solver-0.0.26.tar.gz

---
+++ packages/api-console-solver/api-console-solver-0.0.26.tar.gz/api-console-solver-0.0.26/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='api-console-solver',
+    version='0.0.26',
+    license='MIT License',
+    author='Wesley Romualdo da Silva',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='romualdo@solvedigital.com.br',
+    keywords='console api',
+    description=u'Wrapper oficial da Plataforma Solver',
+    packages=['Solver'],
+    install_requires=['requests'],)
