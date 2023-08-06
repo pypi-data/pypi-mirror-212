87b5d1a251e00a10ea025285f0706d2a90fc17a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:09 +0000

    Add package jdReplace/4.1/jdReplace-4.1.tar.gz

---
+++ packages/jdReplace/jdReplace-4.1.tar.gz/jdReplace-4.1/jdReplace/translations/jdReplace_de.ts
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+  <context>
+    <name>AboutWindow</name>
+    <message>
+      <location filename="../__init__.py" line="28" />
+      <source>With this program you can search and replace a text in all files of a folder</source>
+      <translation>jdReplace sucht und ersetzt einen Text in allen Dateien eines Ordners</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="30" />
+      <source>This program is licensed under GNU GPL 3</source>
+      <translation>Dieses Programm wurde unter GNU GPL 3 lizenziert</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="31" />
+      <source>View Source</source>
+      <translation>Quelltext ansehen</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="32" />
+      <source>Close</source>
+      <translation>Schließen</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="69" />
+      <source>About</source>
+      <translation>Über</translation>
+    </message>
+  </context>
+  <context>
+    <name>ReplaceThread</name>
+    <message>
+      <location filename="../__init__.py" line="95" />
+      <source>Searching {{path}}...</source>
+      <translation>Durchsuche {{path}}...</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="125" />
+      <source>Could not replace text in {{path}}. Maybe it's a binary file.</source>
+      <translation>Konnte Text in {{path}} nicht ersetzen. Möglicherweise ist es eine Binärdatei.</translation>
+    </message>
+  </context>
+  <context>
+    <name>StartWindow</name>
+    <message>
+      <location filename="../__init__.py" line="137" />
+      <source>Directory:</source>
+      <translation>Verzeichnis:</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="202" />
+      <location filename="../__init__.py" line="139" />
+      <source>Browse</source>
+      <translation>Durchsuchen</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="140" />
+      <source>Search for:</source>
+      <translation>Suche nach:</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="142" />
+      <source>Replace with:</source>
+      <translation>Ersetze durch:</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="144" />
+      <source>Search Subdirectories</source>
+      <translation>Unterverzeichnisse durchsuchen</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="145" />
+      <source>Skip Hidden</source>
+      <translation>Keine Versteckten</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="146" />
+      <source>Follow Symlinks</source>
+      <translation>Symbolischen Verknüpfungen folgen</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="148" />
+      <source>About</source>
+      <translation>Über</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="149" />
+      <source>OK</source>
+      <translation>OK</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="223" />
+      <source>Finished</source>
+      <translation>Fertig</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="223" />
+      <source>The text has been successfully replaced in all files</source>
+      <translation>Der Text wurde in allen Dateien erfolgreich ersetzt</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="228" />
+      <source>Not a directory</source>
+      <translation>Kein Verzeichnis</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="228" />
+      <source>'{{path}}' is not a directory!</source>
+      <translation>'{{path}}' ist kein Verzeichnis!</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="233" />
+      <source>No search text</source>
+      <translation>Kein Suchtext</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="233" />
+      <source>Please enter a text to search for</source>
+      <translation>Bitte gib einen Text ein, nach dem gesucht werden soll</translation>
+    </message>
+  </context>
+</TS>
