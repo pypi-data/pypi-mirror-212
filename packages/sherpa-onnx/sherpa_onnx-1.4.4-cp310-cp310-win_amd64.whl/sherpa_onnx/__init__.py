a37259fba1defa68674a834ba90d21c165ad7c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:41 +0000

    Add package sherpa-onnx/1.4.4/sherpa_onnx-1.4.4-cp310-cp310-win_amd64.whl

---
+++ packages/sherpa-onnx/sherpa_onnx-1.4.4-cp310-cp310-win_amd64.whl/sherpa_onnx/__init__.py
@@ -0,0 +1,6 @@
+from _sherpa_onnx import Display
+
+from .online_recognizer import OnlineRecognizer
+from .online_recognizer import OnlineStream
+from .offline_recognizer import OfflineRecognizer
+__version__ = '1.4.4'
