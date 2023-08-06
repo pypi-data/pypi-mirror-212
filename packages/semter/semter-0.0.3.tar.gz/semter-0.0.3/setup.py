38de64abe1a01fd81752b34497121890f9583754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:52 +0000

    Add package semter/0.0.3/semter-0.0.3.tar.gz

---
+++ packages/semter/semter-0.0.3.tar.gz/semter-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from io import open
+from setuptools import setup
+
+version = '0.0.3'
+
+setup(
+    name = 'semter',
+    version=version,
+
+    author='solarmar',
+    author_email='maria134perminova@gmail.com',
+
+    license='Free',
+    
+    packages= ['semter']
+)
