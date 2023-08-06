6ca36eff25f946f28de752dc5568264721a5345e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:11 +0000

    Add package openepd/0.7.0/openepd-0.7.0-py3-none-any.whl

---
+++ packages/openepd/openepd-0.7.0-py3-none-any.whl/openepd/model/common.py
@@ -0,0 +1,86 @@
+#
+#  Copyright 2023 by C Change Labs Inc. www.c-change-labs.com
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This software was developed with support from the Skanska USA,
+#  Charles Pankow Foundation, Microsoft Sustainability Fund, Interface, MKA Foundation, and others.
+#  Find out more at www.BuildingTransparency.org
+#
+from typing import Annotated, Any
+
+import pydantic as pyd
+from pydantic import root_validator
+
+from openepd.model.base import BaseOpenEpdSchema
+
+
+class Amount(BaseOpenEpdSchema):
+    """A value-and-unit pairing for amounts that do not have an uncertainty."""
+
+    qty: float | None = pyd.Field(description="How much of this in the amount.", default=None)
+    unit: str | None = pyd.Field(description="Which unit.  SI units are preferred.", example="kg", default=None)
+
+    @root_validator
+    def check_qty_or_unit(cls, values: dict[str, Any]):
+        """Ensure that qty or unit is provided."""
+        if values["qty"] is None and values["unit"] is None:
+            raise ValueError("Either qty or unit must be provided.")
+        return values
+
+    def to_quantity_str(self):
+        """Return a string representation of the amount."""
+        return f"{self.qty or ''} {self.unit or 'str'}".strip()
+
+
+class Measurement(BaseOpenEpdSchema):
+    """A scientific value with units and uncertainty."""
+
+    mean: float = pyd.Field(description="Mean (expected) value of the measurement")
+    unit: str = pyd.Field(description="Measurement unit")
+    rsd: pyd.PositiveFloat | None = pyd.Field(
+        description="Relative standard deviation, i.e. standard_deviation/mean", default=None
+    )
+    dist: str | None = pyd.Field(description="Statistical distribution of the measurement error.", default=None)
+
+
+class Ingredient(BaseOpenEpdSchema):
+    """
+    An ingredient of a product.
+
+    The Ingredients list gives the core data references and quantities. This list is used to document supply-chain
+    transparency, such as the EPDs of major components (e.g. cement in concrete, or recycled steel
+    in hot-rolled sections).
+    """
+
+    qty: float | None = pyd.Field(
+        description="Number of declared units of this consumed. Negative values indicate an outflow."
+    )
+    link: pyd.AnyUrl | None = pyd.Field(
+        description="Link to this object's OpenEPD declaration. "
+        "An OpenIndustryEPD or OpenLCI link is also acceptable.",
+        default=None,
+    )
+
+
+class WithAttachmentsMixin:
+    """Mixin for objects that can have attachments."""
+
+    attachments: dict[Annotated[str, pyd.Field(max_length=200)], pyd.AnyUrl] | None = pyd.Field(
+        description="Dict of URLs relevant to this entry",
+        example={
+            "Contact Us": "https://www.c-change-labs.com/en/contact-us/",
+            "LinkedIn": "https://www.linkedin.com/company/c-change-labs/",
+        },
+        default=None,
+    )
