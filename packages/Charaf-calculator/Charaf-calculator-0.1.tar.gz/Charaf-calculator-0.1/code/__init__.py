a54f9c11bb633bf80680e45419f8b7e30897c01b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:27 +0000

    Add package Charaf-calculator/0.1/Charaf-calculator-0.1.tar.gz

---
+++ packages/Charaf-calculator/Charaf-calculator-0.1.tar.gz/Charaf-calculator-0.1/code/__init__.py
@@ -0,0 +1,8 @@
+def gamaa(a,b):
+    return a + b
+def tar7(a,b):
+    return a - b
+def darb(a,b):
+    return a * b
+def qisma(a,b):
+    return a / b
---
+++ packages/Charaf-calculator/Charaf-calculator-0.1.tar.gz/Charaf-calculator-0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name= 'Charaf-calculator',
+    version= '0.1',
+    author= 'Charaf-Saaoudi',
+    description= 'simple library in python for test',
+    packages=setuptools.find_packages(),
+    classifiliers=["Programming Language :: 3",
+    "Operating System :: Os indenpendent",
+    "License :: OSI Approved :: MIT License"])
