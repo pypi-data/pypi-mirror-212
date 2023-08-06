61f2ca9909dd94658391969c43bed17e1d78b0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:33 +0000

    Add package demisto-sdk/1.15.5/demisto_sdk-1.15.5-py3-none-any.whl

---
+++ packages/demisto-sdk/demisto_sdk-1.15.5-py3-none-any.whl/demisto_sdk/commands/format/update_correlation_rule.py
@@ -0,0 +1,23 @@
+from demisto_sdk.commands.format.update_generic_yml import BaseUpdateYML
+
+
+class CorrelationRuleYMLFormat(BaseUpdateYML):
+    def __init__(
+        self,
+        input: str = "",
+        output: str = "",
+        path: str = "",
+        from_version: str = "",
+        no_validate: bool = False,
+        **kwargs,
+    ):
+        super().__init__(
+            input=input,
+            output=output,
+            path=path,
+            from_version=from_version,
+            no_validate=no_validate,
+            **kwargs,
+        )
+        if isinstance(self.data, list) and len(self.data) == 1:
+            self.data = self.data[0]
