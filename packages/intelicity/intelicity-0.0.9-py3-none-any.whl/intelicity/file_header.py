e586bb501afd57118074cb7f01ddacd353ad11df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:26 +0000

    Add package intelicity/0.0.9/intelicity-0.0.9-py3-none-any.whl

---
+++ packages/intelicity/intelicity-0.0.9-py3-none-any.whl/intelicity/file_header.py
@@ -0,0 +1,37 @@
+from intelicity.containers import *
+from intelicity.image import *
+from pathlib import Path
+
+
+class FileHeader:
+
+    def __init__(self, path: Path):
+        self.path = path
+        self.file_name = path.name
+
+        stem = path.stem
+        file_name_info = stem.split("-")
+
+        self.driver_id = file_name_info[0]
+        self.date = file_name_info[1]
+        self.time = file_name_info[2]
+
+    def get_neighbor_image(self) -> Image:
+        return Image(self.path.with_suffix(".jpg"))
+
+    def open_as_network_output_container(self, local_root_object_id=0) -> NetworkOutputContainer:
+        lines: list[str]
+        with open(self.path, 'r') as file:
+            lines = file.read().splitlines()
+        network_output_container = NetworkOutputContainer.create_from_line_list(lines, local_root_object_id)
+        return network_output_container
+
+    def open_as_database_container(self) -> DatabaseContainer:
+        lines: list[str]
+        with open(self.path, 'r') as file:
+            lines = file.readlines()
+        database_container = DatabaseContainer.create_from_line_list(lines)
+        return database_container
+
+    def save_container(self, container: Container) -> None:
+        container.save(self.path)
