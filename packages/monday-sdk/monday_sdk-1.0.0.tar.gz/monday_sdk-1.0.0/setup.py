4e50f6f89fa3cc76d4e723bdf16a835b7edef7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:06 +0000

    Add package monday-sdk/1.0.0/monday_sdk-1.0.0.tar.gz

---
+++ packages/monday-sdk/monday_sdk-1.0.0.tar.gz/monday_sdk-1.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['monday_sdk']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.88.0,<0.89.0', 'pendulum>=2.1.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'monday-sdk',
+    'version': '1.0.0',
+    'description': '',
+    'long_description': '# Python SDK for monday.com\n',
+    'author': 'Jonathan Crum',
+    'author_email': 'jcrum@theobogroup.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
