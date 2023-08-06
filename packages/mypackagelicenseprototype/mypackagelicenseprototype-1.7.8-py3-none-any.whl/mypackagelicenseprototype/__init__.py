79eadcd3a5efb569e30a8709e63f5ec665af4c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:11 +0000

    Add package mypackagelicenseprototype/1.7.8/mypackagelicenseprototype-1.7.8-py3-none-any.whl

---
+++ packages/mypackagelicenseprototype/mypackagelicenseprototype-1.7.8-py3-none-any.whl/mypackagelicenseprototype/__init__.py
@@ -0,0 +1,6 @@
+# Pyarmor 8.2.3 (trial), 000000, 2023-06-05T12:12:33.594183
+from mypackagelicenseprototype.pyarmor_runtime import __pyarmor__
+
+from mypackagelicenseprototype.PackageContent import *
+
+
