f59cf52ab151a3148bfd228ab1f6dfa0d7bd471d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:09 +0000

    Add package fractal-tasks-core/0.10.0a0/fractal_tasks_core-0.10.0a0.tar.gz

---
+++ packages/fractal-tasks-core/fractal_tasks_core-0.10.0a0.tar.gz/fractal_tasks_core-0.10.0a0/fractal_tasks_core/tasks/copy_ome_zarr.py
@@ -0,0 +1,206 @@
+"""
+Copyright 2022 (C)
+    Friedrich Miescher Institute for Biomedical Research and
+    University of Zurich
+
+    Original authors:
+    Tommaso Comparin <tommaso.comparin@exact-lab.it>
+    Marco Franzon <marco.franzon@exact-lab.it>
+
+    This file is part of Fractal and was originally developed by eXact lab
+    S.r.l.  <exact-lab.it> under contract with Liberali Lab from the Friedrich
+    Miescher Institute for Biomedical Research and Pelkmans Lab from the
+    University of Zurich.
+
+Task that copies the structure of an OME-NGFF zarr array to a new one
+"""
+import logging
+from pathlib import Path
+from typing import Any
+from typing import Dict
+from typing import Optional
+from typing import Sequence
+
+import anndata as ad
+import zarr
+from anndata.experimental import write_elem
+
+import fractal_tasks_core
+from fractal_tasks_core.lib_regions_of_interest import (
+    convert_ROIs_from_3D_to_2D,
+)
+from fractal_tasks_core.lib_zattrs_utils import extract_zyx_pixel_sizes
+
+logger = logging.getLogger(__name__)
+
+
+__OME_NGFF_VERSION__ = fractal_tasks_core.__OME_NGFF_VERSION__
+
+
+def copy_ome_zarr(
+    *,
+    input_paths: Sequence[str],
+    output_path: str,
+    metadata: Dict[str, Any],
+    project_to_2D: bool = True,
+    suffix: str = "mip",
+    ROI_table_names: Optional[Sequence[str]] = None,
+) -> Dict[str, Any]:
+
+    """
+    Duplicate an input zarr structure to a new path.
+
+    More detailed description:
+
+    1. For each plate, create a new zarr group with the same attributes as the
+    original one.
+    2. For each well (in each plate), create a new zarr subgroup with the same
+    attributes as the original one.
+    3. For each image (in each well), create a new zarr subgroup with the same
+    attributes as the original one.
+    4. For each image (in each well), copy the relevant AnnData tables from the
+    original source.
+
+    Note: this task makes use of methods from the ``Attributes`` class, see
+    https://zarr.readthedocs.io/en/stable/api/attrs.html.
+
+    Examples of some inputs::
+      input_paths = ["/tmp/out/"]
+      output_path = "/tmp/out_mip/"
+
+    :param input_paths: TBD
+    :param output_path: TBD
+    :param metadata: TBD
+    :param project_to_2D: If ``True``, apply a 3D->2D projection to the ROI
+                          tables that are copied to the new zarr.
+    :param suffix: The suffix that is used to transform ``plate.zarr`` into
+                   ``plate_suffix.zarr``. Note that `None` is not currently
+                   supported.
+
+    :param ROI_table_names: List of ROI-table names to be copied. If ``None``,
+                            it is replaced by ``["FOV_ROI_table",
+                            "well_ROI_table"]``. Note: copying non-ROI tables
+                            may fail if ``project_to_2D=True``.
+    """
+
+    # Preliminary check
+    if len(input_paths) > 1:
+        raise NotImplementedError
+    if suffix is None:
+        # FIXME create a standard suffix (with timestamp)
+        raise NotImplementedError
+
+    if ROI_table_names is None:
+        # FIXME: are these defaults OK?
+        ROI_table_names = ["FOV_ROI_table", "well_ROI_table"]
+
+    # List all plates
+    in_path = Path(input_paths[0])
+    list_plates = [p.as_posix() for p in Path(in_path).glob("*.zarr")]
+    logger.info(f"{list_plates=}")
+
+    meta_update: Dict[str, Any] = {"copy_ome_zarr": {}}
+    meta_update["copy_ome_zarr"]["suffix"] = suffix
+    meta_update["copy_ome_zarr"]["sources"] = {}
+
+    # Loop over all plates
+    for zarrurl_old in list_plates:
+        zarrfile = zarrurl_old.split("/")[-1]
+        old_plate_name = zarrfile.split(".zarr")[0]
+        new_plate_name = f"{old_plate_name}_{suffix}"
+        new_plate_dir = Path(output_path).resolve()
+        zarrurl_new = f"{(new_plate_dir / new_plate_name).as_posix()}.zarr"
+        meta_update["copy_ome_zarr"]["sources"][new_plate_name] = zarrurl_old
+
+        logger.info(f"{zarrurl_old=}")
+        logger.info(f"{zarrurl_new=}")
+        logger.info(f"{meta_update=}")
+
+        # Replicate plate attrs
+        old_plate_group = zarr.open_group(zarrurl_old, mode="r")
+        new_plate_group = zarr.open(zarrurl_new)
+        new_plate_group.attrs.put(old_plate_group.attrs.asdict())
+
+        well_paths = [
+            well["path"] for well in new_plate_group.attrs["plate"]["wells"]
+        ]
+        logger.info(f"{well_paths=}")
+        for well_path in well_paths:
+
+            # Replicate well attrs
+            old_well_group = zarr.open_group(
+                f"{zarrurl_old}/{well_path}", mode="r"
+            )
+            new_well_group = zarr.group(f"{zarrurl_new}/{well_path}")
+            new_well_group.attrs.put(old_well_group.attrs.asdict())
+
+            image_paths = [
+                image["path"]
+                for image in new_well_group.attrs["well"]["images"]
+            ]
+            logger.info(f"{image_paths=}")
+
+            for image_path in image_paths:
+
+                # Replicate image attrs
+                old_image_group = zarr.open_group(
+                    f"{zarrurl_old}/{well_path}/{image_path}", mode="r"
+                )
+                new_image_group = zarr.group(
+                    f"{zarrurl_new}/{well_path}/{image_path}"
+                )
+                new_image_group.attrs.put(old_image_group.attrs.asdict())
+
+                # Extract pixel sizes, if needed
+                if ROI_table_names:
+
+                    new_tables_group = new_image_group.create_group("tables/")
+                    new_tables_group.attrs["tables"] = ROI_table_names
+                    if project_to_2D:
+                        path_FOV_zattrs = (
+                            f"{zarrurl_old}/{well_path}/{image_path}/.zattrs"
+                        )
+                        pxl_sizes_zyx = extract_zyx_pixel_sizes(
+                            path_FOV_zattrs, level=0
+                        )
+                        pxl_size_z = pxl_sizes_zyx[0]
+
+                    # Copy the tables in ROI_table_names
+                    for ROI_table_name in ROI_table_names:
+
+                        logger.info(
+                            f"I will now read {ROI_table_name} from "
+                            f"{zarrurl_old=}, convert it to 2D, and "
+                            "write it back to the new zarr file."
+                        )
+                        ROI_table = ad.read_zarr(
+                            f"{zarrurl_old}/{well_path}/{image_path}/"
+                            f"tables/{ROI_table_name}"
+                        )
+                        # Convert 3D FOVs to 2D
+                        if project_to_2D:
+                            new_ROI_table = convert_ROIs_from_3D_to_2D(
+                                ROI_table, pxl_size_z
+                            )
+                        # Write new table
+                        write_elem(
+                            new_tables_group, ROI_table_name, new_ROI_table
+                        )
+
+    for key in ["plate", "well", "image"]:
+        meta_update[key] = [
+            component.replace(".zarr", f"_{suffix}.zarr")
+            for component in metadata[key]
+        ]
+
+    return meta_update
+
+
+if __name__ == "__main__":
+    from fractal_tasks_core.tasks._utils import run_fractal_task
+
+    run_fractal_task(
+        task_function=copy_ome_zarr,
+        coerce_and_validate=True,
+        logger_name=logger.name,
+    )
---
+++ packages/fractal-tasks-core/fractal_tasks_core-0.10.0a0.tar.gz/fractal_tasks_core-0.10.0a0/fractal_tasks_core/tasks/illumination_correction.py
@@ -0,0 +1,280 @@
+"""
+Copyright 2022 (C)
+    Friedrich Miescher Institute for Biomedical Research and
+    University of Zurich
+
+    Original authors:
+    Tommaso Comparin <tommaso.comparin@exact-lab.it>
+    Marco Franzon <marco.franzon@exact-lab.it>
+
+    This file is part of Fractal and was originally developed by eXact lab
+    S.r.l.  <exact-lab.it> under contract with Liberali Lab from the Friedrich
+    Miescher Institute for Biomedical Research and Pelkmans Lab from the
+    University of Zurich.
+
+Apply illumination correction to all fields of view
+"""
+import logging
+import time
+import warnings
+from pathlib import Path
+from typing import Any
+from typing import Dict
+from typing import Optional
+from typing import Sequence
+
+import anndata as ad
+import dask.array as da
+import numpy as np
+import zarr
+from skimage.io import imread
+
+from fractal_tasks_core.lib_channels import get_omero_channel_list
+from fractal_tasks_core.lib_pyramid_creation import build_pyramid
+from fractal_tasks_core.lib_regions_of_interest import (
+    convert_ROI_table_to_indices,
+)
+from fractal_tasks_core.lib_zattrs_utils import extract_zyx_pixel_sizes
+
+logger = logging.getLogger(__name__)
+
+
+def correct(
+    img_stack: np.ndarray,
+    corr_img: np.ndarray,
+    background: int = 110,
+):
+    """
+    Corrects a stack of images, using a given illumination profile (e.g. bright
+    in the center of the image, dim outside).
+
+    img_stack is a four-dimensional (czyx) numpy array, with dummy size along c
+
+    :param dummy: this is just a placeholder
+    :type dummy: int
+    """
+
+    logger.info(f"Start correct, {img_stack.shape}")
+
+    # Check shapes
+    if corr_img.shape != img_stack.shape[2:] or img_stack.shape[0] != 1:
+        raise Exception(
+            "Error in illumination_correction:\n"
+            f"{img_stack.shape=}\n{corr_img.shape=}"
+        )
+
+    # Store info about dtype
+    dtype = img_stack.dtype
+    dtype_max = np.iinfo(dtype).max
+
+    # Background subtraction
+    img_stack[img_stack <= background] = 0
+    img_stack[img_stack > background] -= background
+
+    #  Apply the normalized correction matrix (requires a float array)
+    # img_stack = img_stack.astype(np.float64)
+    new_img_stack = img_stack / (corr_img / np.max(corr_img))[None, None, :, :]
+
+    # Handle edge case: corrected image may have values beyond the limit of
+    # the encoding, e.g. beyond 65535 for 16bit images. This clips values
+    # that surpass this limit and triggers a warning
+    if np.sum(new_img_stack > dtype_max) > 0:
+        warnings.warn(
+            "Illumination correction created values beyond the max range of "
+            f"the current image type. These have been clipped to {dtype_max=}."
+        )
+        new_img_stack[new_img_stack > dtype_max] = dtype_max
+
+    logger.info("End correct")
+
+    # Cast back to original dtype and return
+    return new_img_stack.astype(dtype)
+
+
+def illumination_correction(
+    *,
+    input_paths: Sequence[str],
+    output_path: str,
+    component: str,
+    metadata: Dict[str, Any],
+    overwrite: bool = False,
+    new_component: Optional[str] = None,
+    dict_corr: dict,
+    background: int = 100,
+) -> Dict[str, Any]:
+
+    """
+    FIXME docstring
+
+    Example inputs:
+    input_paths: ["/some/path"]
+    output_path: "/same/or/other/path/"
+    component: myplate.zarr/B/03/0/
+    new_component: myplate_new_name.zarr/B/03/0/
+    metadata: {...}
+
+    :param dummy: this is just a placeholder
+    :type dummy: int
+    """
+
+    # Preliminary checks
+    if len(input_paths) > 1:
+        raise NotImplementedError
+    if (overwrite and new_component is not None) or (
+        new_component is None and not overwrite
+    ):
+        raise Exception(f"{overwrite=}, but {new_component=}")
+
+    if not overwrite:
+        msg = (
+            "We still have to harmonize illumination_correction("
+            "overwrite=False) with replicate_zarr_structure(..., "
+            "suffix=..)"
+        )
+        raise NotImplementedError(msg)
+
+    # Read some parameters from metadata
+    num_levels = metadata["num_levels"]
+    coarsening_xy = metadata["coarsening_xy"]
+
+    # Defione old/new zarrurls
+    plate, well = component.split(".zarr/")
+    in_path = Path(input_paths[0])
+    zarrurl_old = (in_path / component).as_posix()
+    if overwrite:
+        zarrurl_new = zarrurl_old
+    else:
+        new_plate, new_well = new_component.split(".zarr/")
+        if new_well != well:
+            raise Exception(f"{well=}, {new_well=}")
+        zarrurl_new = (Path(output_path) / new_component).as_posix()
+
+    t_start = time.perf_counter()
+    logger.info("Start illumination_correction")
+    logger.info(f"  {overwrite=}")
+    logger.info(f"  {zarrurl_old=}")
+    logger.info(f"  {zarrurl_new=}")
+
+    # Read channels from .zattrs
+    channels = get_omero_channel_list(image_zarr_path=zarrurl_old)
+    num_channels = len(channels)
+
+    # Read FOV ROIs
+    FOV_ROI_table = ad.read_zarr(f"{zarrurl_old}/tables/FOV_ROI_table")
+
+    # Read pixel sizes from zattrs file
+    full_res_pxl_sizes_zyx = extract_zyx_pixel_sizes(
+        f"{zarrurl_old}/.zattrs", level=0
+    )
+
+    # Create list of indices for 3D FOVs spanning the entire Z direction
+    list_indices = convert_ROI_table_to_indices(
+        FOV_ROI_table,
+        level=0,
+        coarsening_xy=coarsening_xy,
+        full_res_pxl_sizes_zyx=full_res_pxl_sizes_zyx,
+    )
+
+    # Extract image size from FOV-ROI indices. Note: this works at level=0,
+    # where FOVs should all be of the exact same size (in pixels)
+    ref_img_size = None
+    for indices in list_indices:
+        img_size = (indices[3] - indices[2], indices[5] - indices[4])
+        if ref_img_size is None:
+            ref_img_size = img_size
+        else:
+            if img_size != ref_img_size:
+                raise Exception(
+                    "ERROR: inconsistent image sizes in list_indices"
+                )
+    img_size_y, img_size_x = img_size[:]
+
+    # Load paths of correction matrices
+    root_path_corr = dict_corr.pop("root_path_corr")
+    if not root_path_corr.endswith("/"):
+        root_path_corr += "/"
+
+    # Assemble dictionary of matrices and check their shapes
+    corrections = {}
+    for channel in channels:
+        wavelength_id = channel["wavelength_id"]
+        corrections[wavelength_id] = imread(
+            root_path_corr + dict_corr[wavelength_id]
+        )
+        if corrections[wavelength_id].shape != (img_size_y, img_size_x):
+            raise Exception(
+                "Error in illumination_correction, "
+                "correction matrix has wrong shape."
+            )
+
+    # Lazily load highest-res level from original zarr array
+    data_czyx = da.from_zarr(f"{zarrurl_old}/0")
+
+    # Create zarr for output
+    if overwrite:
+        fov_path = zarrurl_old
+        new_zarr = zarr.open(f"{zarrurl_old}/0")
+    else:
+        fov_path = zarrurl_new
+        new_zarr = zarr.create(
+            shape=data_czyx.shape,
+            chunks=data_czyx.chunksize,
+            dtype=data_czyx.dtype,
+            store=zarr.storage.FSStore(f"{zarrurl_new}/0"),
+            overwrite=False,
+            dimension_separator="/",
+        )
+
+    # Iterate over FOV ROIs
+    num_ROIs = len(list_indices)
+    for i_c, channel in enumerate(channels):
+        for i_ROI, indices in enumerate(list_indices):
+            # Define region
+            s_z, e_z, s_y, e_y, s_x, e_x = indices[:]
+            region = (
+                slice(i_c, i_c + 1),
+                slice(s_z, e_z),
+                slice(s_y, e_y),
+                slice(s_x, e_x),
+            )
+            logger.info(
+                f"Now processing ROI {i_ROI+1}/{num_ROIs} "
+                f"for channel {i_c+1}/{num_channels}"
+            )
+            # Execute illumination correction
+            corrected_fov = correct(
+                data_czyx[region].compute(),
+                corrections[channel["wavelength_id"]],
+                background=background,
+            )
+            # Write to disk
+            da.array(corrected_fov).to_zarr(
+                url=new_zarr,
+                region=region,
+                compute=True,
+            )
+
+    # Starting from on-disk highest-resolution data, build and write to disk a
+    # pyramid of coarser levels
+    build_pyramid(
+        zarrurl=fov_path,
+        overwrite=overwrite,
+        num_levels=num_levels,
+        coarsening_xy=coarsening_xy,
+        chunksize=data_czyx.chunksize,
+    )
+
+    t_end = time.perf_counter()
+    logger.info(f"End illumination_correction, elapsed: {t_end-t_start}")
+
+    return {}
+
+
+if __name__ == "__main__":
+    from fractal_tasks_core.tasks._utils import run_fractal_task
+
+    run_fractal_task(
+        task_function=illumination_correction,
+        coerce_and_validate=True,
+        logger_name=logger.name,
+    )
