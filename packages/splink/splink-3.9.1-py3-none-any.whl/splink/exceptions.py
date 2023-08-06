a9e433f199b41f62b22a6473535af5c86bed35fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:05 +0000

    Add package splink/3.9.1/splink-3.9.1-py3-none-any.whl

---
+++ packages/splink/splink-3.9.1-py3-none-any.whl/splink/exceptions.py
@@ -0,0 +1,16 @@
+import warnings
+
+# base class for any type of custom exception
+class SplinkException(Exception):
+    pass
+
+
+class EMTrainingException(SplinkException):
+    pass
+
+
+class SplinkDeprecated(DeprecationWarning):
+    pass
+
+
+warnings.simplefilter("always", SplinkDeprecated)
