b26a35c9e461b94a2b6cbc3870aefab461fb87d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:56 +0000

    Add package weave/0.20.0.dev1/weave-0.20.0.dev1.tar.gz

---
+++ packages/weave/weave-0.20.0.dev1.tar.gz/weave-0.20.0.dev1/weave/frontend/assets/Component.f8ff9ae6.js
@@ -0,0 +1,2 @@
+import{af as i,u as o,W as e}from"./index.de10d43c.js";const c=l=>{const a=l.input,r=i({file:a}),t=o(r);return e.createElement("div",null,t.loading?e.createElement("div",null):e.createElement("img",{style:{maxWidth:"100%"},alt:"cool-alt",src:t.result}))};export{c as default};
+//# sourceMappingURL=Component.f8ff9ae6.js.map
