ad3a8861f8dc3f03291d5182bb7666ba7625b230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:07 +0000

    Add package geolat/0.0.1/geolat-0.0.1.tar.gz

---
+++ packages/geolat/geolat-0.0.1.tar.gz/geolat-0.0.1/README.md
@@ -0,0 +1,17 @@
+# geolat
+
+
+[![image](https://img.shields.io/pypi/v/geolat.svg)](https://pypi.python.org/pypi/geolat)
+[![image](https://img.shields.io/conda/vn/conda-forge/geolat.svg)](https://anaconda.org/conda-forge/geolat)
+
+
+**python package for learning interactive mapping**
+
+
+-   Free software: MIT license
+-   Documentation: https://fiopiohoho.github.io/geolat
+    
+
+## Features
+
+-   TODO
---
+++ packages/geolat/geolat-0.0.1.tar.gz/geolat-0.0.1/tests/test_geolat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `geolat` package."""
+
+
+import unittest
+
+from geolat import geolat
+
+
+class TestGeolat(unittest.TestCase):
+    """Tests for `geolat` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
