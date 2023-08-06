e50ebfb250cb10355541022972dc21d3c300273a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:55 +0000

    Add package zhousf-lib/0.6/zhousf-lib-0.6.tar.gz

---
+++ packages/zhousf-lib/zhousf-lib-0.6.tar.gz/zhousf-lib-0.6/zhousflib/decorator/except_util.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# Author:      zhousf
+# Date:        2022/11/1
+# File:        except_util.py
+# Description: 非中断流程异常处理：防止函数因异常中断导致后续流程不执行
+import warnings
+
+
+def except_handler(function):
+    def catch_except(*args, **key_args):
+        try:
+            return function(*args, **key_args)
+        except Exception as e:
+            warnings.warn('{0}: {1}'.format(function.__name__, e))
+
+    return catch_except
+
+
