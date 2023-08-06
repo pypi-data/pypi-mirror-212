711fe85c9b0a5f9c49e9cf4b3a486f9db100db4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:08 +0000

    Add package ggml-python/0.0.4/ggml_python-0.0.4.tar.gz

---
+++ packages/ggml-python/ggml_python-0.0.4.tar.gz/ggml_python-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from skbuild import setup
+
+setup(
+    name="ggml_python",
+    description="Python bindings for ggml",
+    long_description_content_type="text/markdown",
+    version="0.0.4",
+    author="Andrei Betlen",
+    author_email="abetlen@gmail.com",
+    license="MIT",
+    packages=["ggml"],
+    python_requires=">=3.7",
+    install_requires=["numpy>=1.20.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
