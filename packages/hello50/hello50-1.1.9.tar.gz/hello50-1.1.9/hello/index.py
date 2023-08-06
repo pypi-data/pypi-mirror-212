1d7c496eb54f124b8f95f5a928ed3caf51743539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:48 +0000

    Add package hello50/1.1.9/hello50-1.1.9.tar.gz

---
+++ packages/hello50/hello50-1.1.9.tar.gz/hello50-1.1.9/hello/index.py
@@ -0,0 +1,13 @@
+import json
+import os.path
+
+DB_PATH = os.path.join(os.path.dirname(__file__), 'db')
+
+
+def say_hi():
+    return 'hello mother f'
+
+def the_json():
+    print(DB_PATH)
+    with open(f'{DB_PATH}/quran_simple.json', 'r', encoding="utf8") as file:
+        return json.load(file)
