c5c8b73298a3bb39dba2d8f73732b826960679f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:37 +0000

    Add package pyobjc-framework-CoreSpotlight/9.2/pyobjc-framework-CoreSpotlight-9.2.tar.gz

---
+++ packages/pyobjc-framework-CoreSpotlight/pyobjc-framework-CoreSpotlight-9.2.tar.gz/pyobjc-framework-CoreSpotlight-9.2/PyObjCTest/test_corespotlight.py
@@ -0,0 +1,14 @@
+from PyObjCTools.TestSupport import TestCase
+
+import CoreSpotlight
+
+
+class TestCoreSpotlight(TestCase):
+    def testConstants(self):
+        self.assertIsInstance(CoreSpotlight.CoreSpotlightVersionNumber, float)
+        self.assertIsInstance(CoreSpotlight.CoreSpotlightVersionString, bytes)
+
+
+class TestCallableMetadata(TestCase):
+    def test_callable_metadata_is_sane(self):
+        self.assertCallableMetadataIsSane(CoreSpotlight)
