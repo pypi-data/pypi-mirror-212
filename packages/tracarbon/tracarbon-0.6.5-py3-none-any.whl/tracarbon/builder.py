83c209375bdfded5b16024dc57a3af01469ca334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:07 +0000

    Add package tracarbon/0.6.5/tracarbon-0.6.5-py3-none-any.whl

---
+++ packages/tracarbon/tracarbon-0.6.5-py3-none-any.whl/tracarbon/builder.py
@@ -0,0 +1,81 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from tracarbon.conf import TracarbonConfiguration
+from tracarbon.exporters import Exporter, StdoutExporter
+from tracarbon.general_metrics import CarbonEmissionGenerator
+from tracarbon.locations import Country, Location
+
+
+class Tracarbon:
+    """
+    Tracarbon instance.
+    """
+
+    configuration: TracarbonConfiguration
+    exporter: Exporter
+    location: Location
+
+    def __init__(
+        self,
+        configuration: TracarbonConfiguration,
+        exporter: Optional[Exporter],
+        location: Optional[Location],
+    ) -> None:
+        self.configuration = configuration
+        if location:
+            self.location = location
+        else:
+            self.location = Country.get_location(
+                co2signal_api_key=self.configuration.co2signal_api_key,
+                co2signal_url=self.configuration.co2signal_url,
+            )
+        if exporter:
+            self.exporter = exporter
+        else:
+            self.exporter = StdoutExporter(
+                metric_generators=[CarbonEmissionGenerator(location=self.location)]
+            )
+
+    def __enter__(self) -> None:
+        self.exporter.start(interval_in_seconds=self.configuration.interval_in_seconds)
+
+    def __exit__(self, type, value, traceback) -> None:  # type: ignore
+        self.exporter.stop()
+
+    def start(self) -> None:
+        """
+        Start Tracarbon.
+        """
+        self.exporter.start(interval_in_seconds=self.configuration.interval_in_seconds)
+
+    def stop(self) -> None:
+        """
+        Stop Tracarbon.
+        """
+        self.exporter.stop()
+
+
+class TracarbonBuilder(BaseModel):
+    """
+    Tracarbon builder for building Tracarbon.
+    """
+
+    configuration: TracarbonConfiguration = TracarbonConfiguration()
+
+    def build(
+        self, exporter: Optional[Exporter] = None, location: Optional[Location] = None
+    ) -> Tracarbon:
+        """
+        Build Tracarbon with its configuration.
+
+        :param exporter: the exporter to use
+        :param location: the location to use
+        """
+
+        return Tracarbon(
+            configuration=self.configuration,
+            exporter=exporter,
+            location=location,
+        )
---
+++ packages/tracarbon/tracarbon-0.6.5-py3-none-any.whl/tracarbon/exporters/exporter.py
@@ -0,0 +1,137 @@
+import asyncio
+from abc import ABCMeta, abstractmethod
+from threading import Event, Timer
+from typing import AsyncGenerator, Awaitable, Callable, List, Optional
+
+from loguru import logger
+from pydantic import BaseModel
+
+from tracarbon.hardwares.hardware import HardwareInfo
+from tracarbon.locations import Location
+
+
+class Exporter(BaseModel, metaclass=ABCMeta):
+    """The Exporter interface."""
+
+    metric_generators: List["MetricGenerator"]
+    event: Optional[Event] = None
+    stopped: bool = False
+    metric_prefix_name: Optional[str] = None
+
+    class Config:
+        """Pydantic configuration."""
+
+        arbitrary_types_allowed = True
+
+    @abstractmethod
+    async def launch(self, metric_generator: "MetricGenerator") -> None:
+        """
+        Launch the exporter.
+
+        :param metric_generator: the metric generator
+        """
+        pass
+
+    def start(self, interval_in_seconds: int) -> None:
+        """
+        Start the exporter and a dedicated timer configured with the configured timeout.
+
+        :param: interval_in_seconds: the interval for the timer
+        """
+        self.stopped = False
+        if not self.event:
+            self.event = Event()
+
+        def _run() -> None:
+            asyncio.run(self._launch_all())
+            if self.event and not self.stopped and not self.event.is_set():
+                timer = Timer(interval_in_seconds, _run, [])
+                timer.daemon = True
+                timer.start()
+
+        _run()
+
+    def stop(self) -> None:
+        """
+        Stop the explorer and the associated timer.
+
+        :return:
+        """
+        self.stopped = True
+        if self.event:
+            self.event.set()
+
+    async def _launch_all(self) -> None:
+        """
+        Launch the exporter with all the metric generators.
+        """
+        for metric_generator in self.metric_generators:
+            logger.debug(f"Running MetricGenerator[{metric_generator}].")
+            await self.launch(metric_generator=metric_generator)
+
+    @classmethod
+    @abstractmethod
+    def get_name(cls) -> str:
+        """
+        Get the name of the exporter.
+
+        :return: the Exporter's name
+        """
+        pass
+
+
+class Tag(BaseModel):
+    """
+    Tag for a metric.
+    """
+
+    key: str
+    value: str
+
+
+class Metric(BaseModel):
+    """
+    Global metric to use for the exporters.
+    """
+
+    name: str
+    value: Callable[[], Awaitable[float]]
+    tags: List[Tag] = list()
+
+    def format_name(
+        self, metric_prefix_name: Optional[str] = None, separator: str = "."
+    ) -> str:
+        """
+        Format the name of the metric with a prefix and separator.
+
+        :param metric_prefix_name: the prefix to insert before the separator and the name.
+        :param separator: the separator to use between the prefix and the name.
+        """
+        if metric_prefix_name:
+            return f"{metric_prefix_name}{separator}{self.name}"
+        return self.name
+
+    def format_tags(self, separator: str = ":") -> List[str]:
+        """
+        Format tags with a separator.
+
+        :param separator: the separator to insert between the key and value.
+        """
+        return [f"{tag.key}{separator}{tag.value}" for tag in self.tags]
+
+
+class MetricGenerator(BaseModel):
+    """
+    MetricGenerator generates metrics for the Exporter.
+    """
+
+    metrics: List[Metric]
+    platform: str = HardwareInfo.get_platform()
+    location: Optional[Location] = None
+
+    async def generate(self) -> AsyncGenerator[Metric, None]:
+        """
+        Generate a metric.
+        """
+        for metric in self.metrics:
+            yield metric
