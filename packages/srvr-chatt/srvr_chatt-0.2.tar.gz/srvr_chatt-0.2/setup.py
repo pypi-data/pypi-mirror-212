c1e17992ef0aa441ad5608c59f833e37d09d0c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:17 +0000

    Add package srvr-chatt/0.2/srvr_chatt-0.2.tar.gz

---
+++ packages/srvr-chatt/srvr_chatt-0.2.tar.gz/srvr_chatt-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="srvr_chatt",
+    version="0.2",
+    description="Server packet",
+    packages=find_packages(),  # ,Будем искать пакеты тут(включаем авто поиск пакетов)
+    author_email="test@gmail.com",
+    author="Aleksandr",
+    install_requeres=["PyQt5", "sqlalchemy", "pycruptodome", "pycryptodomex"]
+    # зависимости которые нужно до установить
+)
