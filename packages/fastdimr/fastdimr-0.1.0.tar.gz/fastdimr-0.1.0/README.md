85bb020df25ec4cdfbfbe2d51390bf53b9c6c03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:10 +0000

    Add package fastdimr/0.1.0/fastdimr-0.1.0.tar.gz

---
+++ packages/fastdimr/fastdimr-0.1.0.tar.gz/fastdimr-0.1.0/README.md
@@ -0,0 +1,6 @@
+# fastdimr
+
+[![Build Status](https://app.travis-ci.com/inoueakimitsu/fastdimr.svg?branch=main)](https://app.travis-ci.com/inoueakimitsu/fastdimr)
+<a href="https://github.com/inoueakimitsu/fastdimr/issues"><img alt="GitHub issues" src="https://img.shields.io/github/issues/inoueakimitsu/fastdimr"></a> 
+
+Fast predictor of dimensionality reduction and clustering
---
+++ packages/fastdimr/fastdimr-0.1.0.tar.gz/fastdimr-0.1.0/fastdimr/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the fastdimr package.
+"""
+
+__title__ = 'fastdimr'
+__description__ = 'fastdimr - fast dimensionality reduction wrapper.'
+__version__ = '0.1.0'
+__author__ = 'Akimitsu Inoue'
+__author_email__ = 'akimitsu.inoue@gmail.com'
+__license__ = 'MIT'
+__url__ = 'https://github.com/inoueakimitsu/fastdimr'
