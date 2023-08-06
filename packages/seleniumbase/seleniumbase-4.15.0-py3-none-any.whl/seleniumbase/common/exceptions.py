756ada1e11f29ee05c4eb2e08b72bd3cd837e7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:39 +0000

    Add package seleniumbase/4.15.0/seleniumbase-4.15.0-py3-none-any.whl

---
+++ packages/seleniumbase/seleniumbase-4.15.0-py3-none-any.whl/seleniumbase/common/exceptions.py
@@ -0,0 +1,42 @@
+""" SeleniumBase Exceptions
+    NoSuchFileException => Called when self.assert_downloaded_file(...) fails.
+    NotConnectedException => Called when Internet is not reachable when needed.
+    NotUsingChromeException => Used by Chrome-only methods if not using Chrome.
+    NotUsingChromiumException => Used by Chromium-only methods if not Chromium.
+    OutOfScopeException => Used by BaseCase methods when setUp() is skipped.
+    TextNotVisibleException => Called when expected text fails to appear.
+    TimeLimitExceededException => Called when exceeding "--time-limit=SECONDS".
+    VisualException => Called when there's a Visual Diff Assertion Failure.
+"""
+
+
+class NoSuchFileException(Exception):
+    pass
+
+
+class NotConnectedException(Exception):
+    pass
+
+
+class NotUsingChromeException(Exception):
+    pass
+
+
+class NotUsingChromiumException(Exception):
+    pass
+
+
+class OutOfScopeException(Exception):
+    pass
+
+
+class TextNotVisibleException(Exception):
+    pass
+
+
+class TimeLimitExceededException(Exception):
+    pass
+
+
+class VisualException(Exception):
+    pass
