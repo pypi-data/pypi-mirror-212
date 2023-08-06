2a7604d0b8b60d54523de58e97c47281b1bfcf5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:16 +0000

    Add package datasieve/0.1.4/datasieve-0.1.4.tar.gz

---
+++ packages/datasieve/datasieve-0.1.4.tar.gz/datasieve-0.1.4/datasieve/transforms/variance_threshold.py
@@ -0,0 +1,40 @@
+import logging
+import numpy as np
+from sklearn import feature_selection as fs
+from datasieve.transforms.base_transform import BaseTransform
+
+logger = logging.getLogger('datasieve.pipeline')
+
+
+class VarianceThreshold(BaseTransform):
+    """
+    A VarianceThresholdd that removes any feature with a variance below a threshold.
+    Features are removed and a feature list is held for use during subsequent transforms
+    to keep feature columns consistent.
+    """
+
+    def __init__(self, **kwargs) -> None:
+        self._skl: fs.VarianceThreshold = fs.VarianceThreshold(**kwargs)
+        self.feature_list: list = []
+        self.mask = None
+
+    def fit(self, X, y=None, sample_weight=None, feature_list=None, **kwargs):
+        self._skl.fit(X)
+        self.mask = self._skl.get_support()
+        if feature_list is not None:
+            self.feature_list = np.array(feature_list)[self.mask]
+            logger.info("Variance will remove features "
+                        f"{len(feature_list) - len(self.feature_list)} "
+                        f"on transform. {np.array(feature_list)[~self.mask]}")
+        else:
+            self.feature_list = None
+
+        return X, y, sample_weight, self.feature_list
+
+    def transform(self, X, y=None, sample_weight=None,
+                  feature_list=None, outlier_check=False, **kwargs):
+
+        # use mask to filter X array
+        X = X[:, self.mask]
+
+        return X, y, sample_weight, self.feature_list
