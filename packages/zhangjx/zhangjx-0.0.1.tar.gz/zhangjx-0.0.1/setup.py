0264a70e8f58657197191a7813f200cd80be0039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:01 +0000

    Add package zhangjx/0.0.1/zhangjx-0.0.1.tar.gz

---
+++ packages/zhangjx/zhangjx-0.0.1.tar.gz/zhangjx-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zhangjx",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+
+    author="zhangjx",
+    author_email="2624835027@qq.com",
+    description="useless",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="",
+    classifiers=[
+
+    ],
+)
