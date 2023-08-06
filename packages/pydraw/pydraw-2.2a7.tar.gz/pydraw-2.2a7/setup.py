f4ef31228ecf0c149a92f8cdab03f9aae4e64cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package pydraw/2.2a7/pydraw-2.2a7.tar.gz

---
+++ packages/pydraw/pydraw-2.2a7.tar.gz/pydraw-2.2a7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("/Users/noah/Projects/pydraw/README_pypi.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydraw",
+    version="2.2a7",
+    author="Noah Coetsee",
+    author_email="noah@noahcoetsee.me",
+    description="A package designed to make graphics with Python simple and easy!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pydraw/pydraw",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pillow',
+        # 'PyObjC',
+        'pygobject',
+    ],
+    python_requires='>=3.6',
+)
