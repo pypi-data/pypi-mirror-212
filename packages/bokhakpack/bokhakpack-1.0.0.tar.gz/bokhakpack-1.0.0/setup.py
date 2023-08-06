4b396eeb890eebd80376b836536b5586975d7f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:35 +0000

    Add package bokhakpack/1.0.0/bokhakpack-1.0.0.tar.gz

---
+++ packages/bokhakpack/bokhakpack-1.0.0.tar.gz/bokhakpack-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bokhakpack',
+    version='1.0.0',
+    packages=find_packages(),
+    author='dudcks',
+    author_email='dudcks3886@naver.com',
+    description='rest-auth, allauth',
+    # 추가적인 설정
+)
