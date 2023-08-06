a7e3e981bb50dc0e860f18b41939eaa0eecef4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:48 +0000

    Add package datomize/2.3.7/datomize-2.3.7.tar.gz

---
+++ packages/datomize/datomize-2.3.7.tar.gz/datomize-2.3.7/datomizer/utils/messages.py
@@ -0,0 +1,6 @@
+EML_TARGET_INVALID_ROLE = "The selected target is invalid. We currently support only categorical targets."
+EML_TARGET_INVALID_UNIQUE_COUNT = "The selected target is invalid. We currently support only categorical targets with up to 10 classes."
+COLUMN_NOT_EXISTS = "The selected target is invalid. No such field exists in the input data."
+
+OUTPUT_RATIO_INVALID = "Invalid output_ratio: %s. The permitted range is %s - %s"
+MISSING_TARGET_COLUMN = "Missing the target_column argument."
