722cf63bb03dbe6c5cb1ed05bfe7c89dbfc902d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:24 +0000

    Add package molgraph/0.3.6/molgraph-0.3.6.tar.gz

---
+++ packages/molgraph/molgraph-0.3.6.tar.gz/molgraph-0.3.6/molgraph/__init__.py
@@ -0,0 +1,18 @@
+from molgraph import tensors
+from molgraph import layers
+from molgraph import losses
+from molgraph import metrics
+from molgraph import models
+from molgraph import chemistry
+
+from molgraph.tensors import GraphTensor
+from molgraph.tensors import GraphTensorSpec
+
+# filter out some warnings
+from molgraph import _filter_warnings
+
+del _filter_warnings
+
+from . import _version
+
+__version__ = _version.__version__
