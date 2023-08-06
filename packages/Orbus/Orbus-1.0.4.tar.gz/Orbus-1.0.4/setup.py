1ee335c23fda0f7762eba56dea5bea9d902df340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:33 +0000

    Add package Orbus/1.0.4/Orbus-1.0.4.tar.gz

---
+++ packages/Orbus/Orbus-1.0.4.tar.gz/Orbus-1.0.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as ld:
+    long_description = ld.read()
+
+setuptools.setup(
+    name="Orbus",
+    version="1.0.4",
+    author="Wilovy09",
+    author_email="orbuscompany@gmail.com",
+    description="An app skeleton creator, using CLI interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OrbusCompany/Orbus",
+    project_url={
+        "Bug Tracker": "https://github.com/OrbusCompany/Orbus/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    
+    py_modules=["orbusmaker"],
+    entry_points={
+        "console_scripts": [
+            "orbusmaker = orbusmaker.__main__:main"
+        ]
+    },
+    python_requires=">3.10"
+)
