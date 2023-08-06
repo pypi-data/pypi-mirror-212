5ebbe4462b831b64138141bb82f5c3938cfb3110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:40 +0000

    Add package frat-brain/1.5.3a0/frat_brain-1.5.3a0.tar.gz

---
+++ packages/frat-brain/frat_brain-1.5.3a0.tar.gz/frat_brain-1.5.3a0/fRAT/_version.py
@@ -0,0 +1,7 @@
+import toml
+import pathlib
+
+pyproject_path = f"{pathlib.Path(__file__).parents[0].resolve()}/pyproject.toml"
+pyproject_dict = toml.load(pyproject_path)
+
+__version__ = pyproject_dict['tool']['poetry']['version']
