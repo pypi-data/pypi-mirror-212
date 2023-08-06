4d03875e614dff100aef758b293521bd75cb5928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:59 +0000

    Add package repo-review/0.7.0b4/repo_review-0.7.0b4.tar.gz

---
+++ packages/repo-review/repo_review-0.7.0b4.tar.gz/repo_review-0.7.0b4/src/repo_review/checks.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import importlib.metadata
+from collections.abc import Mapping, Set
+from typing import Any, Protocol
+
+from .fixtures import apply_fixtures
+
+__all__ = ["Check", "collect_checks", "is_allowed"]
+
+
+class Check(Protocol):
+    family: str
+    requires: Set[str] = frozenset()  # Optional
+    url: str = ""  # Optional
+
+    def check(self) -> bool | None | str:
+        ...
+
+
+def collect_checks(fixtures: Mapping[str, Any]) -> dict[str, Check]:
+    check_functions = (
+        ep.load() for ep in importlib.metadata.entry_points(group="repo_review.checks")
+    )
+
+    return {
+        k: v
+        for func in check_functions
+        for k, v in apply_fixtures(fixtures, func).items()
+    }
+
+
+def is_allowed(select_list: Set[str], ignore_list: Set[str], name: str) -> bool:
+    """
+    Skips the check if the name is in the ignore list or if the name without the
+    number is in the ignore list. If the select list is not empty, only runs the
+    check if the name or name without the number is in the select list.
+    """
+    if (
+        select_list
+        and name not in select_list
+        and name.rstrip("0123456789") not in select_list
+    ):
+        return False
+    if name in ignore_list or name.rstrip("0123456789") in ignore_list:
+        return False
+    return True
---
+++ packages/repo-review/repo_review-0.7.0b4.tar.gz/repo_review-0.7.0b4/tests/test_cmd.py
@@ -0,0 +1,22 @@
+import subprocess
+
+import pytest
+
+pytest.importorskip("rich")
+pytest.importorskip("sp_repo_review")
+
+
+def test_cmd_help():
+    subprocess.run(["repo-review", "--help"], check=True)
+
+
+def test_cmd_basic():
+    subprocess.run(["repo-review", "."], check=True)
+
+
+def test_cmd_html():
+    subprocess.run(["repo-review", ".", "--format", "html"], check=True)
+
+
+def test_cmd_json():
+    subprocess.run(["repo-review", ".", "--format", "json"], check=True)
