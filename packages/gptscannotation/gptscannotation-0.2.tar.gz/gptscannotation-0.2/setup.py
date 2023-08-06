c706dbbed55b6228c1e25c7c3e13486871f29603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:36 +0000

    Add package gptscannotation/0.2/gptscannotation-0.2.tar.gz

---
+++ packages/gptscannotation/gptscannotation-0.2.tar.gz/gptscannotation-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='gptscannotation',
+    version='0.2',
+    description='Simple package for asking OpenAI GPT to do the scRNA-seq annotation based on the gene signatures',
+    author='Ran Ran',
+    author_email='andran@umich.edu',
+    packages=['gptscannotation'],
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+)
