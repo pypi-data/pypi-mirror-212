30fadb17f210257279f373336ae8d827cde67808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:47 +0000

    Add package liquisource/1.1.6/liquisource-1.1.6-py3-none-any.whl

---
+++ packages/liquisource/liquisource-1.1.6-py3-none-any.whl/liquiclient/mongo_client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pymongo
+
+from liquiclient.config import get_config, get_property
+
+
+# 获取mongodb实例
+def get_client(filepath):
+    get_config(filepath)
+    url = get_property("mongo.url")
+    # 获取mongo链接实例
+    client = pymongo.MongoClient(url)
+
+    return client
+
+
+
+
