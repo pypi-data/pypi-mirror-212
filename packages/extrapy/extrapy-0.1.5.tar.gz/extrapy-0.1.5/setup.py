b056ec163342e62cbceb725f17f89e85e9d351b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:17 +0000

    Add package extrapy/0.1.5/extrapy-0.1.5.tar.gz

---
+++ packages/extrapy/extrapy-0.1.5.tar.gz/extrapy-0.1.5/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['extrapy']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'extrapy',
+    'version': '0.1.5',
+    'description': 'Adding in simple things that are tedious to add in on your own, with one simple line you can unlock easiness.',
+    'long_description': None,
+    'author': 'SalladShooter',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
