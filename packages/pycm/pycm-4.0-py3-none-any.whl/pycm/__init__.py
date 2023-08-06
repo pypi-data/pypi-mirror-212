f96ccdc99843adb69bb085228c1be6b10bee9e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:50 +0000

    Add package pycm/4.0/pycm-4.0-py3-none-any.whl

---
+++ packages/pycm/pycm-4.0-py3-none-any.whl/pycm/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""PyCM modules."""
+from .pycm_param import PYCM_VERSION, OVERALL_BENCHMARK_LIST, CLASS_BENCHMARK_LIST
+from .pycm_error import *
+from .pycm_output import pycm_help, online_help
+from .pycm_distance import DistanceType
+from .pycm_obj import ConfusionMatrix
+from .pycm_compare import Compare
+from .pycm_multilabel_cm import MultiLabelCM
+from .pycm_curve import Curve, ROCCurve, PRCurve
+__version__ = PYCM_VERSION
