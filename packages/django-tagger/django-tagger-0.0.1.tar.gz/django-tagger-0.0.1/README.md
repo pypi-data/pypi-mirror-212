5cb80d3fdc041cdb230c20315dbbb0dca9b90ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:15 +0000

    Add package django-tagger/0.0.1/django-tagger-0.0.1.tar.gz

---
+++ packages/django-tagger/django-tagger-0.0.1.tar.gz/django-tagger-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Django Tagger
+
+This is a generic tagging application for Django projects
+
+This package requires Python 3.7 or later, and Django 2.2 or later.
---
+++ packages/django-tagger/django-tagger-0.0.1.tar.gz/django-tagger-0.0.1/tagging/__init__.py
@@ -0,0 +1,15 @@
+"""
+django-tagger
+"""
+__version__ = '0.0.1'
+__license__ = 'BSD License'
+
+__author__ = 'Jonathan Buchanan'
+__author_email__ = 'jonathan.buchanan@gmail.com'
+
+__maintainer__ = 'Code For Beer'
+__maintainer_email__ = 'codeforbeer@outlook.com'
+
+__url__ = 'https://github.com/tho-asterist/django-tagger'
+
+default_app_config = 'tagging.apps.TaggingConfig'
