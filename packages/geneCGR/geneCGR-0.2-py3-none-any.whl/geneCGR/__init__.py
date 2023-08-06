4d9d0b10f00811a25f29dcfcda7a88c2fb92c19b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:09 +0000

    Add package geneCGR/0.2/geneCGR-0.2-py3-none-any.whl

---
+++ packages/geneCGR/geneCGR-0.2-py3-none-any.whl/geneCGR/__init__.py
@@ -0,0 +1,10 @@
+class Generate:
+    def square(self, i):
+        print("-------------------")
+        print(f'Square of {i}: {i} * {i}')
+        print("-------------------")
+
+    def cube(self, i):
+        print("-------------------")
+        print(f'Cube of {i}: {i} * {i} * {i}')
+        print("-------------------")
