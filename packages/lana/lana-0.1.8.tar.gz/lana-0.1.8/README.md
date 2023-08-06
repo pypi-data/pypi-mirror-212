5f9b98fa80e6836ae157467d1046fe3c2fc3df8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:58 +0000

    Add package lana/0.1.8/lana-0.1.8.tar.gz

---
+++ packages/lana/lana-0.1.8.tar.gz/lana-0.1.8/README.md
@@ -0,0 +1,33 @@
+## Lana 🧶
+
+[![stability-wip](https://img.shields.io/badge/stability-wip-lightgrey.svg)](https://github.com/mkenney/software-guides/blob/master/STABILITY-BADGES.md#work-in-progress) 
+
+**L***inear* **A**lgebra for **n***octurnal* *and* **a**dventurous *data scientists.*
+
+## Install 
+
+```console
+pip install lana
+```
+
+## Example 
+
+```python
+from lana import Matrix, inject
+
+zeros = Matrix.zeros((3,3))
+print(zeros)
+print(f"shape: {zeros.shape}, type: {type(zeros)}")
+print(zeros.to_list()[0], zeros.to_list()[0][0])
+
+mat = Matrix.matrix([[1,2,3],[4,5,6]])
+print(mat)
+print(f"shape: {mat.shape}, type: {type(mat)}")
+for rows in mat.to_list():
+    print(rows, type(rows))
+
+submat = Matrix.matrix(inject(mat.to_list()[0]))
+print(submat)
+print(submat.shape, type(submat))
+```
+
---
+++ packages/lana/lana-0.1.8.tar.gz/lana-0.1.8/src/matrix.rs
@@ -0,0 +1,142 @@
+use pyo3::prelude::*;
+use rayon::prelude::*;
+
+
+#[pyclass]
+#[derive(Debug)]
+pub struct Matrix {
+    #[pyo3(get, set)]
+    data: Vec<Vec<f64>>,
+}
+
+#[pymethods]
+impl Matrix {
+
+    #[staticmethod]
+    pub fn zeros(dimensions: (usize, usize)) -> Self {
+        let (rows, cols) = dimensions;
+        let data: Vec<Vec<f64>> = vec![vec![0.0; cols]; rows];
+
+        Matrix { data }
+    }
+
+    #[staticmethod]
+    pub fn ones(dimensions: (usize, usize)) -> Self {
+        let (rows, cols) = dimensions;
+        let data: Vec<Vec<f64>> = vec![vec![1.0; cols]; rows];
+        Matrix { data }
+    }
+
+    #[staticmethod]
+    pub fn eye(size: usize) -> Self {
+        let data: Vec<Vec<f64>> = (0..size).map(|i| {
+            (0..size).map(|j| if i==j {1.0} else {0.0}).collect()
+        }).collect();
+
+        Matrix { data }
+    }
+
+    #[staticmethod]
+    pub fn full(dimensions: (usize, usize), fill_value: f64) -> Self {
+        let (rows, cols) = dimensions;
+        let data: Vec<Vec<f64>> = vec![vec![fill_value; cols]; rows];
+        Matrix { data }
+    }
+
+    #[staticmethod]
+    pub fn matrix(data: Vec<Vec<f64>>) -> Self {
+        Matrix { data } 
+    }
+
+    #[getter]
+    pub fn shape(&self) -> (usize, usize) {
+        let rows = self.data.len();
+        let cols = self.data[0].len();
+        (rows, cols)
+    }
+
+    pub fn __add__(&self, other: &Matrix) -> Matrix {
+        assert_eq!(self.data.len(), other.data.len());
+        assert_eq!(self.data[0].len(), other.data[0].len());
+        let result: Vec<Vec<f64>> = self
+            .data
+            .par_iter()
+            .zip(other.data.par_iter())
+            .map(|(row1, row2)| {
+                row1.par_iter()
+                    .zip(row2.par_iter())
+                    .map(|(elem1, elem2)| elem1 + elem2)
+                    .collect()
+            }).collect();
+        Matrix { data: result }
+    }
+
+    pub fn __neg__(&self) -> Matrix {
+        let result: Vec<Vec<f64>> = self
+            .data
+            .par_iter()
+            .map(|row| row.iter().map(|elem| -*elem).collect())
+            .collect();
+        Matrix { data: result }
+    }
+
+    pub fn __sub__(&self, other: &Matrix) -> Matrix {
+        assert_eq!(self.data.len(), other.data.len());
+        assert_eq!(self.data[0].len(), other.data[0].len());
+        let result: Vec<Vec<f64>> = self
+            .data
+            .par_iter()
+            .zip(other.data.par_iter())
+            .map(|(row1, row2)| {
+                row1.par_iter()
+                    .zip(row2.par_iter())
+                    .map(|(elem1, elem2)| elem1 - elem2)
+                    .collect()
+            }).collect();
+        Matrix { data: result }
+    }
+
+    pub fn __mul__(&self, other: &Matrix) -> Matrix {
+        assert_eq!(self.data.len(), other.data.len());
+        assert_eq!(self.data[0].len(), other.data[0].len());
+        let result: Vec<Vec<f64>> = self
+            .data
+            .par_iter()
+            .zip(other.data.par_iter())
+            .map(|(row1, row2)| {
+                row1.par_iter()
+                    .zip(row2.par_iter())
+                    .map(|(elem1, elem2)| elem1 * elem2)
+                    .collect()
+            }).collect();
+        Matrix { data: result }
+    } 
+
+    pub fn to_list(&self) -> Vec<Vec<f64>> {
+        self.data.clone()
+    }
+
+    pub fn __repr__(&self) -> String {
+        let mut result = String::new();
+        for (i, row) in self.data.iter().enumerate() {
+            if i == 0 {
+                result.push_str("Matrix([");
+            }
+            result.push_str("[");
+            for (j, val) in row.iter().enumerate() {
+                if j != 0 {
+                    result.push_str(", ");
+                }
+                result.push_str(&val.to_string());
+            }
+            result.push_str("]");
+            if i != self.data.len() - 1 {
+                result.push_str(",\n       ");
+            } else {
+                result.push_str("])");
+            }
+        }
+        result
+    }    
+
+}
