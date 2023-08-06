ced8b42a067a37b7a1145da38dfc7eb233368c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:10 +0000

    Add package olenkapdf/3.0/olenkapdf-3.0.tar.gz

---
+++ packages/olenkapdf/olenkapdf-3.0.tar.gz/olenkapdf-3.0/olenkapdf/pdf2text.py
@@ -0,0 +1,13 @@
+def convert(path=None):
+
+    """
+    Convert the given PDF a text
+
+    Parameters:
+        path(str): The path to PDF file sx text
+
+    Returns:
+          str: The content of the PDF file as text
+    """
+    print("pdf2text")
+
