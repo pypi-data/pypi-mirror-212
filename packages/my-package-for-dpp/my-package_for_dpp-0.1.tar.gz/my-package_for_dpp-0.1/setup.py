e985ad223d406923b4545244d9f187c8a6e469c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:22 +0000

    Add package my-package-for-dpp/0.1/my-package_for_dpp-0.1.tar.gz

---
+++ packages/my-package-for-dpp/my-package_for_dpp-0.1.tar.gz/my-package_for_dpp-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="my-package_for_dpp",
+    version="0.1",
+    packages=find_packages(),
+    description="A sample Python package",
+    install_requires=[],
+)
