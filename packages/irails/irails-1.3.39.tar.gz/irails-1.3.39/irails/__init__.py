bbff32144360ddfcfb93f2367e51f37395e5212c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:43 +0000

    Add package irails/1.3.39/irails-1.3.39.tar.gz

---
+++ packages/irails/irails-1.3.39.tar.gz/irails-1.3.39/irails/__init__.py
@@ -0,0 +1,15 @@
+from fastapi import WebSocketDisconnect,WebSocket
+from .core import (
+    api_router as route, 
+    api,
+    Response,
+    Request, 
+    application,
+    generate_mvc_app,
+    
+    )
+from .base_controller import BaseController
+ 
+from fastapi import Form,UploadFile,File
+
+__version__="1.3.39"
