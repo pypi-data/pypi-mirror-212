6962e5ade6c1c8bd48bf2ca512c5180f6e81f3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:05 +0000

    Add package bqt/1.1.0/bqt-1.1.0.tar.gz

---
+++ packages/bqt/bqt-1.1.0.tar.gz/bqt-1.1.0/bqt/widget_manager.py
@@ -0,0 +1,78 @@
+"""
+widget manager to register your widgets with bqt
+
+- parent widget to blender window (blender_widget)
+- keep widget in front of Blender window only, even when bqt is not wrapped in qt
+"""
+from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import Qt
+import logging
+
+
+__widgets = []
+
+
+class WidgetData():
+    def __init__(self, widget, visible):
+        self.widget = widget
+        self.visible = visible
+
+
+def add(widget):
+    """parent widget to blender window"""
+    if not widget:
+        logging.warning("bqt: widget is None, skipping widget registration")
+        return
+
+    app = QApplication.instance()
+
+    # parent to blender window
+    widget.setParent(app.blender_widget)
+
+    # save widget so we can manage the focus and visibility
+    data = WidgetData(widget, widget.isVisible())  # todo can we init vis state to false?
+    __widgets.append(data)
+
+
+def iter_widget_data():
+    """iterate over all registered widgets, remove widgets that have been removed"""
+    cleanup = []
+    for widget_data in __widgets:
+        if not widget_data.widget:
+            cleanup.append(widget_data)
+            continue
+        yield widget_data
+    for widget_data in cleanup:
+        __widgets.remove(widget_data)
+
+
+def _blender_window_change(hwnd: int):
+    """
+    hide widgets when blender is not focussed,
+    keep widgets in front of the Blender window when Blender is focussed
+    run when changing between a blender & non-blender window
+    """
+    focussed_on_a_blender_window = hwnd != 0  # 0 for windows not created by blender
+
+    for widget_data in iter_widget_data():
+        widget = widget_data.widget
+
+        if focussed_on_a_blender_window:
+
+            # add top flag, ensure the widget stays in front of the blender window
+            widget.setWindowFlags(widget.windowFlags() | Qt.WindowStaysOnTopHint)
+
+            # restore visibility state of the widget
+            if widget_data.visible:
+                widget.show()
+
+        else:  # non-blender window
+            # save visibility state of the widget
+            widget_data.visible = widget.isVisible()
+
+            # remove top flag, allow the widget to be hidden behind the blender window
+            # self.blender_widget2.setWindowFlags(self.blender_widget2.windowFlags() & ~Qt.WindowStaysOnTopHint)
+            widget.hide()  # todo since we hide do we need to remove flag?
+
+    # todo right now widgets stay in front of other blender windows,
+    #  e.g. the preferences window, ideally we handle this
