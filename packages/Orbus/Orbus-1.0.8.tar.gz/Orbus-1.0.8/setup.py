ad647ea6a6d413317b701afae9c225c9c5f5f5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:59 +0000

    Add package Orbus/1.0.8/Orbus-1.0.8.tar.gz

---
+++ packages/Orbus/Orbus-1.0.8.tar.gz/Orbus-1.0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as ld:
+    long_description = ld.read()
+
+setup(
+    name="Orbus",
+    version="1.0.8",
+    author="Wilovy09",
+    author_email="orbuscompany@gmail.com",
+    description="An app skeleton creator, using CLI interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OrbusCompany/Orbus",
+    project_url={
+        "Bug Tracker": "https://github.com/OrbusCompany/Orbus/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=["orbus"],
+    entry_points={"console_scripts": ["create-flet-app = orbusmaker.__main__:FLET"]},
+)
