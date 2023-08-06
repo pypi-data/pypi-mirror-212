d9671650c82c68d6418d8a3c52950272980b2548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:08 +0000

    Add package gogogolokl/1.0.0/gogogolokl-1.0.0.tar.gz

---
+++ packages/gogogolokl/gogogolokl-1.0.0.tar.gz/gogogolokl-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from pathlib import Path
+
+
+
+VERSION = '1.0.0'
+DESCRIPTION = 'dcfdfdfdf'
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="gogogolokl",
+    version=VERSION,
+    author="HW",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
