1d849ef8ef1e1f7084de3e0084357befbc2a9bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:13 +0000

    Add package EnToFa/1.4.1/EnToFa-1.4.1.tar.gz

---
+++ packages/EnToFa/EnToFa-1.4.1.tar.gz/EnToFa-1.4.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EnToFa',
+    version='1.4.1',
+    py_modules=['translator'],
+    install_requires=[
+        'requests',
+        'win10toast'
+    ],
+    entry_points='''
+        [console_scripts]
+        EnToFa=translator:main
+    ''',
+    package_data={
+        '': ['README.md'],
+        'EnToFa': ['assets/images/*'],
+    },
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
