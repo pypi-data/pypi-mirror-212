1bf6571c12d87dc5cdddc16c2de26ee86b3fb4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:33 +0000

    Add package vspreview/0.5.0/vspreview-0.5.0-py3-none-any.whl

---
+++ packages/vspreview/vspreview-0.5.0-py3-none-any.whl/vspreview/core/types/misc.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+
+import vapoursynth as vs
+
+__all__ = [
+    'CroppingInfo',
+    'VideoOutputNode',
+    'ViewMode',
+    'Stretch'
+]
+
+
+@dataclass
+class CroppingInfo:
+    top: int
+    left: int
+    width: int
+    height: int
+    active: bool = True
+    is_absolute: bool = False
+
+
+@dataclass
+class VideoOutputNode:
+    clip: vs.VideoNode
+    alpha: vs.VideoNode | None
+
+
+class ViewMode(str, Enum):
+    NORMAL = 'Normal'
+    FFTSPECTRUM = 'FFTSpectrum'
+
+
+@dataclass
+class Stretch:
+    amount: int = 0
