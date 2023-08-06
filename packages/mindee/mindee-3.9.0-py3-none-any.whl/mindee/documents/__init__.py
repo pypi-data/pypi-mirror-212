0e51252cd83e78e5f7bc2e228bcc91f38d24f975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:13 +0000

    Add package mindee/3.9.0/mindee-3.9.0-py3-none-any.whl

---
+++ packages/mindee/mindee-3.9.0-py3-none-any.whl/mindee/documents/__init__.py
@@ -0,0 +1,25 @@
+from mindee.documents import eu, fr, us
+from mindee.documents.cropper import CropperV1, TypeCropperV1
+from mindee.documents.custom import CustomV1, TypeCustomV1
+from mindee.documents.financial_document import (
+    FinancialDocumentV1,
+    FinancialV1,
+    TypeFinancialDocumentV1,
+    TypeFinancialV1,
+)
+from mindee.documents.invoice import InvoiceV3, InvoiceV4, TypeInvoiceV3, TypeInvoiceV4
+from mindee.documents.invoice_splitter import InvoiceSplitterV1, TypeInvoiceSplitterV1
+from mindee.documents.passport import PassportV1, TypePassportV1
+from mindee.documents.proof_of_address import ProofOfAddressV1, TypeProofOfAddressV1
+from mindee.documents.receipt import (
+    ReceiptV3,
+    ReceiptV4,
+    ReceiptV5,
+    TypeReceiptV3,
+    TypeReceiptV4,
+    TypeReceiptV5,
+)
+from mindee.documents.shipping_container import (
+    ShippingContainerV1,
+    TypeShippingContainerV1,
+)
