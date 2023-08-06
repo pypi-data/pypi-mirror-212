0696f239b0f0655ddcffd093e9b7b7cbdfff2286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:48 +0000

    Add package bahire-hasab/0.2.7/bahire-hasab-0.2.7.tar.gz

---
+++ packages/bahire-hasab/bahire-hasab-0.2.7.tar.gz/bahire-hasab-0.2.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("./README.md", "r") as file:
+    long_description = file.read()
+setup(
+    name="bahire-hasab",
+    author="Hundera Awoke",
+    version="0.2.7",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email="hunderaweke@gmail.com",
+    url="https://github.com/hunderaweke/bahire-hasab",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[],
+    keywords="bahire_hasab",
+    entry_points ={
+        'console_scripts':[
+            "bahire-hasab=bahire_hasab.__main__:main",
+        ]
+    }
+)
