1d32e317c35ab3dd3d623cdf45ee311ca07ba97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:04 +0000

    Add package generate-joke/2.0.0/generate_joke-2.0.0.tar.gz

---
+++ packages/generate-joke/generate_joke-2.0.0.tar.gz/generate_joke-2.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='generate_joke',
+    version='2.0.0',
+    author='Gautam Sagar Mallela',
+    author_email='gautamsagar.mallela@gmail.com',
+    description='This Module is used to fetch a random joke from the server',
+    packages=[],
+    install_requires=[
+        # List any dependencies your package requires
+        "requests"
+    ],
+)
