0a060b7084640ff86289ec6157ad725198382f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:03 +0000

    Add package django-anonymous/0.3.0/django-anonymous-0.3.0.tar.gz

---
+++ packages/django-anonymous/django-anonymous-0.3.0.tar.gz/django-anonymous-0.3.0/django_anonymous/__init__.py
@@ -0,0 +1,11 @@
+from .anonymizer import Anonymizer
+from .faker import Faker
+from .register import register
+
+__all__ = [
+    "Faker",
+    "Anonymizer",
+    "register",
+]
+
+__version__ = "0.3.0"
