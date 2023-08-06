a1b8e3e97978646305a0a264c6a03fc1467d3858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:00 +0000

    Add package muffin-rest/5.1.3/muffin_rest-5.1.3-py3-none-any.whl

---
+++ packages/muffin-rest/muffin_rest-5.1.3-py3-none-any.whl/muffin_rest/peewee/sorting.py
@@ -0,0 +1,64 @@
+"""Support sorting for Peewee ORM."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Type, Union, cast
+
+from peewee import Field
+
+from muffin_rest.sorting import Sort, Sorting
+
+if TYPE_CHECKING:
+    from .types import TVCollection
+
+
+class PWSort(Sort):
+    """Sorter for Peewee."""
+
+    async def apply(
+        self, collection: TVCollection, *, desc: bool = False, **_
+    ) -> TVCollection:
+        """Sort the collection."""
+        return collection.order_by_extend(self.field if not desc else self.field.desc())
+
+
+class PWSorting(Sorting):
+    """Sort Peewee ORM Queries."""
+
+    MUTATE_CLASS: Type[PWSort] = PWSort
+
+    def prepare(self, collection: TVCollection) -> TVCollection:
+        """Prepare collection for sorting."""
+        return collection.order_by()
+
+    def convert(self, obj: Union[str, Field, PWSort], **meta):
+        """Prepare sorters."""
+        from . import PWRESTHandler
+
+        if isinstance(obj, PWSort):
+            return obj
+
+        handler = cast(PWRESTHandler, self.handler)
+        model_meta = handler.meta.model._meta  # type: ignore[]
+
+        if isinstance(obj, Field):
+            name, field = obj.name, obj
+
+        else:
+            name = obj
+            field = meta.pop("field", model_meta.fields.get(name))
+
+        if field:
+            sort = self.MUTATE_CLASS(name, field=field, **meta)
+            if sort.meta.get("default"):
+                self.default.append(sort)
+
+            return sort
+
+    def sort_default(self, collection: TVCollection) -> TVCollection:
+        """Sort collection by default."""
+        sorting = [
+            sort.field.desc() if sort.meta["default"] == "desc" else sort.field
+            for sort in self.default
+        ]
+        return collection.order_by(*sorting)
